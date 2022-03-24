d62b0216a8bfd7ae844134a50a9286a31b8ccd8a
Jeff Omhover <jf.omhover@gmail.com>
2022-03-24 11:10:58 -0700

    Vision: make training pipeline configurable, share main() function with canary (#10)
    * make train configurable from cli
    * factor main function into distinct file
    * use main() in canary

--- vision/src/pipelines/benchmark/train.py
+++ vision/src/pipelines/benchmark/train.py
@@ -1,3 +1,7 @@
+"""
+Azure ML Pipeline Script - Runs the torchvision image classification
+training component on a given training/validation dataset.
+"""
 import os
 import sys
 import argparse
@@ -26,12 +30,56 @@ from common.main import main, VISION_COMPONENTS_FOLDER
 def build_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     """Constructs the argument parser"""
     parser.add_argument(
-        "--cpu_cluster",
+        "--training_dataset",
         required=False,
         type=str,
-        default="cpu-cluster",
-        help="name of CPU cluster",
+        default="stanford_dogs:1",
+        help="NAME:VERSION of training dataset (default: stanford_dogs:1)",
     )
+    parser.add_argument(
+        "--validation_dataset",
+        required=False,
+        type=str,
+        default="stanford_dogs:1",
+        help="NAME:VERSION of training dataset (default: stanford_dogs:1)",
+    )
+    parser.add_argument(
+        "--epochs",
+        required=False,
+        type=int,
+        default=5,
+        help="number of epochs (default: 5)",
+    )
+    # parser.add_argument(
+    #     "--input_mode",
+    #     required=False,
+    #     type=str,
+    #     choices=['rw_mount', 'ro_mount', 'download'],
+    #     default="ro_mount",
+    #     help="which mode to use for input data",
+    # )
+    parser.add_argument(
+        "--prefetch_factor",
+        type=int,
+        required=False,
+        default=2,
+        help="ata loader prefetch factor (default: 2)",
+    )
+    parser.add_argument(
+        "--num_workers",
+        required=False,
+        type=int,
+        default=-1,
+        help="number of workers for pre-fetching (default: -1 to use all available cpus)",
+    )
+    parser.add_argument(
+        "--enable_profiling",
+        required=False,
+        action="store_true",
+        default=False,
+        help="enable pytorch profiler",
+    )
+
     parser.add_argument(
         "--gpu_cluster",
         required=False,
@@ -68,11 +116,6 @@ def build(ml_client: MLClient, config: argparse.Namespace) -> Any:
         pipeline_instance (Any): the pipeline instance built, ready to submit
     """
     # load the components from their yaml specifications
-    generate_func = dsl.load_component(
-        yaml_file=os.path.join(
-            VISION_COMPONENTS_FOLDER, "generate_random_image_classes", "spec.yaml"
-        )
-    )
     training_func = dsl.load_component(
         yaml_file=os.path.join(
             VISION_COMPONENTS_FOLDER, "torchvision_finetune", "spec.yaml"
@@ -81,29 +124,22 @@ def build(ml_client: MLClient, config: argparse.Namespace) -> Any:
 
     # the dsl decorator tells the sdk that we are defining an Azure ML pipeline
     @dsl.pipeline(
-        description="canary pipeline image classification",  # TODO: document
+        description="torchvision image classification",  # TODO: document
     )
-    def canary_pipeline():
-        # generate random data
-        generate_random_step = generate_func(
-            classes=4,
-            train_samples=100,
-            valid_samples=100,
-            width=300,
-            height=300,
-        )
-        # we set the name of the compute target for this training job
-        generate_random_step.compute = config.cpu_cluster
-
+    def e2e_pipeline(
+        train_images,
+        valid_images,
+    ):
         # the training step is calling our training component with the right arguments
         training_step = training_func(
             # inputs
-            train_images=generate_random_step.outputs.output_train,
-            valid_images=generate_random_step.outputs.output_valid,
-            # params (hardcoded for canary)
-            num_epochs=1,
-            num_workers=0,  # do not use prefetching
-            enable_profiling=False,  # turns profiler off
+            train_images=train_images,
+            valid_images=valid_images,
+            # params (some hardcoded, some given by pipeline parameters)
+            num_epochs=config.epochs,
+            prefetch_factor=config.prefetch_factor,
+            num_workers=config.num_workers,
+            enable_profiling=config.enable_profiling,
         )
         # we set the name of the compute target for this training job
         training_step.compute = config.gpu_cluster
@@ -119,8 +155,20 @@ def build(ml_client: MLClient, config: argparse.Namespace) -> Any:
         # this pipeline will not return anything
         return {}
 
+    # unpack arguments NAME:VERSION from CLI
+    train_images = config.training_dataset.split(":")[0]
+    train_images_version = int(config.training_dataset.split(":")[1])
+    valid_images = config.validation_dataset.split(":")[0]
+    valid_images_version = int(config.validation_dataset.split(":")[1])
+
+    # call the pipeline function to create instance
+    pipeline_instance = e2e_pipeline(
+        train_images=ml_client.datasets.get(train_images, version=train_images_version),
+        valid_images=ml_client.datasets.get(valid_images, version=valid_images_version)
+    )
+
     # return the instance
-    return canary_pipeline()
+    return pipeline_instance
 
 
 if __name__ == "__main__":
