c241e9aa1ad4fe2c9ba9ab76ba47ba9892e60521
Jeff Omhover <jf.omhover@gmail.com>
2022-04-06 15:07:39 -0700

    Vision: separate model loader to allow for multiple libraries (#27)
    * separate model loader to allow for multiple libraries
    
    * linting
    
    Co-authored-by: Jeff Omhover <jeomhove@microsoft.com>

---
+++ vision/src/components/torchvision_finetune/model/model_loader.py
@@ -0,0 +1,48 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""
+This script provides code to load and setup a variety of models from multiple libraries.
+"""
+
+MODEL_ARCH_MAP = {
+    "resnet18": {"input_size": 224, "library": "torchvision"},
+    "resnet34": {"input_size": 224, "library": "torchvision"},
+    "resnet50": {"input_size": 224, "library": "torchvision"},
+    "resnet101": {"input_size": 224, "library": "torchvision"},
+    "resnet152": {"input_size": 224, "library": "torchvision"},
+    "alexnet": {"input_size": 224, "library": "torchvision"},
+    "vgg11": {"input_size": 224, "library": "torchvision"},
+    "vgg11_bn": {"input_size": 224, "library": "torchvision"},
+    "vgg13": {"input_size": 224, "library": "torchvision"},
+    "vgg13_bn": {"input_size": 224, "library": "torchvision"},
+    "vgg16": {"input_size": 224, "library": "torchvision"},
+    "vgg16_bn": {"input_size": 224, "library": "torchvision"},
+    "vgg19": {"input_size": 224, "library": "torchvision"},
+    "vgg19_bn": {"input_size": 224, "library": "torchvision"},
+}
+
+MODEL_ARCH_LIST = list(MODEL_ARCH_MAP.keys())
+
+
+def get_model_metadata(model_arch: str):
+    """Returns the model metadata"""
+    if model_arch in MODEL_ARCH_MAP:
+        return MODEL_ARCH_MAP[model_arch]
+    else:
+        return NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
+
+
+def load_model(model_arch: str, output_dimension: int = 1, pretrained: bool = True):
+    """Loads a model from a given arch and sets it up for training"""
+    if model_arch not in MODEL_ARCH_MAP:
+        return NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
+
+    if MODEL_ARCH_MAP[model_arch]["library"] == "torchvision":
+        from .torchvision_models import load_torchvision_model
+
+        return load_torchvision_model(model_arch, output_dimension, pretrained)
+    else:
+        return NotImplementedError(
+            f"library {MODEL_ARCH_MAP[model_arch]['library']} is not implemented yet."
+        )
--- vision/src/components/torchvision_finetune/model/torchvision_models.py
+++ vision/src/components/torchvision_finetune/model/torchvision_models.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
 
-MODEL_ARCH_LIST = [
+TORCHVISION_MODEL_ARCH_LIST = [
     "resnet18",
     "resnet34",
     "resnet50",
@@ -25,60 +25,10 @@ MODEL_ARCH_LIST = [
     "vgg16_bn",
     "vgg19",
     "vgg19_bn",
-    # "squeezenet",
-    # "densenet",
-    # "inception",
-    # "googlenet",
-    # "shufflenet",
-    # "mobilenet_v2",
-    # "mobilenet_v3_large",
-    # "mobilenet_v3_small",
-    # "resnext50_32x4d",
-    # "wide_resnet50_2",
-    # "mnasnet",
-    # "efficientnet_b0",
-    # "efficientnet_b1",
-    # "efficientnet_b2",
-    # "efficientnet_b3",
-    # "efficientnet_b4",
-    # "efficientnet_b5",
-    # "efficientnet_b6",
-    # "efficientnet_b7",
-    # "regnet_y_400mf",
-    # "regnet_y_800mf",
-    # "regnet_y_1_6gf",
-    # "regnet_y_3_2gf",
-    # "regnet_y_8gf",
-    # "regnet_y_16gf",
-    # "regnet_y_32gf",
-    # "regnet_x_400mf",
-    # "regnet_x_800mf",
-    # "regnet_x_1_6gf",
-    # "regnet_x_3_2gf",
-    # "regnet_x_8gf",
-    # "regnet_x_16gf",
-    # "regnet_x_32gf",
 ]
 
-MODEL_ARCH_INPUT_SIZES = {
-    "resnet18" : 224,
-    "resnet34" : 224,
-    "resnet50" : 224,
-    "resnet101" : 224,
-    "resnet152" : 224,
-    "alexnet" : 224,
-    "vgg11" : 224,
-    "vgg11_bn" : 224,
-    "vgg13" : 224,
-    "vgg13_bn" : 224,
-    "vgg16" : 224,
-    "vgg16_bn" : 224,
-    "vgg19" : 224,
-    "vgg19_bn" : 224,
-}
 
-
-def load_and_model_arch(
+def load_torchvision_model(
     model_arch: str, output_dimension: int = 1, pretrained: bool = True
 ):
     """Loads a model from a given arch and sets it up for training"""
@@ -87,7 +37,7 @@ def load_and_model_arch(
     logger.info(
         f"Loading model from arch={model_arch} pretrained={pretrained} output_dimension={output_dimension}"
     )
-    if model_arch in MODEL_ARCH_LIST:
+    if model_arch in TORCHVISION_MODEL_ARCH_LIST:
         model = getattr(models, model_arch)(pretrained=pretrained)
     else:
         raise NotImplementedError(
@@ -98,22 +48,22 @@ def load_and_model_arch(
     if model_arch.startswith("resnet"):
         model.fc = torch.nn.Sequential(
             torch.nn.Linear(model.fc.in_features, output_dimension),
-            torch.nn.Softmax(dim=1)   # adding Softmax to output probs
+            torch.nn.Softmax(dim=1),  # adding Softmax to output probs
         )
     elif model_arch == "alexnet":
         model.classifier[6] = torch.nn.Sequential(
             torch.nn.Linear(4096, output_dimension),
-            torch.nn.Softmax(dim=1)   # adding Softmax to output probs
+            torch.nn.Softmax(dim=1),  # adding Softmax to output probs
         )
     elif model_arch.startswith("vgg"):
         model.classifier[6] = torch.nn.Sequential(
             torch.nn.Linear(4096, output_dimension),
-            torch.nn.Softmax(dim=1)   # adding Softmax to output probs
+            torch.nn.Softmax(dim=1),  # adding Softmax to output probs
         )
     elif model_arch == "densenet":
         model.classifier = torch.nn.Sequential(
             torch.nn.Linear(1024, output_dimension),
-            torch.nn.Softmax(dim=1)   # adding Softmax to output probs
+            torch.nn.Softmax(dim=1),  # adding Softmax to output probs
         )
     else:
         raise NotImplementedError(
