df8e39e116342ce2b4777c78fa4935e9d8c3cc9b
Jeff Omhover <jf.omhover@gmail.com>
2022-04-11 17:35:43 -0700

    Vision: add tests for model loader (#32)
    * add tests for model loader
    * add torch test
    * add raise test and fix

--- vision/src/components/pytorch_image_classifier/model/model_loader.py
+++ vision/src/components/pytorch_image_classifier/model/model_loader.py
@@ -30,19 +30,19 @@ def get_model_metadata(model_arch: str):
     if model_arch in MODEL_ARCH_MAP:
         return MODEL_ARCH_MAP[model_arch]
     else:
-        return NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
+        raise NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
 
 
 def load_model(model_arch: str, output_dimension: int = 1, pretrained: bool = True):
     """Loads a model from a given arch and sets it up for training"""
     if model_arch not in MODEL_ARCH_MAP:
-        return NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
+        raise NotImplementedError(f"model_arch={model_arch} is not implemented yet.")
 
     if MODEL_ARCH_MAP[model_arch]["library"] == "torchvision":
         from .torchvision_models import load_torchvision_model
 
         return load_torchvision_model(model_arch, output_dimension, pretrained)
     else:
-        return NotImplementedError(
+        raise NotImplementedError(
             f"library {MODEL_ARCH_MAP[model_arch]['library']} is not implemented yet."
         )
---
+++ vision/tests/components/test_model_loader.py
@@ -0,0 +1,38 @@
+"""
+Tests running the pytorch_image_classifier/model/ loader
+"""
+import pytest
+import torch
+
+from components.pytorch_image_classifier.model import (
+    MODEL_ARCH_LIST,
+    get_model_metadata,
+    load_model,
+)
+
+# IMPORTANT: see conftest.py for fixtures
+
+@pytest.mark.parametrize("model_arch", MODEL_ARCH_LIST)
+def test_model_loader(model_arch):
+    """Tests src/components/pytorch_image_classifier/model/"""
+    model_metadata = get_model_metadata(model_arch)
+
+    assert model_metadata is not None
+    assert isinstance(model_metadata, dict)
+    assert "library" in model_metadata
+    assert "input_size" in model_metadata
+
+    # using pretrained=False to avoid downloading each time we unit test
+    model = load_model(model_arch, output_dimension=4, pretrained=False)
+
+    assert model is not None
+    assert isinstance(model, torch.nn.Module)
+
+
+def test_model_loader_failure():
+    """Test asking for a model that deosn't exist"""
+    with pytest.raises(NotImplementedError):
+        get_model_metadata("not_a_model")
+
+    with pytest.raises(NotImplementedError):
+        load_model("not_a_model", output_dimension=4, pretrained=False)
