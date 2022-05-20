2cbb5ce70830eb7fdd599d5cb40f6b233bb84037
Louie Larson <lolarson@microsoft.com>
2022-05-20 12:53:55 -0400

    Simplify test

--- test/test_test_assets.py
+++ test/test_test_assets.py
@@ -1,19 +1,20 @@
 from pathlib import Path
+import pytest
 
 import azureml.assets as assets
 
+RESOURCES_DIR = Path("resources")
 
-def test_test_assets():
+
+@pytest.mark.parametrize(
+    "test_subdir,expected",
+    [("good-assets-with-requirements", True), ("bad-assets", False), ("mixed-assets", False)]
+)
+def test_test_assets(test_subdir: str, expected: bool):
     this_dir = Path(__file__).parent
 
     assert assets.test_assets(
-        this_dir / "resources/good-assets",
+        this_dir / RESOURCES_DIR / test_subdir,
         assets.DEFAULT_ASSET_FILENAME,
         this_dir / "../scripts/test-requirements.txt",
-        [])
-
-    assert not assets.test_assets(
-        this_dir / "resources/bad-assets",
-        assets.DEFAULT_ASSET_FILENAME,
-        this_dir / "../scripts/test-requirements.txt",
-        [])
+        []) == expected
