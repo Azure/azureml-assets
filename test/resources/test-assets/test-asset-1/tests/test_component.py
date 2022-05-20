52063061b3ff6cfac57494d30525dba0ccfa871c
Louie Larson <lolarson@microsoft.com>
2022-05-20 12:40:21 -0400

    Add good and bad assets to test

--- test/resources/test-assets/test-asset-1/tests/test_component.py
+++
@@ -1,6 +0,0 @@
-def test_success():
-    assert True
-
-
-def test_failure():
-    assert False
--- test/test_test_assets.py
+++ test/test_test_assets.py
@@ -3,12 +3,17 @@ from pathlib import Path
 import azureml.assets as assets
 
 
-# TODO: Make this work
-
 def test_test_assets():
     this_dir = Path(__file__).parent
+
     assert assets.test_assets(
-        this_dir / "resources/test-assets",
+        this_dir / "resources/good-assets",
+        assets.DEFAULT_ASSET_FILENAME,
+        this_dir / "../scripts/test-requirements.txt",
+        [])
+
+    assert not assets.test_assets(
+        this_dir / "resources/bad-assets",
         assets.DEFAULT_ASSET_FILENAME,
         this_dir / "../scripts/test-requirements.txt",
         [])
