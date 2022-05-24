125aae9261685596a6da46992b8faff8e73f6261
Louie Larson <lolarson@microsoft.com>
2022-05-24 17:33:22 -0400

    Try to get version to the top of task YAML files

--- scripts/azureml-assets/azureml/assets/environment/build.py
+++ scripts/azureml-assets/azureml/assets/environment/build.py
@@ -51,8 +51,8 @@ def create_acr_task(image_name: str,
     # Write YAML file to disk
     task_file = build_context_dir / task_filename
     with open(task_file, "w") as f:
-        yaml.dump(task, f)
-    print(yaml.dump(task))
+        yaml.dump(task, f, default_flow_style=False, sort_keys=False)
+    print(yaml.dump(task, default_flow_style=False, sort_keys=False))
 
     return task_file
 
