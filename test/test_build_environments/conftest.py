3ad83ea1bbf276f44a27c88b83d69d195ff61219
Louie Larson <lolarson@microsoft.com>
2022-05-26 06:24:18 -0600

    Try with two conftest.py files at different levels

--- test/test_build_environments/conftest.py
+++ test/test_build_environments/conftest.py
@@ -1,17 +1,4 @@
-def pytest_addoption(parser):
-    parser.addoption("--resource-group", action="store")
-    parser.addoption("--registry", action="store")
-
-
 def pytest_generate_tests(metafunc):
-    resource_group_value = metafunc.config.option.resource_group
-    if 'resource_group' in metafunc.fixturenames and resource_group_value is not None:
-        metafunc.parametrize('resource_group', [resource_group_value])
-
-    registry_value = metafunc.config.option.registry
-    if 'registry' in metafunc.fixturenames and registry_value is not None:
-        metafunc.parametrize('registry', [registry_value])
-
     params = [
         ("build-bad", False),
         ("build-test-bad", False),
