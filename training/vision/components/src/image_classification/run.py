3e06e75b47012b4d88e118260e8b14158cfb6557
shubham soni <sonishubham@microsoft.com>
2022-06-03 18:03:59 +0530

    Adding changes for comments

--- training/vision/components/src/image_classification/run.py
+++ training/vision/components/src/image_classification/run.py
@@ -4,38 +4,54 @@ import json
 from azureml.automl.core.shared.constants import MLTableDataLabel, MLTableLiterals, Tasks
 from azureml.automl.dnn.vision.common import utils
 from azureml.automl.dnn.vision.common.constants import SettingsLiterals
-try:
-    from azureml.automl.dnn.vision.classification import runner
-except ImportError:
-    from azureml.contrib.automl.dnn.vision.classification import runner
-
+from azureml.automl.dnn.vision.classification import runner
 from common.telemetry_utils import create_component_telemetry_wrapper
+from azureml.core import Run
+
+def runClassificationComponent(task_type):
+    @create_component_telemetry_wrapper(task_type)
+    def runnerComponent():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(utils._make_arg('training_data'), type=str)
+        parser.add_argument(utils._make_arg('validation_data'), type=str)
+        parser.add_argument(utils._make_arg('model_output'), type=str)
+        args, _ = parser.parse_known_args()
+        args_dict = vars(args)
+
+        mltable_data_dict = {
+            MLTableDataLabel.TrainData.value: {
+                MLTableLiterals.MLTABLE_RESOLVEDURI: args_dict['training_data']
+            }
+        }
+
+        if args_dict['validation_data']:
+            mltable_data_dict[MLTableDataLabel.ValidData.value] = {
+                MLTableLiterals.MLTABLE_RESOLVEDURI: args_dict['validation_data']
+            }
+
+        mltable_data_json = json.dumps(mltable_data_dict)
+
+        settings = {SettingsLiterals.TASK_TYPE: task_type}
+        runner.run(settings, mltable_data_json=mltable_data_json)
+
+        run = Run.get_context()
+        run.download_files(prefix='outputs/mlflow-model', output_directory=args_dict['model_output'], append_prefix=False)
+    
+    runnerComponent()
 
 
-@create_component_telemetry_wrapper(Tasks.IMAGE_CLASSIFICATION)
 def run():
     parser = argparse.ArgumentParser()
-    parser.add_argument(utils._make_arg('training_data'), type=str)
-    parser.add_argument(utils._make_arg('validation_data'), type=str)
+    parser.add_argument(utils._make_arg('multilabel'), type=bool)
     args, _ = parser.parse_known_args()
     args_dict = vars(args)
 
-    mltable_data_dict = {
-        MLTableDataLabel.TrainData.value: {
-            MLTableLiterals.MLTABLE_RESOLVEDURI: args_dict['training_data']
-        }
-    }
+    task_type= Tasks.IMAGE_CLASSIFICATION
 
-    if args_dict['validation_data']:
-        mltable_data_dict[MLTableDataLabel.ValidData.value] = {
-            MLTableLiterals.MLTABLE_RESOLVEDURI: args_dict['validation_data']
-        }
+    if args_dict['multilabel']== True:
+        task_type = Tasks.IMAGE_CLASSIFICATION_MULTILABEL
 
-    mltable_data_json = json.dumps(mltable_data_dict)
-
-    settings = {SettingsLiterals.TASK_TYPE: Tasks.IMAGE_CLASSIFICATION}
-    runner.run(settings, mltable_data_json=mltable_data_json)
-
+    runClassificationComponent(task_type)
 
 if __name__ == "__main__":
     run()
