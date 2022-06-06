a978bf5daa2244c9f1ca46835fac9642986c53fb
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-06-06 10:54:48 -0700

    sync for device switching

--- scripts/azureml-assets/azureml/assets/release/test_file_convert.py
+++ scripts/azureml-assets/azureml/assets/release/test_file_convert.py
@@ -2,3 +2,15 @@ from pathlib import Path
 import os
 import sys
 import ruamel.yaml
+import shutil
+
+tests_folder_dir = sys.argv[1]
+
+src_dir = Path("../../../../../vision/src")
+
+yaml = ruamel.yaml.YAML()
+with open(src_dir.__str__()+"/tests.yml") as fp:
+    data = yaml.load(fp)
+    for job in data:
+        test_file_dir = src_dir.__str__()+job["path"]
+        shutil.copyfile(test_file_dir, tests_folder_dir.__str__()+"/"+job)
