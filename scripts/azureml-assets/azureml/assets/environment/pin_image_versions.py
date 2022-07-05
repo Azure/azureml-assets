47c9ac737fc7d41a6575ffcc8f2becb547e88919
Louie Larson <lolarson@microsoft.com>
2022-07-05 12:24:29 -0400

    Allow image tag regex

--- scripts/azureml-assets/azureml/assets/environment/pin_image_versions.py
+++ scripts/azureml-assets/azureml/assets/environment/pin_image_versions.py
@@ -3,42 +3,28 @@ import json
 import re
 import urllib.parse
 from pathlib import Path
+from typing import List, Tuple
 from urllib.request import Request, urlopen
 
 from azureml.assets.util import logger
 
 LATEST_TAG = "latest"
-LATEST_IMAGE_TAG = re.compile(r"([^\"'\s]+):\{\{latest-image-tag\}\}")
+# Handles registry/image_name:{{latest-image-tag}} and registry/image_name:{{latest-image-tag:regex}}
+LATEST_IMAGE_TAG = re.compile(r"([^\"'\s]+):\{\{latest-image-tag(?::(.+))?\}\}")
 
 
-def get_latest_image_suffix(image: str):
+def get_latest_tag_or_digest(image: str, tags: List[str]) -> Tuple[str, str]:
     (hostname, repo) = image.split("/", 1)
 
-    # Retrieve tags
-    try:
-        response = urlopen(f"https://{hostname}/v2/{repo}/tags/list")
-    except Exception as e:
-        raise Exception(f"Failed to retrieve tags for {repo}: {e}")
-    tags = json.loads(response.read().decode("utf-8")).get("tags", [])
-
-    # Ensure latest is present
-    if LATEST_TAG not in tags:
-        raise Exception(f"{image} does not have a {LATEST_TAG} tag")
-
-    # Sort tags in descending order because this should be faster,
-    # and move latest to the beginning to ensure we get its digest first
-    tags_sorted = sorted([t for t in tags if t != LATEST_TAG], reverse=True)
-    tags_sorted.insert(0, LATEST_TAG)
-
     # Find another tag corresponding to latest
-    latest_digest = None
     latest_tag = None
-    for tag in tags_sorted:
+    latest_digest = None
+    for tag in tags:
         # Retrieve digest
         encoded_tag = urllib.parse.quote(tag, safe="")
         request = Request(f"https://{hostname}/v2/{repo}/manifests/{encoded_tag}",
-                          method="HEAD",
-                          headers={'Accept': "application/vnd.docker.distribution.manifest.v2+json"})
+                        method="HEAD",
+                        headers={'Accept': "application/vnd.docker.distribution.manifest.v2+json"})
         try:
             response = urlopen(request)
         except Exception as e:
@@ -52,6 +38,41 @@ def get_latest_image_suffix(image: str):
             # Found matching digest
             latest_tag = tag
             break
+
+    return latest_tag, latest_digest
+
+
+def get_latest_image_suffix(image: str, regex: re.Pattern = None) -> str:
+    (hostname, repo) = image.split("/", 1)
+
+    # Retrieve tags
+    try:
+        response = urlopen(f"https://{hostname}/v2/{repo}/tags/list")
+    except Exception as e:
+        raise Exception(f"Failed to retrieve tags for {repo}: {e}")
+    tags = json.loads(response.read().decode("utf-8")).get("tags", [])
+
+    # Filter tags and sort in descending order because this should be faster
+    tags_sorted = sorted([t for t in tags if t != LATEST_TAG and
+                         (regex is None or regex.search(t) is not None)], reverse=True)
+    
+    # Handle regex
+    if regex is not None:
+        # Use the most recent matching tag
+        if tags_sorted:
+            latest_tag = tags_sorted[0]
+        else:
+            raise Exception(f"{image} does not have tag that matches {regex}")
+    else:
+        # Ensure latest is present
+        if LATEST_TAG not in tags:
+            raise Exception(f"{image} does not have a {LATEST_TAG} tag")
+
+        # Insert latest at the beginning to ensure we get its digest first
+        tags_sorted.insert(0, LATEST_TAG)
+
+        # Find another tag corresponding to latest, or default to digest
+        latest_tag, latest_digest = get_latest_tag_or_digest(image, tags)
 
     # Return tag or digest
     if latest_tag is not None:
@@ -68,8 +89,13 @@ def pin_images(contents: str) -> str:
         if not match:
             break
         repo = match.group(1)
-        logger.log_debug(f"Finding latest image tag/digest for {repo}")
-        suffix = get_latest_image_suffix(repo)
+        regex = match.group(2)
+        message = f"Finding latest image tag/digest for {repo}"        
+        if regex is not None:
+            message += f" matching {regex}"
+            regex = re.compile(regex)
+        logger.log_debug(message)
+        suffix = get_latest_image_suffix(repo, regex)
         logger.log_debug(f"Latest image reference is {repo}{suffix}")
         contents = contents[:match.start()] + f"{repo}{suffix}" + contents[match.end():]
 
