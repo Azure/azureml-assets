6b532d2f64da9b3185615c4980adea664c92fa2c
Komnus丶Q <40655746+quchuyuan@users.noreply.github.com>
2022-07-07 15:31:34 -0700

    Update scripts/azureml-assets/azureml/assets/release/test_file_convert.py
    Co-authored-by: Louie Larson <lolarson@microsoft.com>

--- scripts/azureml-assets/azureml/assets/release/test_file_convert.py
+++ scripts/azureml-assets/azureml/assets/release/test_file_convert.py
@@ -27,16 +27,12 @@ shutil.copy(src_dir / yaml_name , tests_folder_dir / yaml_name)
 with open(src_dir/ yaml_name) as fp:
     data = yaml.load(fp, Loader=yaml.FullLoader)
     for test_group in data:
-        if data[test_group].get('includes') != None:
-            for include_file in data[test_group].get('includes'):
-                if (src_dir / include_file).is_dir():
-                    print("copying folder: "+include_file)
-                    copy_replace_dir((src_dir / include_file), (tests_folder_dir / include_file))
-                else:
-                    print("copying file: "+include_file)
-                    Path.mkdir(tests_folder_dir / Path(include_file).parent, parents=True, exist_ok=True)
-                    shutil.copy((src_dir / include_file), (tests_folder_dir / include_file))
-
-
-            
-        
+        for include_file in data[test_group].get('includes', []):
+            target_path = tests_folder_dir / include_file
+            if (src_dir / include_file).is_dir():
+                print(f"copying folder: {include_file}")
+                copy_replace_dir(src_dir / include_file, target_path )
+            else:
+                print(f"copying file: {include_file}")
+                Path.mkdir(target_path .parent, parents=True, exist_ok=True)
+                shutil.copy(src_dir / include_file, target_path )
