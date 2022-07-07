9cca65a11b3c43b7251f39a241338ee0f1f0ca11
Erica DeBarge <ericadebarge@microsoft.com>
2022-07-07 14:06:22 -0400

    test environment variables

--- training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/test/example_test.py
+++ training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/test/example_test.py
@@ -1,2 +1,12 @@
+from azure.ai.ml import MLClient
+from azure.ai.ml import command, MpiDistribution
+from azure.identity import DefaultAzureCredential
+
 def test_that_tests_run():
-	assert True
+	subscription_id = workspace_sub_id
+	resource_group = workspace_resource_group
+	workspace = workspace_name
+	ml_client = MLClient(
+		DefaultAzureCredential(), subscription_id, resource_group, workspace
+	)
+	assert ml_client.workspace_name == "registry-built-in-assets-test-ws"
