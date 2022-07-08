f424b8525acd503895c0edb8a78f9ba6195414cb
Jeff Omhover <jf.omhover@gmail.com>
2022-07-08 11:44:46 -0700

    refactor folders

--- benchmark/src/scripts/pytorch_benchmark/image_classifier.py
+++ benchmark/src/scripts/pytorch_benchmark/image_classifier.py
@@ -47,8 +47,8 @@ if SCRIPTS_ROOT not in sys.path:
 # internal imports
 from common.profiling import LogTimeBlock, LogDiskIOBlock # non-pt-specific helper code
 from pytorch_benchmark.helper.profiling import PyTorchProfilerHandler # pt profiler code
-from pytorch_benchmark.helper.classification.model import get_model_metadata, load_model
-from pytorch_benchmark.helper.classification.io import build_image_datasets
+from pytorch_benchmark.classification.model import get_model_metadata, load_model
+from pytorch_benchmark.classification.io import build_image_datasets
 from pytorch_benchmark.helper.training import PyTorchDistributedModelTrainingSequence
 
 
