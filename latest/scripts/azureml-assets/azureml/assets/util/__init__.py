013c576f8a25b2812697fdfdb319a6ac9c4a29fb
assets-release <>
2022-07-12 21:17:49 +0000

    Release https://github.com/Azure/azureml-assets/actions/runs/2659449124

---
+++ latest/scripts/azureml-assets/azureml/assets/util/__init__.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from .logger import logger
+from .template import render
+from .util import (
+    apply_tag_template,
+    apply_version_template,
+    are_dir_trees_equal,
+    copy_asset_to_output_dir,
+    copy_replace_dir,
+    find_asset_config_files,
+    find_assets,
+    find_common_directory,
+    get_asset_output_dir,
+    get_asset_release_dir,
+)
