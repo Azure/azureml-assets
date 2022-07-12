b36bc0fdf4f7ec602a941072fdab7240fa0f7ed8
Erica DeBarge <ericadebarge@microsoft.com>
2022-07-12 16:38:43 -0400

    change environment to azureaiml version

--- training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/tests/sample_model_test.py
+++ training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/tests/sample_model_test.py
@@ -6,12 +6,14 @@ import os
 from pathlib import Path
 from azure.ai.ml import MLClient
 from azure.ai.ml import command, Input
+from azure.ai.ml.entities import Environment, BuildContext
 from azure.identity import DefaultAzureCredential
 
-from azureml.core import Environment, Workspace
+# from azureml.core import Environment, Workspace
 
-DOCKERFILE = Path("../context/Dockerfile")
-CONDA_SPEC = Path("../context/conda_dependencies.yaml")
+# DOCKERFILE = Path("../context/Dockerfile")
+# CONDA_SPEC = Path("../context/conda_dependencies.yaml")
+BUILD_CONTEXT = Path("../context")
 
 
 def test_sklearn_1_1():
@@ -25,9 +27,18 @@ def test_sklearn_1_1():
         DefaultAzureCredential(), subscription_id, resource_group, workspace_name
     )
 
-    env = Environment.from_dockerfile("sklearn1_1", this_dir / DOCKERFILE, this_dir / CONDA_SPEC)
-    ws = Workspace.get(name=workspace_name, subscription_id=subscription_id, resource_group=resource_group)
-    env = env.register(ws)
+    # env = Environment.from_dockerfile("sklearn1_1", this_dir / DOCKERFILE, this_dir / CONDA_SPEC)
+    # ws = Workspace.get(name=workspace_name, subscription_id=subscription_id, resource_group=resource_group)
+    # env = env.register(ws)
+
+    env_name = "sklearn1_1"
+
+    env_docker_context = Environment(
+        build=BuildContext(path=this_dir / BUILD_CONTEXT),
+        name="sklearn1_1",
+        description="Sklearn 1.1 environment created from a Docker context.",
+    )
+    ml_client.environments.create_or_update(env_docker_context)
 
     # create the command
     job = command(
@@ -39,7 +50,7 @@ def test_sklearn_1_1():
                 path="https://azuremlexamples.blob.core.windows.net/datasets/diabetes.csv",
             )
         },
-        environment=env,
+        environment=env_name,
         compute=os.environ.get("cpu_cluster"),
         display_name="sklearn-diabetes-example",
         # description,
