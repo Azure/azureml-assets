aa91bda3899ff8c2dbdfe1aee3b641ef92318d07
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-07-12 10:40:08 -0700

    resolved comments

--- scripts/azureml-assets/azureml/assets/release/asset_publish.py
+++ scripts/azureml-assets/azureml/assets/release/asset_publish.py
@@ -5,8 +5,8 @@ from subprocess import check_call
 import argparse
 from pathlib import Path
 import yaml
-import azureml.assets as assets
-import azureml.assets.util as util
+import assets
+import assets.util as util
 TEST_YML = "tests.yml"
 PROD_REGISTRY_NAME = "azureml"
 def test_files_location(dir: Path):
