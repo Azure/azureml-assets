61a4861d124edd6775407fc2b532001efd9416fd
Erica DeBarge <ericadebarge@microsoft.com>
2022-07-13 09:54:11 -0400

    change back to default credential

--- training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/tests/sample_model_test.py
+++ training/general/src/environments/sklearn-1.1-ubuntu20.04-py38-cpu/tests/sample_model_test.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from azure.ai.ml import MLClient
 from azure.ai.ml import command, Input
 from azure.ai.ml.entities import Environment, BuildContext
-from azure.identity import InteractiveBrowserCredential
+from azure.identity import DefaultAzureCredential
 
 # from azureml.core import Environment, Workspace
 
@@ -24,7 +24,7 @@ def test_sklearn_1_1():
     workspace_name = os.environ.get("workspace")
 
     ml_client = MLClient(
-        InteractiveBrowserCredential(), subscription_id, resource_group, workspace_name
+        DefaultAzureCredential(), subscription_id, resource_group, workspace_name
     )
 
     # env = Environment.from_dockerfile("sklearn1_1", this_dir / DOCKERFILE, this_dir / CONDA_SPEC)
