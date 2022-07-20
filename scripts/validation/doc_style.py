c97a99255bafe4679d2aa6e98ef5927fb5c2daef
Louie Larson <lolarson@microsoft.com>
2022-07-20 14:45:32 -0400

    Add more code health checks (#146)
    * Start checking code documentation
    * Allow pydocstyle validation, enforce some line lengths
    * Remove deprecated scripts

--- scripts/validation/doc_style.py
+++ scripts/validation/doc_style.py
@@ -4,77 +4,54 @@
 
 import argparse
 import json
+import re
 import sys
 from pathlib import Path
 from subprocess import run, PIPE, STDOUT
-from typing import Dict, List, Set, Tuple
+from typing import List, Set
 
 RULES_FILENAME = "validation_rules.json"
+FILE_NAME_PATTERN = re.compile(r"^(.+):\d+\s+")
 
 
 class Rules:
-    ROOT_KEY = "pep8"
+    ROOT_KEY = "doc"
     IGNORE = "ignore"
-    IGNORE_FILE = "ignore-file"
     EXCLUDE = "exclude"
-    MAX_LINE_LENGTH = "max-line-length"
-    DEFAULT_MAX_LINE_LENGTH = 10000
+    FORCE = "force"
 
     def __init__(self, file_name: Path = None):
         if file_name is not None and file_name.exists():
-            parent_path = file_name.parent
             # Load rules from file
             with open(file_name) as f:
                 rules = json.load(f).get(self.ROOT_KEY, {})
                 self.ignore = set(rules.get(self.IGNORE, []))
-                self.ignore_file = self._parse_ignore_file(parent_path, rules.get(self.IGNORE_FILE, []))
-                self.exclude = {parent_path / p for p in rules.get(self.EXCLUDE, [])}
-                self.max_line_length = rules.get(self.MAX_LINE_LENGTH)
+                self.exclude = {file_name.parent / p for p in rules.get(self.EXCLUDE, [])}
+                self.force = set(rules.get(self.FORCE, []))
         else:
             # Initialize empty
             self.ignore = set()
-            self.ignore_file = {}
             self.exclude = set()
-            self.max_line_length = None
-
-    @staticmethod
-    def _parse_ignore_file(parent_path: Path, ignore_file: List[str]) -> Dict[str, Set[str]]:
-        results = {}
-        for pair in ignore_file:
-            file, file_rules = pair.split(":")
-            file = parent_path / file
-            file_rules = {r.strip() for r in file_rules.split(",") if not r.isspace()}
-            results[file] = file_rules
-        return results
-
-    def get_effective_max_line_length(self) -> int:
-        return self.max_line_length or self.DEFAULT_MAX_LINE_LENGTH
+            self.force = set()
 
     def __or__(self, other: "Rules") -> "Rules":
         rules = Rules()
         rules.ignore = self.ignore | other.ignore
-        for key in self.ignore_file.keys() | other.ignore_file.keys():
-            rules.ignore_file[key] = self.ignore_file.get(key, set()) | other.ignore_file.get(key, set())
         rules.exclude = self.exclude | other.exclude
-        rules.max_line_length = other.max_line_length or self.max_line_length
+        rules.force = self.force | other.force
         return rules
 
 
-def run_flake8(testpath: Path, rules: Rules) -> Tuple[int, str]:
+def run_docstyle(testpath: Path, rules: Rules):
     cmd = [
-        "flake8",
-        f"--max-line-length={rules.get_effective_max_line_length()}",
+        "pydocstyle",
+        r"--match=.*\.py",
         str(testpath)
     ]
-    if rules.exclude:
-        cmd.insert(1, "--exclude={}".format(",".join([str(e) for e in rules.exclude])))
-    if rules.ignore_file:
-        file_ignore_list = []
-        for file, ignores in rules.ignore_file.items():
-            file_ignore_list.extend([f"{file}:{i}" for i in ignores])
-        cmd.insert(1, "--per-file-ignores={}".format(",".join(file_ignore_list)))
-    if rules.ignore:
-        cmd.insert(1, "--ignore={}".format(",".join(rules.ignore)))
+
+    ignore = rules.ignore - rules.force
+    if ignore:
+        cmd.insert(1, "--ignore={}".format(",".join(ignore)))
 
     print(f"Running {cmd}")
     p = run(cmd,
@@ -84,17 +61,39 @@ def run_flake8(testpath: Path, rules: Rules) -> Tuple[int, str]:
     return p.stdout.decode()
 
 
+def filter_docstyle_output(output: str, rules: Rules) -> List[str]:
+    lines = [line for line in output.splitlines() if len(line) > 0]
+    if lines:
+        filtered_lines = []
+        for i in range(0, len(lines) - 1, 2):
+            line = lines[i]
+            match = FILE_NAME_PATTERN.match(line)
+            if not match:
+                raise Exception(f"Unable to extract filename from {line}")
+            file = Path(match.group(1))
+            file_is_excluded = False
+            for exclude in rules.exclude:
+                if file == exclude or (exclude.is_dir() and file.is_relative_to(exclude)):
+                    file_is_excluded = True
+                    break
+            if not file_is_excluded:
+                filtered_lines.append(line)
+                filtered_lines.append(lines[i + 1])
+        lines = filtered_lines
+
+    return lines
+
+
 def inherit_rules(rootpath: Path, testpath: Path) -> Rules:
-    # Process paths from rootpath to testpath, to ensure max_line_length is calculated properly
-    paths = [p for p in testpath.parents if p == rootpath or p.is_relative_to(rootpath)]
-    paths.reverse()
     rules = Rules()
-    for path in paths:
-        rules |= Rules(path / RULES_FILENAME)
+    upperpath = testpath
+    while upperpath != rootpath:
+        upperpath = upperpath.parent
+        rules |= Rules(upperpath / RULES_FILENAME)
     return rules
 
 
-def test(rootpath: Path, testpath: Path) -> bool:
+def test(rootpath: Path, testpath: Path, force: Set[str] = {}) -> bool:
     testpath_rules = inherit_rules(rootpath, testpath)
 
     rules_files = list(testpath.rglob(RULES_FILENAME))
@@ -107,11 +106,15 @@ def test(rootpath: Path, testpath: Path) -> bool:
     for path in dirs:
         rules = Rules()
         rules.exclude = {d for d in dirs if d != path and not path.is_relative_to(d)}
+        rules.force = force
         rules |= testpath_rules | inherit_rules(testpath, path) | Rules(path / RULES_FILENAME)
-        errors.extend([line for line in run_flake8(path, rules).splitlines() if len(line) > 0])
+
+        output = run_docstyle(path, rules)
+        filtered_output = filter_docstyle_output(output, rules)
+        errors.extend(filtered_output)
 
     if len(errors) > 0:
-        print("flake8 errors:")
+        print("pydocstyle errors:")
         for line in errors:
             print(line)
         return False
@@ -124,7 +127,9 @@ if __name__ == '__main__':
     parser.add_argument("-i", "--input-directory", required=True, type=Path,
                         help="Directory to validate")
     parser.add_argument("-r", "--root-directory", type=Path,
-                        help="Root directory containing flake8 rules, must be a parent of --input-directory")
+                        help="Root directory containing docstyle rules, must be a parent of --input-directory")
+    parser.add_argument("-f", "--force", default="",
+                        help="Comma-separated list of rules that can't be ignored")
     args = parser.parse_args()
 
     # Handle directories
@@ -135,7 +140,10 @@ if __name__ == '__main__':
     elif not input_directory.is_relative_to(root_directory):
         parser.error(f"{root_directory} is not a parent directory of {input_directory}")
 
-    success = test(root_directory, input_directory)
+    # Parse forced rules
+    force = {r.strip() for r in args.force.split(",") if not r.isspace()}
+
+    success = test(root_directory, input_directory, force)
 
     if not success:
         sys.exit(1)
