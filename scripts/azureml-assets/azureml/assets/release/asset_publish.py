4711f6c32756f51feb80cf39611e46b0142cb877
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-07-25 17:34:19 -0700

    added test coverage

--- scripts/azureml-assets/azureml/assets/release/asset_publish.py
+++ scripts/azureml-assets/azureml/assets/release/asset_publish.py
@@ -12,7 +12,6 @@ from string import Template
 ASSET_ID_TEMPLATE = Template(
     "azureml://registries/$registry_name/$asset_type/$asset_name/versions/$version")
 TEST_YML = "tests.yml"
-PROD_REGISTRY_NAME = "azureml"
 
 
 def test_files_location(dir: Path):
@@ -63,17 +62,23 @@ if __name__ == '__main__':
         type=str,
         help="the resource group name")
     parser.add_argument(
+        "-s",
+        "--subscription-id",
+        required=True,
+        type=str,
+        help="the subscription-id")
+    parser.add_argument(
         "-w",
         "--workspace",
         required=True,
         type=str,
         help="the workspace name")
     parser.add_argument(
-        "-c",
-        "--component-directory",
+        "-a",
+        "--assets-directory",
         required=True,
         type=Path,
-        help="the component directory")
+        help="the assets directory")
     parser.add_argument(
         "-t",
         "--tests-directory",
@@ -81,64 +86,66 @@ if __name__ == '__main__':
         type=Path,
         help="the tests directory")
     parser.add_argument(
-        "-v",
-        "--version",
+        "--version-suffix",
         required=False,
         type=str,
-        help="the version")
+        help="the version suffix")
     parser.add_argument(
         "-l",
-        "--whitelist",
+        "--publish-list",
         required=False,
         type=Path,
-        help="the path of the whitelist file")
+        help="the path of the publish list file")
     args = parser.parse_args()
     registry_name = args.registry_name
+    subscription_id = args.subscription_id
     resource_group = args.resource_group
     workspace = args.workspace
     tests_dir = args.tests_directory
-    component_dir = args.component_directory
-    passed_version = args.version
-    whitelist_dir = args.whitelist
-    whitelist = []
+    component_dir = args.assets_directory
+    passed_version = args.version_suffix
+    publish_list_dir = args.publish_list
+    publish_list = {}
     asset_ids = {}
     print("publishing assets")
-    if registry_name == PROD_REGISTRY_NAME and not args.whitelist:
-        print("No whitelist file provided for production registry")
-        exit(1)
-    if registry_name == PROD_REGISTRY_NAME:
-        with open(whitelist_dir) as fp:
-            data = yaml.load(fp, Loader=yaml.FullLoader)
-            whitelist = [a for a in data]
 
-    asset_version_with_build_id = registry_name + "." + passed_version
-    print(f"generated componentVersionWithBuildId: {asset_version_with_build_id}")
+    if args.publish_list:
+        with open(publish_list_dir) as fp:
+            data = yaml.load(fp, Loader=yaml.FullLoader)
+            for asset_type in data:
+                publish_list[asset_type] = data.get(asset_type,[])
+
     assets_set = util.find_assets(
         input_dirs=component_dir,
         asset_config_filename=assets.DEFAULT_ASSET_FILENAME)
     for asset in assets_set:
-        if registry_name == PROD_REGISTRY_NAME and asset.name not in whitelist:
+        if '*' not in publish_list.get(asset.type.value, []) and asset.name not in publish_list.get(asset.type.value, []):
             print(
-                f"Skipping registering asset {asset.name} because it is not in the whitelist")
+                f"Skipping registering asset {asset.name} because it is not in the publish list")
             continue
         print(f"Registering {asset.name}")
         final_version = asset.version
         spec_path = asset.spec_with_path
-        if registry_name != PROD_REGISTRY_NAME:
-            final_version = final_version + '-' + asset_version_with_build_id
+        if args.version_suffix:
+            final_version = final_version + passed_version
         print(f"final version: {final_version}")
         asset_ids[asset.name] = ASSET_ID_TEMPLATE.substitute(registry_name=registry_name,
                                                              asset_type=f"{asset.type.value}s",
                                                              asset_name=asset.name,
                                                              version=final_version)
-        cmd = f"az ml component create --file {spec_path} --registry-name {registry_name} " \
+        # switch case for asset type
+        if asset.type == assets.AssetType.COMPONENT:
+            cmd = f"az ml component create --subscription{subscription_id} --file {spec_path} --registry-name {registry_name} " \
                 f"--version {final_version} --workspace {workspace} --resource-group {resource_group}"
-        print(cmd)
-        try:
-            check_call(cmd, shell=True)
-        except Exception as ex:
-            print(
-                f"catch error creating {asset.type}: {asset.name} with exception {ex}")
+            print(cmd)
+            try:
+                check_call(cmd, shell=True)
+            except Exception as ex:
+                print(
+                    f"catch error creating {asset.type.value}: {asset.name} with exception {ex}")
+        # TO-DO: add other asset types
+        else:
+            print(f"unsupported asset type: {asset.type.value}")
     print('All assets published')
 
     print('starting locating test files')
