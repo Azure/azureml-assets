f7d8171e10f25bdbc988340e053253aa5c4f155d
Erica DeBarge <ericadebarge@microsoft.com>
2022-07-29 14:29:41 -0400

    increase timeout

--- training/general/src/environments/pytorch-1.11-ubuntu20.04-py38-cuda11-gpu/tests/pytorch1_11_sample_test.py
+++ training/general/src/environments/pytorch-1.11-ubuntu20.04-py38-cuda11-gpu/tests/pytorch1_11_sample_test.py
@@ -58,7 +58,7 @@ def test_pytorch_1_11():
 
     polling.poll(
         lambda: returned_job.status == "Completed",
-        timeout=600,  # 10 minute timeout
+        timeout=1200,  # 10 minute timeout
         step=30       # poll every 30 seconds
     )
 
