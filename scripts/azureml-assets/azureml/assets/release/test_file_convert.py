e0d9a9bbfb147c6952df5273bf4c61cc4d34478e
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-08-17 11:54:32 -0700

    fixing scripts

--- scripts/azureml-assets/azureml/assets/release/test_file_convert.py
+++ scripts/azureml-assets/azureml/assets/release/test_file_convert.py
@@ -99,6 +99,7 @@ if __name__ == '__main__':
 
     input_dirs = [Path(d) for d in args.input_dirs.split(",")]
     src_dirs, exclude_dirs = _convert_excludes(input_dirs)
+    print(f"exclude_dirs: {exclude_dirs}")
     for src_dir in src_dirs:
         if src_dir in exclude_dirs:
             continue
