7aa531ed037ebde3be5ba285c491a52741fda90f
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-08-31 16:39:51 -0700

    added version list

--- scripts/azureml-assets/azureml/assets/release/asset_delete.py
+++ scripts/azureml-assets/azureml/assets/release/asset_delete.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 """Python script to publish assets."""
-from subprocess import check_call
 import argparse
 from pathlib import Path
 import yaml
@@ -14,29 +13,59 @@ from collections import defaultdict
 ASSET_ID_TEMPLATE = Template(
     "azureml://registries/$registry_name/$asset_type/$asset_name/versions/$version")
 
-def delete_single_version(url: str, asset_id: str, token: str):
+VERSION_CHECK_TEMPLATE = Template("$base_url/mferp/managementfrontend/subscriptions/$subscription/resourceGroups/$resource_group/providers/Microsoft.MachineLearningServices/registries/$registry/$asset_type/$asset_name/versions?api-version=2021-10-01-dataplanepreview")
+    
+def construct_asset_id(asset_name: str, asset_version: str, asset_type: str, registry_name: str):
+    return ASSET_ID_TEMPLATE.substitute(
+        registry_name=registry_name,
+        asset_type=f'{asset_type}s',
+        asset_name=asset_name,
+        version=asset_version)
+
+def delete_single_version(base_url: str, asset_id: str, token: str):
+    target_url = f'{base_url}/assetstore/v1.0/delete'
     payload=json.dumps({"id": asset_id }) # "azureml://registries/azureml-dev/components/pytorch_image_classifier/versions/1.0.4"
     headers={'Authorization': f'Bearer {token}',  'Content-Type': 'application/json'}
-    response=requests.request("DELETE", url, headers=headers, data=payload)
+    response=requests.request("DELETE", target_url, headers=headers, data=payload)
     print(response.text)
 
 
-def delete_all_versions(url: str, asset_id: str, token: str):
+def delete_all_versions(base_url: str, asset_name: str, asset_type: str, token: str, subscription: str, resource_group: str, registry: str):
+    version_list = list_all_version(base_url, asset_name, asset_type, token, subscription, resource_group, registry)
+    for version in version_list[1:]:
+        asset_id = construct_asset_id(asset_name, version, asset_type, registry)
+        delete_single_version(base_url, asset_id, token)
+    default_version = version_list[0]
+    asset_id = construct_asset_id(asset_name, default_version, asset_type, registry)
+    delete_single_version(base_url, asset_id, token)
 
+def check_latest_version(base_url: str, asset_name: str, asset_version: str, asset_type: str, token: str, subscription: str, resource_group: str, registry: str):
+    version_list = list_all_version(base_url, asset_name, asset_type, token, subscription, resource_group, registry)
+    print(f'version_list: {version_list}')
+    print(len(version_list)>0 and asset_version == version_list[0])
+    return len(version_list)>0 and asset_version == version_list[0]
+
+def list_all_version(base_url: str, asset_name: str, asset_type: str, token: str, subscription: str, resource_group: str, registry: str):
+    target_url = VERSION_CHECK_TEMPLATE.substitute(base_url=base_url, subscription=subscription, resource_group=resource_group, registry=registry, asset_type=f'{asset_type}s', asset_name=asset_name)
+    headers={'Authorization': f'Bearer {token}'}
+    response=requests.request("GET", target_url, headers=headers)
+    response_detail = response.json()
+    version_list = []
+    for version in response_detail['value']:
+        version_list.append(version['name'])
+    return version_list
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--deletion-list", required=True, type=Path, help="the path of deletion list file")
-    parser.add_argument("-u, --url", required=True, type=str, help="the url of the deletion api")
+    parser.add_argument("--url", required=True, type=str, help="the base url of the api")
     parser.add_argument("-r", "--registry-name", required=True, type=str, help="the registry name")
     parser.add_argument("-g", "--resource-group", required=True, type=str, help="the resource group name")
     parser.add_argument("-s", "--subscription-id", required=True, type=str, help="the subscription-id")
-    parser.add_argument("-w", "--workspace", required=True, type=str, help="the workspace name")
     parser.add_argument("-t", "--token", required=True, type=str, help="the Bearer token")
     args = parser.parse_args()
     subscription_id = args.subscription_id
     resource_group = args.resource_group
-    workspace = args.workspace
     registry_name = args.registry_name
     token = args.token
     url = args.url
@@ -45,17 +74,19 @@ if __name__ == '__main__':
         deletion_yaml = yaml.load(fp, Loader=yaml.FullLoader)
         for asset_type, asset_list in deletion_yaml.items():
              for asset in asset_list:
-                asset_name, asset_version = asset['name'], asset['version']
-                asset_id = ASSET_ID_TEMPLATE.substitute(registry_name=registry_name, asset_type=f'{asset_type}s', asset_name=asset_name, version=asset_version)
-                deletion_list[asset_type].append(asset_id)
+                deletion_list[asset_type].append({'name': asset['name'], 'version': asset['version']})
+    print(f'deletion_list: {deletion_list}')
 
     for asset_type in deletion_list:
         if asset_type == assets.AssetType.COMPONENT.value:
-            for asset_id in deletion_list[asset_type]:
-                if asset_id.endswith('*'):
-                    delete_all_versions(url, asset_id, token)
+            for asset in deletion_list[asset_type]:
+                if asset['version'] == '*':
+                    delete_all_versions(url, asset['name'], asset_type, token, subscription_id, resource_group, registry_name)
                 else:
-                    delete_single_version(url, asset_id, token)
+                    if not check_latest_version(url, asset['name'], asset['version'], asset_type, token, subscription_id, resource_group, registry_name):
+                        asset_id = construct_asset_id(asset['name'], asset['version'], asset_type, registry_name)
+                        print(f'delete asset: {asset_id}')
+                        delete_single_version(url, asset_id, token)
         
         # TO-DO: add other asset types
         else:
