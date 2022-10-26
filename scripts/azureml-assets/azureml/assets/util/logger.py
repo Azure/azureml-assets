3607081b44e7d8d36b75b8c74f52dd1297f3941a
Louie Larson <lolarson@microsoft.com>
2022-10-26 14:54:39 -0400

    Add documentation

--- scripts/azureml-assets/azureml/assets/util/logger.py
+++ scripts/azureml-assets/azureml/assets/util/logger.py
@@ -1,6 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+"""Provide unified logging across GitHub and Azure DevOps, with a fallback to console."""
+
 import os
 import threading
 
@@ -9,46 +11,113 @@ _output_lock = threading.Lock()
 
 
 class Logger:
+    """Base logger class."""
     def log_debug(self, message: str, title: str = None):
+        """Log a debug message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         pass
 
     def log_warning(self, message: str, title: str = None):
+        """Log a warning message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         pass
 
     def log_error(self, message: str, title: str = None):
+        """Log an error message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         pass
 
     def start_group(self, title: str):
+        """Start a collapsible group.
+
+        Args:
+            title (str): Title.
+        """
         pass
 
     def end_group(self):
+        """End a collapsible group."""
         pass
 
     def set_output(self, name: str, value: str):
+        """Set an output variable.
+
+        Args:
+            name (str): Name.
+            value (str): Value.
+        """
         pass
 
     def print(self, message: str):
+        """Print a message, blocking until complete.
+
+        Args:
+            message (str): Message.
+        """
         with _stdout_lock:
             print(message)
 
 
 class GitHubLogger(Logger):
+    """Logger running under a GitHub agent."""
     def log_debug(self, message: str, title: str = None):
+        """Log a debug message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("debug", message, title)
 
     def log_warning(self, message: str, title: str = None):
+        """Log a warning message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("warning", message, title)
 
     def log_error(self, message: str, title: str = None):
+        """Log an error message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("error", message, title)
 
     def start_group(self, title: str):
+        """Start a collapsible group.
+
+        Args:
+            title (str): Title.
+        """
         self.print(f"::group::{title}")
 
     def end_group(self):
+        """End a collapsible group."""
         self.print("::endgroup::")
 
     def set_output(self, name: str, value: str):
+        """Set an output variable.
+
+        Args:
+            name (str): Name.
+            value (str): Value.
+        """
         output_filename = os.environ["GITHUB_OUTPUT"]
         with _output_lock, open(output_filename, "a") as f:
             f.write(f"{name}={value}\n")
@@ -59,22 +128,53 @@ class GitHubLogger(Logger):
 
 
 class AzureDevOpsLogger(Logger):
+    """Logger running under an Azure DevOps agent."""
     def log_debug(self, message: str, title: str = None):
+        """Log a debug message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("debug", message, title)
 
     def log_warning(self, message: str, title: str = None):
+        """Log a warning message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("warning", message, title)
 
     def log_error(self, message: str, title: str = None):
+        """Log an error message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Defaults to None.
+        """
         self._log("error", message, title)
 
     def start_group(self, title: str):
+        """Start a collapsible group.
+
+        Args:
+            title (str): Title.
+        """
         self.print(f"##[group]{title}")
 
     def end_group(self):
+        """End a collapsible group."""
         self.print("##[endgroup]")
 
     def set_output(self, name: str, value: str):
+        """Set an output variable.
+
+        Args:
+            name (str): Name.
+            value (str): Value.
+        """
         self.print(f"##vso[task.setvariable variable={name};isoutput=true]{value}")
 
     def _log(self, log_level: str, message: str, title: str = None):
@@ -87,21 +187,42 @@ class AzureDevOpsLogger(Logger):
 
 class ConsoleLogger(Logger):
     def log_debug(self, message: str, title: str = None):
+        """Log a debug message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Ignored by this logger.
+        """
         self._log("debug", message, title)
 
     def log_warning(self, message: str, title: str = None):
+        """Log a warning message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title.  Ignored by this logger.
+        """
         self._log("warning", message, title)
 
     def log_error(self, message: str, title: str = None):
+        """Log an error message.
+
+        Args:
+            message (str): Message.
+            title (str, optional): Message title. Ignored by this logger.
+        """
         self._log("error", message, title)
 
     def start_group(self, title: str):
+        """Ignored by this logger."""
         pass
 
     def end_group(self):
+        """Ignored by this logger."""
         pass
 
     def set_output(self, name: str, value: str):
+        """Ignored by this logger."""
         pass
 
     def _log(self, log_level: str, message: str, title: str = None):
