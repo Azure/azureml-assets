a18710023e6c2588b3966767dce4d3d340b897f9
Chuyuan Qu <chuyuanqu@microsoft.com>
2022-11-09 13:58:20 -0800

    update

--- scripts/azureml-assets/azureml/assets/release/e2e_test.py
+++ scripts/azureml-assets/azureml/assets/release/e2e_test.py
@@ -59,12 +59,11 @@ if __name__ == '__main__':
             for test_group in data:
                 print(f"now processing test group: {test_group}")
                 cmd = ["python3", "-u", "group_test.py", "-i", area, "-g", test_group, "-s", subscription_id,
-                       "-r", "resource_group", "-w", workspace]  # , "-t", token]
+                       "-r", resource_group, "-w", workspace]  # , "-t", token]
                 if coverage_report:
                     cmd.extend(["-c", coverage_report])
                 if version_suffix:
                     cmd.extend(["-v", version_suffix])
-                print(f"running command: {' '.join(cmd)}")
                 p = run(cmd, shell=True)
                 return_code = p.returncode
                 print(f"return code: {return_code}")
