64dec366473a88e06772aabacb132acd33f23e92
Cassie Esvelt <cesvelt@microsoft.com>
2022-12-12 14:40:01 -0800

    move data into higher folder

--- training/deepspeed/deepspeed-autotuning/train.py
+++ training/deepspeed/deepspeed-autotuning/train.py
@@ -55,10 +55,10 @@ print("Current tracking uri: {}".format(tracking_uri))
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=False, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root="../data", train=True, download=False, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root="./data", train=False, download=False, transform=transform)
+testset = torchvision.datasets.CIFAR10(root="../data", train=False, download=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
 
 classes = (
