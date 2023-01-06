259794e1c5a14f226fe4b0e2150991b1c24496cb
Savita Mittal <savitam@microsoft.com>
2023-01-05 17:17:04 -0800

    added pytorch 1.13

---
+++ training/general/src/environments/acpt-pytorch-1.13-py38-cuda11.7-gpu/tests/src/network.py
@@ -0,0 +1,28 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Base class for neural network module."""
+
+# imports
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# define network(s)
+class SimpleMLP(nn.Module):
+    """Base class for neural network module."""
+
+    def __init__(self):
+        """Initialize."""
+        super(SimpleMLP, self).__init__()
+        self.l1 = nn.Linear(4, 16)
+        self.l2 = nn.Linear(16, 16)
+        self.l3 = nn.Linear(16, 3)
+
+    def forward(self, x):
+        """Forward."""
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.softmax(self.l3(x), dim=1)
+
+        return x
