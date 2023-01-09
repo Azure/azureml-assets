3566b61b92e3d0c854acc04344a488d9970bbd09
Ayush Mishra <61145377+novaturient95@users.noreply.github.com>
2023-01-09 22:54:46 +0530

    fix flake

--- training/automl/tests/preprocessing/vision/__init__.py
+++ training/automl/tests/preprocessing/vision/__init__.py
@@ -2,8 +2,3 @@
 # Licensed under the MIT License.
 
 """Vision Test preprocessing init file."""
-
-from .prepare_classification_data import prepare_data as prepare_classification_data
-from .prepare_classification_multilabel_data import prepare_data as prepare_classification_multilabel_data
-from .prepare_instance_segmentation_data import prepare_data as prepare_instance_segmentation_data
-from .prepare_object_detection_data import prepare_data as prepare_object_detection_data
