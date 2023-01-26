4d07f57f6c3622421516514374e2f810521fd03d
tongy-msft <91754176+tongyu-microsoft@users.noreply.github.com>
2023-01-26 11:23:13 -0800

    Update rai_component_utilities.py

--- responsibleai/tabular/components/src/rai_component_utilities.py
+++ responsibleai/tabular/components/src/rai_component_utilities.py
@@ -97,7 +97,8 @@ def load_mlflow_model(
         except subprocess.CalledProcessError as e:
             _logger.error("Installing dependency using requriments.txt from mlflow model failed: {}".format(e.output))
             _classify_and_log_pip_install_error(e.output)
-            raise UserConfigError("Installing dependency using requirments.txt from mlflow model failed. "
+            raise UserConfigError(
+                "Installing dependency using requirments.txt from mlflow model failed. "
                 "This behavior can be turned off with setting use_model_dependency to False in job spec. "
                 "You may also check error log above to manually resolve package conflict error")
         _logger.info("Successfully installed model dependencies")
