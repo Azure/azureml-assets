b45d5e0b988b7c19012d3af6a7d3979c8e63ed95
tongy-msft <91754176+tongyu-microsoft@users.noreply.github.com>
2023-01-26 10:20:56 -0800

    Update rai_component_utilities.py

--- responsibleai/tabular/components/src/rai_component_utilities.py
+++ responsibleai/tabular/components/src/rai_component_utilities.py
@@ -42,6 +42,9 @@ _tool_directory_mapping: Dict[str, str] = {
     RAIToolType.EXPLANATION: "explainer",
 }
 
+class UserConfigError(Exception):
+    pass
+
 
 def print_dir_tree(base_dir):
     print("\nBEGIN DIRTREE")
@@ -73,18 +76,45 @@ def load_mlflow_model(
     model_path: Optional[str] = None,
 ) -> Any:
     model_uri = model_path
+    mlflow.set_tracking_uri(workspace.get_mlflow_tracking_uri())
+
     if model_id:
-        model = Model._get(workspace, id=model_id)
-        mlflow.set_tracking_uri(workspace.get_mlflow_tracking_uri())
+        try:
+            model = Model._get(workspace, id=model_id)
+        except Exception as e:
+            raise UserConfigError("Unable to retrieve model by model id {} in workspace {}, error:\n{}".format(model_id, workspace.name, e))
         model_uri = "models:/{}/{}".format(model.name, model.version)
 
     if use_model_dependency:
-        pip_file = mlflow.pyfunc.get_model_dependencies(model_uri)
-        subprocess.check_call([sys.executable, "-m", "pip",
-                               "install", "-r", pip_file])
+        try:
+            pip_file = mlflow.pyfunc.get_model_dependencies(model_uri)
+        except Exception as e:
+            raise ValueError("Failed to get model dependency from given model {}, error:\n{}".format(model_uri, e))
+        try:
+            subprocess.check_output([sys.executable, "-m", "pip",
+                                    "install", "-r", pip_file])
+        except subprocess.CalledProcessError as e:
+            _logger.error("Installing dependency using requriments.txt from mlflow model failed: {}".format(e.output))
+            _classify_and_log_pip_install_error(e.output)
+            raise UserConfigError("Installing dependency using requirments.txt from mlflow model failed. "
+            "This behavior can be turned off with setting use_model_dependency to False in job spec. "
+            "You may also check error log above to manually resolve package conflict error"
+            )
         _logger.info("Successfully installed model dependencies")
 
-    return mlflow.pyfunc.load_model(model_uri)._model_impl
+    try:
+        model = mlflow.pyfunc.load_model(model_uri)._model_impl
+        return model
+    except Exception as e:
+        raise ValueError("Unable to load mlflow model from {} in current environment due to error:\n{}".format(model_uri, e))
+
+
+def _classify_and_log_pip_install_error(elog):
+    if "Could not find a version that satisfies the requirement" in elog:
+        _logger.warn("Detected unsatisfiable version requirment.")
+
+    if "package versions have conflicting dependencies" in elog:
+        _logger.warn("Detected dependency conflict error.")
 
 
 def load_mltable(mltable_path: str) -> pd.DataFrame:
@@ -291,7 +321,7 @@ def create_rai_insights_from_port_path(my_run: Run, port_path: str) -> RAIInsigh
     use_model_dependency = input_args["use_model_dependency"]
     model_id = config[DashboardInfo.RAI_INSIGHTS_MODEL_ID_KEY]
     _logger.info("Loading model: {0}".format(model_id))
-
+    
     model_estimator = load_mlflow_model(
         workspace=my_run.experiment.workspace,
         use_model_dependency=use_model_dependency,
@@ -329,7 +359,7 @@ def get_dataset_name_version(run, dataset_input_name):
 
 
 def default_json_handler(data):
-    if isinstance(data, FeatureMetadata):
+    if  isinstance(data, FeatureMetadata): 
         meta_dict = data.__dict__
         type_name = type(data).__name__
         meta_dict[data_type] = type_name
