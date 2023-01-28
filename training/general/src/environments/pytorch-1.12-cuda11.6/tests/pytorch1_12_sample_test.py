f10d9df43c9243b333bfa6997eaa1da20a47c06c
Cassie Esvelt <cesvelt@microsoft.com>
2023-01-27 16:15:09 -0800

    rename envs

---
+++ training/general/src/environments/pytorch-1.12-cuda11.6/tests/pytorch1_12_sample_test.py
@@ -0,0 +1,69 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests running a sample job in the pytorch 1.12 environment."""
+import os
+import time
+from pathlib import Path
+from azure.ai.ml import command, Input, MLClient
+from azure.ai.ml.entities import Environment, BuildContext
+from azure.identity import AzureCliCredential
+
+BUILD_CONTEXT = Path("../context")
+JOB_SOURCE_CODE = "src"
+TIMEOUT_MINUTES = os.environ.get("timeout_minutes", 40)
+
+
+def test_pytorch_1_12():
+    """Tests a sample job using pytorch 1.12 as the environment."""
+    this_dir = Path(__file__).parent
+
+    subscription_id = os.environ.get("subscription_id")
+    resource_group = os.environ.get("resource_group")
+    workspace_name = os.environ.get("workspace")
+
+    ml_client = MLClient(
+        AzureCliCredential(), subscription_id, resource_group, workspace_name
+    )
+
+    env_name = "pytorch-1_12-cuda11_6"
+
+    env_docker_context = Environment(
+        build=BuildContext(path=this_dir / BUILD_CONTEXT),
+        name=env_name,
+        description="Pytorch 1.12 environment created from a Docker context.",
+    )
+    ml_client.environments.create_or_update(env_docker_context)
+
+    # create the command
+    job = command(
+        code=this_dir / JOB_SOURCE_CODE,  # local path where the code is stored
+        command="pip install -r requirements.txt && python main.py --iris-csv ${{inputs.iris_csv}} "
+                "--epochs ${{inputs.epochs}} --lr ${{inputs.lr}}",
+        inputs={
+            "iris_csv": Input(
+                type="uri_file",
+                path="https://azuremlexamples.blob.core.windows.net/datasets/iris.csv",
+            ),
+            "epochs": 10,
+            "lr": 0.1,
+        },
+        environment=f"{env_name}@latest",
+        compute=os.environ.get("gpu_cluster"),
+        display_name="pytorch-iris-example",
+        description="Train a neural network with PyTorch on the Iris dataset.",
+        experiment_name="pytorch112_Cuda116_Experiment"
+    )
+
+    returned_job = ml_client.create_or_update(job)
+    assert returned_job is not None
+
+    # Poll until final status is reached or timed out
+    timeout = time.time() + (TIMEOUT_MINUTES * 60)
+    while time.time() <= timeout:
+        current_status = ml_client.jobs.get(returned_job.name).status
+        if current_status in ["Completed", "Failed"]:
+            break
+        time.sleep(30)  # sleep 30 seconds
+
+    assert current_status == "Completed"
