16ba9b09e1e93350568d0c724099de1022469496
Louie Larson <lolarson@microsoft.com>
2023-01-30 10:29:35 -0500

    Create script to extract tagged assets

---
+++ scripts/azureml-assets/azureml/assets/extract_tagged_assets.py
@@ -0,0 +1,64 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import argparse
+import re
+from collections import defaultdict
+from git import Repo
+from pathlib import Path
+
+import azureml.assets as assets
+import azureml.assets.util as util
+from azureml.assets.util import logger
+
+
+def extract_tag_released_assets(asset_config_filename: str,
+                                release_directory_root: Path,
+                                output_directory_root: Path,
+                                pattern: str = None):
+    repo = Repo(release_directory_root)
+
+    # Select tags
+    commits_tags = defaultdict(list)
+    for tag in repo.tags:
+        if not pattern or re.fullmatch(pattern, tag.name):
+            commits_tags[tag.commit].append(tag)
+
+    # Checkout tags
+    for commit, tags in commits_tags.items():
+        logger.print(f"Checking out commit {commit} for {len(tags)} tag(s)")
+        repo.git().checkout(commit)
+
+        # Iterate over tags
+        for tag in tags:
+            # Copy asset to output directory
+            type, name, version = util.parse_release_tag(tag.name)
+            release_dir = util.get_asset_release_dir_from_parts(type, name, release_directory_root)
+            output_directory = util.get_asset_output_dir_from_parts(type, name, output_directory_root, version)
+            if release_dir.exists():
+                logger.print(f"Copying {type.value} {name}:{version} to {output_directory}")
+                util.copy_replace_dir(release_dir, output_directory)
+            else:
+                logger.log_warning(f"{type.value.capitalize()} {name}:{version} not found in commit")
+
+    # Reset to release branch
+    repo.git().checkout("release")
+
+
+if __name__ == "__main__":
+    # Handle command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--asset-config-filename", default=assets.DEFAULT_ASSET_FILENAME,
+                        help="Asset config file name to search for")
+    parser.add_argument("-r", "--release-directory", required=True, type=Path,
+                        help="Directory to which the release branch has been cloned")
+    parser.add_argument("-o", "--output-directory", required=True, type=Path,
+                        help="Directory to which unreleased assets will be written")
+    parser.add_argument("-t", "--pattern",
+                        help="Regex pattern to select assets to extract, in the format <type>/<name>/<version>")
+    args = parser.parse_args()
+
+    extract_tag_released_assets(asset_config_filename=args.asset_config_filename,
+                                release_directory_root=args.release_directory,
+                                output_directory_root=args.output_directory,
+                                pattern=args.pattern)
