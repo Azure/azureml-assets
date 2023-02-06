e6e10bc56137d1ccdb5f56f96cb3c2c209dde898
Ilya Matiach <ilmat@microsoft.com>
2023-02-06 11:37:39 -0500

    update to latest RAI vision and text components and environments

---
+++ responsibleai/vision/components/src/constants.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+
+class TaskType:
+    IMAGE_CLASSIFICATION = "image_classification"
+    MULTILABEL_IMAGE_CLASSIFICATION = "multilabel_image_classification"
