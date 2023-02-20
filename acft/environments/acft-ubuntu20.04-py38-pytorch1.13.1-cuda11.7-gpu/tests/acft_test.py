293b3cea6b30193849aef1296627d9321bdd8c89
SitaRam Chaitanya Kanakamedala <skanakamedal@microsoft.com>
2023-02-20 16:23:52 +0530

    Adding ACFT docker context

---
+++ acft/environments/acft-ubuntu20.04-py38-pytorch1.13.1-cuda11.7-gpu/tests/acft_test.py
@@ -0,0 +1,60 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests running a sample job in the ACFT environment."""
+
+import os
+import time
+from pathlib import Path
+from azure.ai.ml import command, Input, MLClient
+from azure.ai.ml.entities import Environment, BuildContext
+from azure.identity import AzureCliCredential
+
+BUILD_CONTEXT = Path("../context")
+JOB_SOURCE_CODE = "src"
+TIMEOUT_MINUTES = os.environ.get("timeout_minutes", 90)
+
+def test_acft():
+    """Tests a sample job usingACFT as the environment."""
+    this_dir = Path(__file__).parent
+
+    subscription_id = os.environ.get("subscription_id")
+    resource_group = os.environ.get("resource_group")
+    workspace_name = os.environ.get("workspace")
+
+    ml_client = MLClient(
+        AzureCliCredential(), subscription_id, resource_group, workspace_name
+    )
+
+    env_name = "acft-ubuntu20.04-py38-pytorch1.13.1-cuda11.7-gpu"
+
+    env_docker_context = Environment(
+        build=BuildContext(path=this_dir / BUILD_CONTEXT),
+        name=env_name,
+        description="ACFT environment created from a Docker context.",
+    )
+    ml_client.environments.create_or_update(env_docker_context)
+
+    # create the command
+    job = command(
+        code=this_dir / JOB_SOURCE_CODE,  # local path where the code is stored
+        command="python main.py",
+        environment=f"{env_name}@latest",
+        compute=os.environ.get("gpu_cluster"),
+        display_name="acft-finetuning-example",
+        description="Train a HF model with ACFT on a custom dataset.",
+        experiment_name="ACFT_Experiment"
+    )
+
+    returned_job = ml_client.create_or_update(job)
+    assert returned_job is not None
+
+    # Poll until final status is reached or timed out
+    timeout = time.time() + (TIMEOUT_MINUTES * 60)
+    while time.time() <= timeout:
+        current_status = ml_client.jobs.get(returned_job.name).status
+        if current_status in ["Completed", "Failed", "Canceled"]:
+            break
+        time.sleep(30)  # sleep 30 seconds
+
+    assert current_status == "Completed"
---
+++ acft/environments/acft-ubuntu20.04-py38-pytorch1.13.1-cuda11.7-gpu/tests/src/main.py
@@ -0,0 +1,181 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Sample Finetuning via ACFT."""
+# imports
+# flake8: noqa
+
+
+import argparse
+from azureml.acft.accelerator.finetune import AzuremlFinetuneArgs, AzuremlDatasetArgs, AzuremlTrainer
+from azureml.acft.accelerator.constants import HfTrainerType
+from datasets.load import load_dataset
+from pathlib import Path
+from transformers.data.data_collator import DataCollatorWithPadding
+from transformers import AutoConfig, AutoTokenizer # type: ignore
+from transformers.models.auto.modeling_auto import AutoModelForSequenceClassification
+import numpy as np
+from sklearn.metrics import (
+    f1_score,
+    accuracy_score,
+    matthews_corrcoef,
+    precision_score,
+    recall_score
+)
+from transformers.trainer_utils import EvalPrediction
+from typing import Any, Dict
+
+
+# <functions>
+# define functions
+def main(args):
+    """Finetune model."""
+    # read in data
+
+    finetune_params = {
+        "task_name": "SingleLabelClassification",
+        "model_name": "roberta-base",
+        "model_name_or_path": "roberta-base",
+        "model_type": "roberta",
+        "problem_type": "single_label_classification",
+        "metric_for_best_model": "f1",
+        "greater_is_better": True,
+        "pad_to_max_length": True,
+        "max_seq_length": 512,
+        "ignore_mismatched_sizes": True,
+        "pytorch_model_folder": "./outputs/acft_outputs/pytorch_model_folder",
+        "output_dir": "./outputs/acft_outputs/pytorch_model_folder",
+        "model_selector_output": "dummy_folder"
+    }
+
+    this_dir = Path(__file__).parent
+    data_dir = Path.joinpath(this_dir, "data")
+    train_file = str(Path.joinpath(data_dir, "train.jsonl"))
+    valid_file = str(Path.joinpath(data_dir, "valid.jsonl"))
+    dataset_dict = load_dataset("json", data_files={"train": train_file, "validation": valid_file})
+
+    finetune_params["class_names"] = [0, 1]
+    finetune_params["num_labels"] = len(finetune_params["class_names"])
+
+    tokenizer = None
+    try:
+        tokenizer = AutoTokenizer.from_pretrained(
+            finetune_params["model_name_or_path"],
+            use_fast=True,
+        )
+    except Exception as e:
+        print(f"Fast tokenizer not supported: {e}")
+        print("Trying default tokenizer.")
+        # slow tokenizer
+        tokenizer =  AutoTokenizer.from_pretrained(
+            finetune_params["model_name_or_path"],
+        )
+    print("Loaded tokenizer : {}".format(tokenizer))
+
+    dataset_args = AzuremlDatasetArgs(
+        train_dataset=dataset_dict["train"], # type: ignore
+        validation_dataset=dataset_dict["validation"], # type: ignore
+        data_collator=DataCollatorWithPadding(tokenizer) if tokenizer is not None else None
+    )
+
+    finetune_args = AzuremlFinetuneArgs(
+        finetune_params,
+        trainer_type=HfTrainerType.DEFAULT
+    )
+
+    class_names = finetune_params["class_names"]
+    id2label = {idx: lbl for idx, lbl in enumerate(class_names)}
+    label2id = {lbl: idx for idx, lbl in enumerate(class_names)}
+
+    config = AutoConfig.from_pretrained(
+        finetune_params["model_name_or_path"],
+        problem_type=finetune_params["problem_type"],
+        num_labels=finetune_params["num_labels"],
+        id2label=id2label,
+        label2id=label2id,
+        output_attentions=False,
+        output_hidden_states=False,
+    )
+
+    model, model_loading_metadata = AutoModelForSequenceClassification.from_pretrained(
+        finetune_params["model_name_or_path"],
+        config=config,
+        ignore_mismatched_sizes=finetune_params.pop("ignore_mismatched_sizes", False),
+        output_loading_info=True,
+    )
+
+    Path(finetune_params["output_dir"]).mkdir(exist_ok=True, parents=True)
+
+    trainer = AzuremlTrainer(
+        finetune_args=finetune_args,
+        dataset_args=dataset_args,
+        model=model,
+        tokenizer=tokenizer,
+        metric_func=single_label_metrics_func,
+        new_initalized_layers=model_loading_metadata["missing_keys"],
+    )
+
+    trainer.train()
+
+
+# </functions>
+
+
+def single_label_metrics_func(eval_pred: EvalPrediction) -> Dict[str, Any]:
+    """
+    compute and return metrics for sequence classification
+    """
+
+    predictions, labels = eval_pred
+    pred_flat = np.argmax(predictions, axis=1).flatten()
+    labels_flat = labels.flatten()
+
+    # NOTE `len` method is supported for torch tensor or numpy array. It is the count of elements in first dimension
+    print(f"Predictions count: {len(pred_flat)} | References count: {len(labels_flat)}")
+    accuracy = accuracy_score(y_true=labels_flat, y_pred=pred_flat)
+    f1_macro = f1_score(y_true=labels_flat, y_pred=pred_flat, average="macro")
+    mcc = matthews_corrcoef(y_true=labels_flat, y_pred=pred_flat)
+    precision_macro = precision_score(y_true=labels_flat, y_pred=pred_flat, average="macro")
+    recall_macro = recall_score(y_true=labels_flat, y_pred=pred_flat, average="macro")
+
+    metrics = {
+        "accuracy": accuracy,
+        "f1_macro": f1_macro,
+        "mcc": mcc,
+        "precision_macro": precision_macro,
+        "recall_macro": recall_macro
+    }
+
+    return metrics
+
+
+def parse_args():
+    """Parse arguments."""
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--model-name", type=str, default="roberta-base", required=False)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
