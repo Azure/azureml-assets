5d42297b7ce8ab1266cb24f9b0c2bc231443e525
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2023-03-08 10:49:33 -0800

    Update responsibleai_sample_test.py

--- responsibleai/environments/responsibleai-ubuntu20.04-py38-cpu/tests/responsibleai_sample_test.py
+++ responsibleai/environments/responsibleai-ubuntu20.04-py38-cpu/tests/responsibleai_sample_test.py
@@ -9,6 +9,8 @@ from azure.ai.ml import MLClient
 from azure.ai.ml import command, Input
 from azure.ai.ml._restclient.models import JobStatus
 from azure.ai.ml.entities import Environment, BuildContext
+from azure.ai.ml import automl
+from azure.ai.ml import Input
 from azure.identity import AzureCliCredential
 
 BUILD_CONTEXT = Path("../context")
@@ -56,6 +58,76 @@ def test_responsibleai():
     )
 
     returned_job = ml_client.create_or_update(job)
+    assert returned_job is not None
+
+    # Poll until final status is reached, or timed out
+    timeout = time.time() + (TIMEOUT_MINUTES * 60)
+    while time.time() <= timeout:
+        current_status = ml_client.jobs.get(returned_job.name).status
+        if current_status in [JobStatus.COMPLETED, JobStatus.FAILED]:
+            break
+        time.sleep(30)  # sleep 30 seconds
+
+    if current_status == JobStatus.FAILED:
+        ml_client.jobs.download(returned_job.name)
+        if STD_LOG.exists():
+            print(f"*** BEGIN {STD_LOG} ***")
+            with open(STD_LOG, "r") as f:
+                print(f.read(), end="")
+            print(f"*** END {STD_LOG} ***")
+        else:
+            ml_client.jobs.stream(returned_job.name)
+
+    assert current_status == "Completed"
+
+
+ def test_responsibleai_automl():
+    """Tests a sample automl job using responsibleai image as the environment."""
+    this_dir = Path(__file__).parent
+
+    subscription_id = os.environ.get("subscription_id")
+    resource_group = os.environ.get("resource_group")
+    workspace_name = os.environ.get("workspace")
+
+    ml_client = MLClient(
+        AzureCliCredential(), subscription_id, resource_group, workspace_name
+    )
+    # general job parameters
+    max_trials = 5
+    exp_name = "dpv2-regression-experiment"
+    my_training_data_input  = Input(
+        type=AssetTypes.MLTABLE,
+        path="azureml://datastores/workspaceblobstore/paths/my-regression-mltable")
+    
+    # Create the AutoML regression job with the related factory-function.
+    regression_job = automl.regression(
+        compute=os.environ.get("cpu_cluster"),,
+        experiment_name=exp_name,
+        training_data=my_training_data_input,
+        target_column_name="ERP",
+        primary_metric="R2Score",
+        n_cross_validations=5,
+        enable_model_explainability=True,
+        tags={"my_custom_tag": "My custom value"},
+        properties={"_aml_internal_automl_best_rai": True}
+    )
+
+    # Limits are all optional
+    regression_job.set_limits(
+        timeout_minutes=600,
+        trial_timeout_minutes=20,
+        max_trials=max_trials,
+        # max_concurrent_trials = 4,
+        # max_cores_per_trial: -1,
+        enable_early_termination=True,
+    )
+    
+    # Submit the AutoML job
+    returned_job = ml_client.jobs.create_or_update(
+        regression_job
+    )  # submit the job to the backend
+
+    print(f"Created job: {returned_job}")
     assert returned_job is not None
 
     # Poll until final status is reached, or timed out
