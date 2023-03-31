07bff65b96d6fef751b2853c9574ec0c908848a6
Renyi Xiong <renyix@microsoft.com>
2023-03-31 14:01:16 -0700

    Add pytest job

---
+++ data/data-transfer/tests/test_data_transfer_components.py
@@ -0,0 +1,20 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Test AutoML NLP designer components."""
+
+import logging
+import pytest
+from azure.ai.ml import load_job
+from azure.ai.ml.entities import PipelineJob
+
+
+logger = logging.getLogger(name=__file__)
+
+
+@pytest.mark.unittest
+class TestDataTransferComponents:
+    def test_pipeline_job_load_with_data_transfer_components(self):
+        pipeline = load_job(source="./data-transfer/tests/test_data_transfer_components.yaml")
+
+        assert isinstance(pipeline, PipelineJob)
