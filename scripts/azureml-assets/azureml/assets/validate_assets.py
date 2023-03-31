49dd03e4f2dfdbeded742a58211893635ba15703
Louie Larson <elesel@gmail.com>
2023-03-31 16:54:51 -0400

    Check publishing details (#481)

--- scripts/azureml-assets/azureml/assets/validate_assets.py
+++ scripts/azureml-assets/azureml/assets/validate_assets.py
@@ -12,6 +12,7 @@ from typing import List
 
 import azureml.assets as assets
 import azureml.assets.util as util
+from azureml.assets import PublishLocation, PublishVisibility
 from azureml.assets.config import ValidationException
 from azureml.assets.util import logger
 
@@ -177,6 +178,14 @@ def validate_image_publishing(asset_config: assets.AssetConfig,
     # Check publishing info
     if not environment_config.publish_enabled:
         _log_error(asset_config, "Image publishing information is not specified")
+        error_count += 1
+
+    # Check publishing details
+    if environment_config.publish_location != PublishLocation.MCR:
+        _log_error(asset_config, "Image publishing location should be 'mcr'")
+        error_count += 1
+    if environment_config.publish_visibility != PublishVisibility.PUBLIC:
+        _log_error(asset_config, "Image publishing visibility should be 'public'")
         error_count += 1
 
     return error_count
