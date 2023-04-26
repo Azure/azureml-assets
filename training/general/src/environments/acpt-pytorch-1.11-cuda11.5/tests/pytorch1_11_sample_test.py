f16d2018e2a315594efd36401e5203609e0864e8
cassieesvelt <cassieesvelt@gmail.com>
2023-04-26 10:38:07 -0700

    remove deprecated env test change

--- training/general/src/environments/acpt-pytorch-1.11-cuda11.5/tests/pytorch1_11_sample_test.py
+++ training/general/src/environments/acpt-pytorch-1.11-cuda11.5/tests/pytorch1_11_sample_test.py
@@ -5,15 +5,14 @@
 import os
 import time
 from pathlib import Path
-from azure.ai.ml import command, Output, MLClient, PyTorchDistribution
-from azure.ai.ml.entities import Environment, BuildContext, JobResourceConfiguration
+from azure.ai.ml import command, Input, MLClient
+from azure.ai.ml.entities import Environment, BuildContext
 from azure.identity import AzureCliCredential
-import subprocess
 
 BUILD_CONTEXT = Path("../context")
 JOB_SOURCE_CODE = "src"
 TIMEOUT_MINUTES = os.environ.get("timeout_minutes", 40)
-STD_LOG = Path("artifacts/user_logs/std_log.txt")
+
 
 def test_pytorch_1_11():
     """Tests a sample job using pytorch 1.11 as the environment."""
@@ -39,26 +38,25 @@ def test_pytorch_1_11():
     # create the command
     job = command(
         code=this_dir / JOB_SOURCE_CODE,  # local path where the code is stored
-        command="pip install -r requirements.txt && python pretrain_glue.py --tensorboard_log_dir \"/outputs/runs/\" --deepspeed ds_config.json --num_train_epochs 10 --output_dir outputs --disable_tqdm 1 --local_rank $RANK --evaluation_strategy \"epoch\"  --logging_strategy \"epoch\" --per_device_train_batch_size 93 --gradient_accumulation_steps 1 --per_device_eval_batch_size 93 --learning_rate 3e-05 --adam_beta1 0.8 --adam_beta2 0.999 --weight_decay 3e-07 --warmup_steps 500 --fp16 --logging_steps 1000 --model_checkpoint \"bert-large-uncased\"",
-        outputs={
-            "output": Output(
-                type="uri_folder",
-                mode="rw_mount",
-                path="azureml://datastores/workspaceblobstore/paths/outputs"
-            )
+        command="pip install -r requirements.txt && python main.py --iris-csv ${{inputs.iris_csv}} "
+                "--epochs ${{inputs.epochs}} --lr ${{inputs.lr}}",
+        inputs={
+            "iris_csv": Input(
+                type="uri_file",
+                path="https://azuremlexamples.blob.core.windows.net/datasets/iris.csv",
+            ),
+            "epochs": 10,
+            "lr": 0.1,
         },
         environment=f"{env_name}@latest",
-        compute=os.environ.get("gpu_v100_cluster"),
-        display_name="bert-pretrain-GLUE",
-        description="Pretrain the BERT model on the GLUE dataset.",
-        experiment_name="pytorch111_Cuda115_Experiment",
-        distribution=PyTorchDistribution(process_count_per_instance=2),
-        resources=JobResourceConfiguration(instance_count=1),
+        compute=os.environ.get("gpu_cluster"),
+        display_name="pytorch-iris-example",
+        description="Train a neural network with PyTorch on the Iris dataset.",
+        experiment_name="pytorch111_Cuda115_Experiment"
     )
 
     returned_job = ml_client.create_or_update(job)
     assert returned_job is not None
-
 
     # Poll until final status is reached or timed out
     timeout = time.time() + (TIMEOUT_MINUTES * 60)
@@ -67,21 +65,5 @@ def test_pytorch_1_11():
         if current_status in ["Completed", "Failed"]:
             break
         time.sleep(30)  # sleep 30 seconds
-
-    bashCommand = "ls"
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    print(output)
-    print(error)
-
-    if current_status=="Failed":
-        ml_client.jobs.download(returned_job.name)
-        if STD_LOG.exists():
-            print(f"*** BEGIN {STD_LOG} ***")
-            with open(STD_LOG, "r") as f:
-                print(f.read(), end="")
-            print(f"*** END {STD_LOG} ***")
-        else:
-            ml_client.jobs.stream(returned_job.name)
 
     assert current_status == "Completed"
