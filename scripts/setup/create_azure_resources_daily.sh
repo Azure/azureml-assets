659d4eeb48cd51baa8f6651d673e0e4d79827485
cassieesvelt <cassieesvelt@gmail.com>
2023-04-27 14:15:53 -0700

    add Standard_NC6s_v3 cluster

--- scripts/setup/create_azure_resources_daily.sh
+++ scripts/setup/create_azure_resources_daily.sh
@@ -12,10 +12,9 @@ az extension add -n ml -y
 
 echo "Configuring Azure CLI defaults"
 az configure --defaults group=$resource_group workspace=$workspace location=$location
-az ml compute delete --name $gpu_v100_cluster --yes
 
 # See if the last resource to be created already exists, and if so bail early
-if az ml compute show --name $gpu_cluster >/dev/null 2>&1; then
+if az ml compute show --name $gpu_v100_cluster >/dev/null 2>&1; then
     echo "Azure resources already exist"
 else
     resource_name="resource group ${resource_group}"
@@ -58,7 +57,7 @@ else
     echo "Checking ${resource_name}"
     if ! az ml compute show --name $gpu_v100_cluster --output none >/dev/null 2>&1; then
         echo "Creating ${resource_name}"
-        az ml compute create --name $gpu_v100_cluster --size Standard_NC12s_v3 --min-instances 0 --max-instances 2 --type AmlCompute
+        az ml compute create --name $gpu_v100_cluster --size Standard_NC6s_v3 --min-instances 0 --max-instances 2 --type AmlCompute
     fi
 fi
 
