d922c5c58dbba60b8819f05e7a569d933bd0f241
Kelly Ly <kellyly@microsoft.com>
2023-04-28 11:29:13 -0400

    Create assets-docs.yaml and add assets documentation generation scripts

---
+++ scripts/docs/generate_asset_documentation.py
@@ -0,0 +1,213 @@
+from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedMap, CommentedSeq
+import snakemd
+import argparse
+import re
+import os
+
+
+def create_asset_doc(spec_file_name, asset_type):
+    # read the component yaml file and parse it
+    yaml = YAML()
+    with open(spec_file_name, 'r') as f:
+        component = yaml.load(f)
+
+    asset_type = str(asset_type).replace("AssetType.", "").lower()
+
+    # create component document and add info
+    doc = snakemd.new_doc()
+    doc = add_intro(doc, component)
+    doc = add_inputs(doc, component)
+    doc = add_outputs(doc, component)
+    doc = add_additional_details(doc, component)
+
+    # check if ReferenceDocumentation folder exists, create one if not
+    if not os.path.exists("ReferenceDocumentation/"):
+        os.mkdir("ReferenceDocumentation/")
+
+    # check if asset folder exists in ReferenceDocumentation, create one if not
+    if not os.path.exists("ReferenceDocumentation/" + asset_type + "s/"):
+        os.mkdir("ReferenceDocumentation/" + asset_type + "s")
+
+    with open("ReferenceDocumentation/" + asset_type + "s/" + asset_type + "-" + component["name"] + ".md", 'w') as f:
+        f.write(str(doc))
+
+    # return md file name
+    return asset_type, asset_type + "-" + component["name"]
+
+
+
+def add_intro(doc, component):
+
+    doc.add_heading(component['display_name'] if "display_name" in component else component["name"], level=2)
+
+    doc.add_heading("README file ", level=3)
+
+    doc.add_heading("Component Overview ", level=3)
+    if "description" in component:
+        doc.add_paragraph("**Description**: " + str(component['description']))
+    if "version" in component:
+        doc.add_paragraph("**Version**: " + str(component['version']))
+    if "type" in component:
+        doc.add_paragraph("**Type**: " + component['type'])
+    if "tags" in component and "license" in component["tags"]:
+        doc.add_paragraph("**License**: " + component["tags"]["license"])
+
+    doc.add_heading("YAML Syntax ", level=3)
+
+    if "type" in component and component["type"] == "automl":
+        doc.add_paragraph("**Task**: " + component["task"] if "task" in component else "")
+
+    if "properties" in component:
+        doc.add_heading("Properties", level=4)
+        doc.add_paragraph("**SHA**: " + component["properties"]["SHA"] if "SHA" in component["properties"] else "")
+        doc.add_paragraph("**Datasets**: " + component["properties"]["datasets"] if "datasets" in component["properties"] else "")
+        doc.add_paragraph("**Finetuning Tasks**: " + component["properties"]["finetuning-tasks"] if "finetuning-tasks" in component["properties"] else "")
+        doc.add_paragraph("**Languages**: " + component["properties"]["languages"] if "languages" in component["properties"] else "")
+
+    return doc
+
+
+def add_additional_details(doc, component):
+
+    doc.add_heading("Parameters ", level=3)
+    
+    doc.add_heading("Code", level=3)
+    if "type" in component and component["type"] == "command" and "code" in component:
+        doc.add_paragraph(component['code'])
+
+    doc.add_heading("Environment ", level=3)
+    doc.add_paragraph(component['environment'] if "environment" in component else "")
+        
+    doc.add_heading("Compute Specifications ", level=3)
+    doc.add_paragraph(component['tags']['min_inference_sku'] if "tags" in component and "min_inference_sku" in component["tags"] else "")
+
+    return doc
+
+
+# set attributes
+def get_comments_map(self, key):
+    coms = []
+    comments = self.ca.items.get(key)
+    if comments is None:
+        return coms
+    for token in comments:
+        if token is None:
+            continue
+        elif isinstance(token, list):
+            coms.extend(token)
+        else:
+            coms.append(token)
+    return coms
+
+def get_comments_seq(self, idx):
+    coms = []
+    comments = self.ca.items.get(idx)
+    if comments is None:
+        return coms
+    for token in comments:
+        if token is None:
+            continue
+        elif isinstance(token, list):
+            coms.extend(token)
+        else:
+            coms.append(token)
+    return coms
+
+
+setattr(CommentedMap, 'get_comments', get_comments_map)
+setattr(CommentedSeq, 'get_comments', get_comments_seq)
+
+def check_comments(data):
+    if isinstance(data, CommentedMap):
+        for k, v in data.items():
+           comments = data.get_comments(k)
+    elif isinstance(data, CommentedSeq):
+        for idx, item in enumerate(data):
+            comments = data.get_comments(k)
+    for comment in comments:
+        text = comment.value
+        text = re.sub(r"[\n\t\s]*", "", text)
+        if text:
+            return True 
+    return False
+        
+def insert_comments_between_inputs(doc, data):
+    if isinstance(data, CommentedMap):
+        for k, v in data.items():
+           comments = data.get_comments(k)
+    elif isinstance(data, CommentedSeq):
+        for idx, item in enumerate(data):
+            comments = data.get_comments(k)
+    for comment in comments:
+        text=comment.value
+        text = re.sub(r"#", "", text)
+        doc.add_paragraph(text)
+
+    return doc
+
+def insert_comments_under_input(doc, data):
+    for comments in data:
+        if comments:
+            for comment in comments:
+                doc.add_paragraph(comment.value.strip('#'))
+    return doc
+
+def add_inputs(doc, component):
+    doc.add_heading("Inputs ", level=2)
+
+    if "inputs" in component:
+        headers = ['Name', 'Description', 'Type', 'Default', 'Optional', 'Enum']
+        rows = []
+        if component.ca.items.get('inputs') is not None:
+            doc = insert_comments_under_input(doc, component.ca.items.get('inputs'))
+
+        for k, v in component['inputs'].items():
+            row = []
+            row.append(k)
+            row.append(v['description'] if 'description' in v else ' ')
+            row.append(v['type'])
+            row.append(v['default'] if 'default' in v else ' ')
+            row.append(v['optional'] if 'optional' in v else ' ')
+            row.append(v['enum'] if 'enum' in v else ' ') 
+            
+            if check_comments(v):
+                rows.append(row)
+                doc.add_table(headers, rows)
+                doc = insert_comments_between_inputs(doc, v)
+                rows = []  
+            else:
+                rows.append(row)
+
+        doc.add_table(headers, rows)
+
+    return doc
+
+
+def add_outputs(doc, component):
+    doc.add_heading("Outputs ", level=2)
+
+    if "outputs" in component:
+        headers = ['Name', 'Description', 'Type']
+        rows = []
+        if component.ca.items.get('outputs') is not None:
+            doc = insert_comments_under_input(doc, component.ca.items.get('outputs'))
+
+        for k, v in component['outputs'].items():
+            row = []
+            row.append(k)
+            row.append(v['description'] if 'description' in v else ' ')
+            row.append(v['type'])
+            
+            if check_comments(v):
+                rows.append(row)
+                doc.add_table(headers, rows)
+                doc = insert_comments_between_inputs(doc, v)
+                rows = []      
+            else:
+                rows.append(row)
+
+        doc.add_table(headers, rows)
+
+    return doc
+
---
+++ scripts/docs/parse_assets.py
@@ -0,0 +1,112 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Parse assets from directory and generate markdown files."""
+import generate_asset_documentation
+
+import argparse
+import re
+from pathlib import Path
+from typing import List
+
+import azureml.assets as assets
+import azureml.assets.util as util
+from azureml.assets.util import logger
+
+import snakemd
+from collections import defaultdict
+
+PARSED_COUNT = "parsed_count"
+
+
+
+def parse_assets(input_dirs: List[Path],
+                asset_config_filename: str,
+                release_directory_root: Path = None,
+                use_version_dirs: bool = False,
+                pattern: re.Pattern = None):
+    """Parse all assets from input directory and generate documentation for each.
+
+    Args:
+        input_dirs (List[Path]): List of directories to search for assets.
+        asset_config_filename (str): Asset config filename to search for.
+        release_directory_root (Path, optional): Release directory location. Defaults to None.
+        use_version_dirs (bool, optional): Use version directories for output. Defaults to False.
+        pattern (re.Pattern, optional): Regex pattern for assets to copy. Defaults to None.
+    """
+    # Find assets under release dir
+    asset_count = 0
+
+    references = defaultdict(list)
+
+    for asset_config in util.find_assets(input_dirs, asset_config_filename, pattern=pattern):
+        asset_count += 1
+
+        asset_type, asset_file_name = generate_asset_documentation.create_asset_doc(asset_config.spec_with_path, asset_config.type)
+        references[asset_type].append(asset_file_name)
+
+    logger.print(f"{asset_count} asset(s) copied")
+
+
+    for asset_type in references:
+
+
+        print(asset_type)
+        # create new md file (folder_name + ".md")
+
+        doc = snakemd.new_doc()
+        doc.add_heading(asset_type.capitalize() + "s", level=1)
+
+        doc.add_heading("Glossary", level=2)
+
+        # [componentA]: https://github.com/vizhur/vizhur/wiki/componentA
+        # [componentB]: https://github.com/vizhur/vizhur/wiki/componentB
+        # [componentC]: https://github.com/vizhur/vizhur/wiki/componentC
+
+        # ***
+        # * [ComponentA][componentA]
+
+        for asset_file_name in references[asset_type]:
+            doc.add_paragraph("[" + asset_file_name + "]: https://github.com/vizhur/vizhur/wiki/" + asset_type + "-" + asset_file_name)
+            
+        doc.add_horizontal_rule()  # aka ***
+
+        asset_links_list = []
+        for asset_file_name in references[asset_type]:
+            asset_links_list.append(snakemd.Paragraph(asset_file_name).insert_link(asset_file_name, asset_file_name))
+            # add xref link to Glossary!
+
+        doc.add_unordered_list(asset_links_list)
+
+
+        with open("ReferenceDocumentation/" + asset_type + "s/" + asset_type + "s.md", 'w') as f:
+            f.write(str(doc))
+
+
+
+
+if __name__ == '__main__':
+    # Handle command-line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input-dirs", required=True,
+                        help="Comma-separated list of directories containing assets")
+    parser.add_argument("-a", "--asset-config-filename", default=assets.DEFAULT_ASSET_FILENAME,
+                        help="Asset config file name to search for")
+    parser.add_argument("-r", "--release-directory", type=Path,
+                        help="Directory to which the release branch has been cloned. "
+                        "If provided, only asset versions without a release tag will be copied.")
+    parser.add_argument("-v", "--use-version-dirs", action="store_true",
+                        help="Use version directories when storing assets in output directory")
+    parser.add_argument("-t", "--pattern", type=re.compile,
+                        help="Regex pattern to select assets to copy, in the format <type>/<name>/<version>")
+    args = parser.parse_args()
+
+    # Convert comma-separated values to lists
+    input_dirs = [Path(d) for d in args.input_dirs.split(",")]
+
+    # Copy assets
+    parse_assets(input_dirs=input_dirs,
+                asset_config_filename=args.asset_config_filename,
+                release_directory_root=args.release_directory,
+                use_version_dirs=args.use_version_dirs,
+                pattern=args.pattern)
