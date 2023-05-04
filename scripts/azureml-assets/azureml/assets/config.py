cea91c076c70bb9bc3dc40d9a007aff20b1f0e45
Kelly Ly <kellyly@microsoft.com>
2023-05-04 11:27:34 -0400

    Include categories in AssetConfig

--- scripts/azureml-assets/azureml/assets/config.py
+++ scripts/azureml-assets/azureml/assets/config.py
@@ -744,6 +744,7 @@ class AssetConfig(Config):
             enabled: true
             pip_requirements: tests/requirements.txt
             tests_dir: tests
+        categories: ["PyTorch", "Python 3.9"] # List of categories
     """
 
     def __init__(self, file_name: Path):
