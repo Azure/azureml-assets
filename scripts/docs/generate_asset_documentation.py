83f88862311acee2a221c38a178330f8210af66d
vizhur <vizhur@microsoft.com>
2023-05-08 15:17:01 -0400

    Update generate_asset_documentation.py

--- scripts/docs/generate_asset_documentation.py
+++ scripts/docs/generate_asset_documentation.py
@@ -10,183 +10,306 @@ import os
 from pathlib import Path
 from glob import glob as search
 
+supported_asset_types=["environment", "component", "model"]
+default_category = "Uncategorized"
 
-def create_asset_doc(asset_config, asset_type):
-    """Generate asset markdown document with information based on its asset yaml file."""
-    # read the asset yaml file and parse it
-    yaml = YAML()
-    with open(asset_config.spec_with_path, 'r') as f:
-        asset = yaml.load(f)
+class AssetInfo:
+    _asset_config = None
+    _asset = None
+    _doc = None
 
-    asset_type = asset_type.value
+    def __init__(self, asset_config):
+        self._asset_config = asset_config
+        yaml = YAML()
+        with open(self._asset_config.spec_with_path, 'r') as f:
+            self._asset = yaml.load(f)
+        self._doc = snakemd.new_doc()
 
-    # create asset document and add info
-    if asset_type == "environment":
-        doc, description, name, categories = create_environment_doc(asset, asset_config)
-    elif asset_type == "component":
-        doc, description, name, categories = create_component_doc(asset, asset_config)
-    elif asset_type == "model":
-        doc, description, name, categories = create_model_doc(asset, asset_config)
+    @property
+    def type(self):
+        return self._asset_config.type.value
+    
+    @property
+    def name(self):
+        return self._asset["name"]
 
-    # check if asset folder exists, create one if not
-    asset_type_dir = Path(f"{asset_type}s")
-    asset_type_dir.mkdir(exist_ok=True)
+    @property
+    def version(self):
+        return str(self._asset["version"])
+    
+    @property
+    def description(self):
+        return str(self._asset.get("description", ""))
+    
+    @property
+    def categories(self):
+        return [default_category]
 
-    asset_file_name = f"{asset_type}-{asset['name']}"
-    full_asset_file_name = f"{asset_type_dir}/{asset_file_name}.md"
+    @property
+    def filename(self):
+        return f"{self.type}-{self.name}"
 
-    # write the file into the asset folder
-    with open(full_asset_file_name, 'w') as f:
-        f.write(str(doc))
+    @property
+    def directory(self):
+        return Path(f"{self.type}s")
+    
+    @property
+    def fullpath(self):
+        return f"{self.directory}/{self.filename}.md"
 
-    return asset_type, asset["name"], asset_file_name, asset.get("description", "")
+    @property
+    def doc(self):
+        _doc = snakemd.new_doc()
+        return _doc
 
+    def save(self):
+        self.directory.mkdir(exist_ok=True)
+        with open(self.fullpath, 'w') as f:
+            f.write(str(self.doc))
 
-def create_environment_doc(asset, asset_config):
-    """Generate environment document content."""
-    doc = snakemd.new_doc()
+    @staticmethod
+    def create_asset_info(asset_config):
+        # TODO: Use AssetType.COMPONENT
+        if asset_config.type.value == "environment":
+            return EnvironmentInfo(asset_config)
+        if asset_config.type.value == "component":
+            return ComponentInfo(asset_config)
+        if asset_config.type.value == "model":
+            return ModelInfo(asset_config)
 
-    doc = add_intro(doc, asset)
-    link = "https://ml.azure.com/registries/azureml/environments/{}/version/{}".format(asset["name"], asset["version"])
-    doc.add_paragraph("**View in Studio**:  [{}]({})".format(link, link))
-    # doc.add_paragraph("**View in Studio**:  <a href=\"{}\" target=\"_blank\">{}</a>".format(link, link))
-    doc = add_mcr_image(doc, asset, asset_config)
-    doc = add_docker_context(doc, asset, asset_config)
-    doc = add_additional_details(doc, asset)
+        raise Exception(f"Not supported asset type {asset_config.type}. Use {supported_asset_types}")
 
-    description = None
-    name = None
-    categories = None
-    return doc, description, name, categories
+#region Doc Formatting
+    def _add_doc_name(self, doc):
+        if "display_name" in self._asset:
+            doc.add_heading(self._asset['display_name'])
+        doc.add_heading(self.name)
 
+    def _add_doc_overview(self, doc):
+        doc.add_heading("Overview ", level=2)
+    
+    def _add_doc_description(self, doc):
+        doc.add_paragraph("**Description**: " + self.description)
+    
+    def _add_doc_asset_version(self, doc):
+        doc.add_paragraph("**Version**: " + self.version)
 
-def create_component_doc(asset, asset_config):
-    """Generate component document content."""
-    doc = snakemd.new_doc()
+    def _add_doc_license_from_tags(self, doc):
+        if "tags" in self._asset and "license" in self._asset["tags"]:
+            doc.add_paragraph("**License**: " + self._asse["tags"]["license"])
+  
+    def _add_doc_properties(self, doc):
+        if "properties" in self._asset:
+            doc.add_heading("Properties", level=4)
+            for property, value in self._asset["properties"].items():
+                doc.add_paragraph(f"**{property}**: {value}")
 
-    doc = add_intro(doc, asset)
-    doc = add_inputs(doc, asset)
-    doc = add_outputs(doc, asset)
-    doc = add_additional_details(doc, asset)
+    def _add_doc_tags(self, doc):
+        if "tags" in self._asset:
+            doc.add_heading("Tags ", level=3)
+            tags = []
+            for tag, value in self._asset["tags"].items():
+                tags.append("`{}` ".format(tag if len(str(value)) == 0 else (tag + " : " + str(value))))
+            tags.sort(key=lambda x: x[0].lower())
+            doc.add_raw(" ".join(tags))
+        return doc
 
-    description = None
-    name = None
-    categories = None
-    return doc, description, name, categories
-
-
-def create_model_doc(asset, asset_config):
-    """Generate model document content."""
-    doc = snakemd.new_doc()
-
-    doc = add_intro(doc, asset)
-
-    doc = add_inputs(doc, asset)
-    doc = add_outputs(doc, asset)
-    doc = add_additional_details(doc, asset)
-
-    description = None
-    name = None
-    categories = None
-    return doc, description, name, categories
-
-
-def add_mcr_image(doc, asset, asset_config):
-    """Add MCR Image."""
-    img = ":".join([str(asset_config.extra_config_as_object().get_full_image_name()), asset["version"]])
-    doc.add_paragraph("**Docker image**: " + img)
-    return doc
-
-
-def add_docker_context(doc, asset, asset_config):
-    """Add docker context content."""
-    doc.add_heading("Docker build context", level=2)
-    context = {}
-    dockerfile = ""
-    for context_file in search(str(asset_config.extra_config_as_object().context_dir_with_path) + "/*"):
-        content = ""
-        with open(context_file, "r") as f:
-            # do not expect nested structure for now
-            content = f.read()
-        filename = os.path.basename(context_file)
-        if filename.lower() == "dockerfile":
-            dockerfile = content
-        else:
-            context[filename] = content
-
-    doc.add_heading("Dockerfile", level=3)
-    doc.add_code(dockerfile, lang="dockerfile")
-
-    for name, file_content in context.items():
-        doc.add_heading(name, level=3)
-        filename, file_extension = os.path.splitext(name)
-        doc.add_code(file_content, lang=file_extension.strip("."))
-    return doc
+    def _add_doc_mcr_image(self, doc):
+        """Add MCR Image."""
+        img = ":".join([self._asset_config.extra_config_as_object().get_full_image_name(), self.version])
+        doc.add_paragraph("**Docker image**: " + img)
 
 
-def add_intro(doc, asset, asset_type=None):
-    """Add information like asset name and description to asset doc."""
-    doc.add_heading(asset['display_name'] if "display_name" in asset else asset["name"], level=2)
+    def _add_doc_docker_context(self, doc):
+        """Add docker context content."""
+        doc.add_heading("Docker build context", level=2)
+        context = {}
+        dockerfile = ""
+        for context_file in search(str(self._asset_config.extra_config_as_object().context_dir_with_path) + "/*"):
+            content = ""
+            with open(context_file, "r") as f:
+                # do not expect nested structure for now
+                content = f.read()
+            filename = os.path.basename(context_file)
+            if filename.lower() == "dockerfile":
+                dockerfile = content
+            else:
+                context[filename] = content
 
-    # doc.add_heading("README file ", level=3)
+        doc.add_heading("Dockerfile", level=3)
+        doc.add_code(dockerfile, lang="dockerfile")
 
-    doc.add_heading("Overview ", level=2)
+        for name, file_content in context.items():
+            doc.add_heading(name, level=3)
+            filename, file_extension = os.path.splitext(name)
+            doc.add_code(file_content, lang=file_extension.strip("."))
 
-    if "description" in asset:
-        doc.add_paragraph("**Description**: " + str(asset['description']))
+    def _add_doc_component_type_and_code(self, doc):
+        # TODO: have a code reference on GH
+        # doc.add_heading("Code", level=3)
+        # doc.add_paragraph(self._asset['code'])
+        pass        
 
-    if "version" in asset:
-        doc.add_paragraph("**Version**: " + str(asset['version']))
-    if "type" in asset:
-        doc.add_paragraph("**Type**: " + asset['type'])
-    if "tags" in asset and "license" in asset["tags"]:
-        doc.add_paragraph("**License**: " + asset["tags"]["license"])
+    def _add_doc_environment_dependency(self, doc):    
+        if "environment" in self._asset:
+            doc.add_heading("Environment", level=3)
+            doc.add_paragraph(self._asset['environment'])
 
-    # doc.add_heading("YAML Syntax ", level=3)
+    def _add_doc_compute_sku(self, doc):
+        if "tags" in self._asset and "min_inference_sku" in self._asset["tags"]:
+            doc.add_heading("Compute Specifications ", level=3)
+            doc.add_paragraph(self._asset['tags']['min_inference_sku'])
 
-    if "type" in asset and asset["type"] == "automl":
-        doc.add_paragraph("**Task**: " + asset["task"] if "task" in asset else "")
+    @staticmethod
+    def _add_doc_insert_comments_between_inputs(doc, data):
+        """Insert comments between inputs."""
+        if isinstance(data, CommentedMap):
+            for k, v in data.items():
+                comments = data.get_comments(k)
+        elif isinstance(data, CommentedSeq):
+            for idx, item in enumerate(data):
+                comments = data.get_comments(k)
+        for comment in comments:
+            text = comment.value
+            text = re.sub(r"#", "", text)
+            doc.add_paragraph(text)
 
-    if "properties" in asset:
-        doc.add_heading("Properties", level=4)
-        if "SHA" in asset["properties"]:
-            doc.add_paragraph("**SHA**: " + asset["properties"]["SHA"])
-        if "datasets" in asset["properties"]:
-            doc.add_paragraph("**Datasets**: " + asset["properties"]["datasets"])
-        if "finetuning-tasks" in asset["properties"]:
-            doc.add_paragraph("**Finetuning Tasks**: " + asset["properties"]["finetuning-tasks"])
-        if "languages" in asset["properties"]:
-            doc.add_paragraph("**Languages**: " + asset["properties"]["languages"])
+    @staticmethod
+    def _add_doc_insert_comments_under_input(doc, data):
+        """Insert comments under inputs."""
+        for comments in data:
+            if comments:
+                for comment in comments:
+                    doc.add_paragraph(comment.value.strip('#'))
 
-    if "tags" in asset:
-        doc.add_heading("Tags ", level=3)
-        tags = []
-        for tag, value in asset["tags"].items():
-            tags.append("`{}` ".format(tag if len(str(value)) == 0 else (tag + " : " + str(value))))
-        tags.sort(key=lambda x: x[0].lower())
-        doc.add_raw(" ".join(tags))
+    def _add_doc_asset_inputs(self, doc):
+        """Generate inputs table for the asset doc."""
+        if "inputs" in self._asset:
+            doc.add_heading("Inputs ", level=2)
 
-    return doc
+            headers = ['Name', 'Description', 'Type', 'Default', 'Optional', 'Enum']
+            rows = []
+            if self._asset.ca.items.get('inputs') is not None:
+                self._add_doc_insert_comments_under_input(doc, self._asset.ca.items.get('inputs'))
+
+            for k, v in self._asset['inputs'].items():
+                row = []
+                row.append(k)
+                row.append(v['description'] if 'description' in v else ' ')
+                row.append(v['type'])
+                row.append(v['default'] if 'default' in v else ' ')
+                row.append(v['optional'] if 'optional' in v else ' ')
+                row.append(v['enum'] if 'enum' in v else ' ')
+
+                if check_comments(v):
+                    rows.append(row)
+                    doc.add_table(headers, rows)
+                    self._add_doc_insert_comments_between_inputs(doc, v)
+                    rows = []
+                else:
+                    rows.append(row)
+
+            doc.add_table(headers, rows)
 
 
-def add_additional_details(doc, asset):
-    """Add information like parameters and compute specifications to asset doc."""
-    # doc.add_heading("Parameters ", level=3)
+    def _add_doc_asset_outputs(self, doc):
+        """Generate an outputs table for the asset doc."""
+        if "outputs" in self._asset:
+            doc.add_heading("Outputs ", level=2)
 
-    if "type" in asset and asset["type"] == "command" and "code" in asset:
-        doc.add_heading("Code", level=3)
-        doc.add_paragraph(asset['code'])
+            headers = ['Name', 'Description', 'Type']
+            rows = []
+            if self._asset.ca.items.get('outputs') is not None:
+                doc = self._add_doc_insert_comments_under_input(doc, self._asset.ca.items.get('outputs'))
 
-    if "environment" in asset:
-        doc.add_heading("Environment", level=3)
-        doc.add_paragraph(asset['environment'])
+            for k, v in self._asset['outputs'].items():
+                row = []
+                row.append(k)
+                row.append(v['description'] if 'description' in v else ' ')
+                row.append(v['type'])
 
-    if "tags" in asset and "min_inference_sku" in asset["tags"]:
-        doc.add_heading("Compute Specifications ", level=3)
-        doc.add_paragraph(asset['tags']['min_inference_sku'])
+                if check_comments(v):
+                    rows.append(row)
+                    doc.add_table(headers, rows)
+                    self._add_doc_insert_comments_between_inputs(doc, v)
+                    rows = []
+                else:
+                    rows.append(row)
 
-    return doc
+            doc.add_table(headers, rows)
 
+
+#endregion
+
+class EnvironmentInfo(AssetInfo):
+
+    def __init__(self, asset_config):
+        super().__init__(asset_config)
+
+    @AssetInfo.doc.getter
+    def doc(self):
+        _doc = snakemd.new_doc()
+        
+        self._add_doc_name(_doc)
+        self._add_doc_overview(_doc)
+        self._add_doc_description(_doc)
+        self._add_doc_asset_version(_doc)
+        self._add_doc_tags(_doc)
+
+
+        link = "https://ml.azure.com/registries/azureml/environments/{}/version/{}".format(self.name, self.version)
+        _doc.add_paragraph("**View in Studio**:  [{}]({})".format(link, link))
+        # doc.add_paragraph("**View in Studio**:  <a href=\"{}\" target=\"_blank\">{}</a>".format(link, link))
+
+        self._add_doc_mcr_image(_doc)
+        self._add_doc_docker_context(_doc)
+
+        return _doc
+
+class ComponentInfo(AssetInfo):
+
+    def __init__(self, asset_config):
+        super().__init__(asset_config)
+
+    @AssetInfo.doc.getter
+    def doc(self):
+        _doc = snakemd.new_doc()
+
+        self._add_doc_name(_doc)
+        self._add_doc_overview(_doc)
+        self._add_doc_description(_doc)
+        self._add_doc_asset_version(_doc)
+        self._add_doc_tags(_doc)
+
+        self._add_doc_asset_inputs(_doc)
+        self._add_doc_asset_outputs(_doc)
+
+        self._add_doc_component_type_and_code(_doc)
+        self._add_doc_environment_dependency(_doc)
+
+        return _doc
+
+
+class ModelInfo(AssetInfo):
+
+    def __init__(self, asset_config):
+        super().__init__(asset_config)
+
+    @AssetInfo.doc.getter
+    def doc(self):
+        _doc = snakemd.new_doc()
+        self._add_doc_name(_doc)
+        self._add_doc_overview(_doc)
+        self._add_doc_description(_doc)
+        self._add_doc_asset_version(_doc)
+        self._add_doc_tags(_doc)
+
+        self._add_doc_properties(_doc)
+        self._add_doc_compute_sku(_doc)
+
+        return _doc
+
+#region attibutes
 
 # set attributes
 def get_comments_map(self, key):
@@ -240,88 +363,4 @@ def check_comments(data):
             return True
     return False
 
-
-def insert_comments_between_inputs(doc, data):
-    """Insert comments between inputs."""
-    if isinstance(data, CommentedMap):
-        for k, v in data.items():
-            comments = data.get_comments(k)
-    elif isinstance(data, CommentedSeq):
-        for idx, item in enumerate(data):
-            comments = data.get_comments(k)
-    for comment in comments:
-        text = comment.value
-        text = re.sub(r"#", "", text)
-        doc.add_paragraph(text)
-
-    return doc
-
-
-def insert_comments_under_input(doc, data):
-    """Insert comments under inputs."""
-    for comments in data:
-        if comments:
-            for comment in comments:
-                doc.add_paragraph(comment.value.strip('#'))
-    return doc
-
-
-def add_inputs(doc, asset):
-    """Generate inputs table for the asset doc."""
-    if "inputs" in asset:
-        doc.add_heading("Inputs ", level=2)
-
-        headers = ['Name', 'Description', 'Type', 'Default', 'Optional', 'Enum']
-        rows = []
-        if asset.ca.items.get('inputs') is not None:
-            doc = insert_comments_under_input(doc, asset.ca.items.get('inputs'))
-
-        for k, v in asset['inputs'].items():
-            row = []
-            row.append(k)
-            row.append(v['description'] if 'description' in v else ' ')
-            row.append(v['type'])
-            row.append(v['default'] if 'default' in v else ' ')
-            row.append(v['optional'] if 'optional' in v else ' ')
-            row.append(v['enum'] if 'enum' in v else ' ')
-
-            if check_comments(v):
-                rows.append(row)
-                doc.add_table(headers, rows)
-                doc = insert_comments_between_inputs(doc, v)
-                rows = []
-            else:
-                rows.append(row)
-
-        doc.add_table(headers, rows)
-
-    return doc
-
-
-def add_outputs(doc, asset):
-    """Generate an outputs table for the asset doc."""
-    if "outputs" in asset:
-        doc.add_heading("Outputs ", level=2)
-
-        headers = ['Name', 'Description', 'Type']
-        rows = []
-        if asset.ca.items.get('outputs') is not None:
-            doc = insert_comments_under_input(doc, asset.ca.items.get('outputs'))
-
-        for k, v in asset['outputs'].items():
-            row = []
-            row.append(k)
-            row.append(v['description'] if 'description' in v else ' ')
-            row.append(v['type'])
-
-            if check_comments(v):
-                rows.append(row)
-                doc.add_table(headers, rows)
-                doc = insert_comments_between_inputs(doc, v)
-                rows = []
-            else:
-                rows.append(row)
-
-        doc.add_table(headers, rows)
-
-    return doc
+#endregion
