e73fd0b06f65c4171455936bd1ceb5a4207cd7bd
esrao <7992454+esrao@users.noreply.github.com>
2023-05-15 16:31:48 -0700

    dummy change to test validation workflow

--- training/model_management/src/run_model_download.py
+++ training/model_management/src/run_model_download.py
@@ -37,8 +37,8 @@ if __name__ == "__main__":
     model_download_details = download_model(
         model_source=model_source, model_id=model_id, download_dir=model_output_dir
     )
-    print(f"Model files downloaded at: {model_output_dir} !!!")
+    print(f"Model files downloaded at: {model_output_dir} !!!!!")
 
     with open(model_download_metadata_path, "w") as f:
         json.dump(model_download_details, f)
-    print("Successfully persisted model info !!!")
+    print("Successfully persisted model info !!!!!")
