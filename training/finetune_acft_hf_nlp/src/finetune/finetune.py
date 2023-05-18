5532d0bf5144d746b74e34acbfb190956ec36856
SitaRam Chaitanya Kanakamedala <skanakamedal@microsoft.com>
2023-05-18 16:11:47 +0530

    Remove save_as_mlflow_model

--- training/finetune_acft_hf_nlp/src/finetune/finetune.py
+++ training/finetune_acft_hf_nlp/src/finetune/finetune.py
@@ -284,10 +284,6 @@ def get_parser():
     )
 
     parser.add_argument(
-        "--save_as_mlflow_model", type=str2bool, default="true", help="Save as mlflow model with pyfunc as flavour"
-    )
-
-    parser.add_argument(
         "--preprocess_output",
         default=None,
         type=str,
