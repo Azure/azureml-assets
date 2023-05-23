6135df15ed1abd8279e2d318a0bbf52e1224a219
Ayush Mishra <61145377+novaturient95@users.noreply.github.com>
2023-05-23 17:18:23 +0530

    Add Deepspeed MII deployment component

---
+++ common/src/deepspeed_mii_deployment/deploy.py
@@ -0,0 +1,381 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Run MII Model deployment module."""
+
+import argparse
+import json
+import logging
+import os
+import re
+import time
+from azure.ai.ml import MLClient
+from azure.ai.ml.entities import (
+    ManagedOnlineEndpoint,
+    ManagedOnlineDeployment,
+    OnlineRequestSettings,
+    CodeConfiguration,
+    ProbeSettings,
+)
+from azure.ai.ml.identity import AzureMLOnBehalfOfCredential
+from azure.identity import ManagedIdentityCredential
+from azureml.core.run import Run
+from pathlib import Path
+
+
+CODE = "script"
+SCORE = "predict.py"
+DEPLOYMENT_ENV_ASSET_URI = "azureml://registries/azureml/environments/python-sdk-v2/versions/1"
+
+# Env vars
+MODEL_MOUNT_PATH = "/var/azureml-model"
+AML_APP_ROOT = f"{MODEL_MOUNT_PATH}/{CODE}"
+
+# PROBE CONSTANTS
+PROBE_FAILURE_THRESHOLD = 50
+PROBE_SUCCESS_THRESHOLD = 50
+PROBE_TIMEOUT = 500
+PROBE_PERIOD = 500
+PROBE_INITIAL_DELAY = 500
+
+MAX_REQUEST_TIMEOUT = 90000
+MAX_INSTANCE_COUNT = 20
+
+
+def get_mlclient(registry_name=None):
+    """Return ML client."""
+    credential = AzureMLOnBehalfOfCredential()
+    try:
+        # Check if given credential can get token successfully.
+        credential.get_token("https://management.azure.com/.default")
+    except Exception as ex:
+        # Fall back to ManagedIdentityCredential in case AzureMLOnBehalfOfCredential not work
+        print(f"Failed to get obo credentials - {ex}")
+        msi_client_id = os.environ.get("DEFAULT_IDENTITY_CLIENT_ID")
+        credential = ManagedIdentityCredential(client_id=msi_client_id)
+    try:
+        credential.get_token("https://management.azure.com/.default")
+    except Exception as ex:
+        raise (f"Failed to get credentials : {ex}")
+
+    if registry_name:
+        return MLClient(
+            credential=credential,
+            registry_name=registry_name,
+        )
+
+    run = Run.get_context(allow_offline=False)
+    ws = run.experiment.workspace
+    mlclient = MLClient(
+        credential=credential,
+        subscription_id=ws._subscription_id,
+        resource_group_name=ws._resource_group,
+        workspace_name=ws._workspace_name,
+    )
+    return mlclient
+
+
+def create_endpoint_and_deployment(ml_client, model_id, endpoint_name, deployment_name, args):
+    """Create endpoint and deployment and return details."""
+    env_vars = {
+        "AML_APP_ROOT": AML_APP_ROOT,
+        "AZUREML_LOG_LEVEL": "DEBUG",
+        "LOG_IO": 1,
+        "WORKER_TIMEOUT": 2400
+    }
+    endpoint = ManagedOnlineEndpoint(name=endpoint_name, auth_mode="key")
+
+    code_configuration = CodeConfiguration(code=CODE, scoring_script=SCORE)
+    request_settings = OnlineRequestSettings(
+        max_concurrent_requests_per_instance=args.max_concurrent_requests_per_instance,
+        request_timeout_ms=args.request_timeout_ms,
+        max_queue_wait_ms=args.max_queue_wait_ms,
+    )
+
+    liveness_probe_settings = ProbeSettings(
+        failure_threshold=args.failure_threshold_liveness_probe,
+        timeout=args.timeout_liveness_probe,
+        period=args.period_liveness_probe,
+        initial_delay=args.initial_delay_liveness_probe,
+    )
+
+    readiness_probe_settings = ProbeSettings(
+        failure_threshold=args.failure_threshold_readiness_probe,
+        success_threshold=args.success_threshold_readiness_probe,
+        timeout=args.timeout_readiness_probe,
+        period=args.period_readiness_probe,
+        initial_delay=args.initial_delay_readiness_probe,
+    )
+
+    # deployment
+    deployment = ManagedOnlineDeployment(
+        name=deployment_name,
+        code_configuration=code_configuration,
+        name=deployment_name,
+        endpoint_name=endpoint_name,
+        environment=DEPLOYMENT_ENV_ASSET_URI,
+        environment_variables=env_vars,
+        endpoint_name=endpoint_name,
+        model=model_id,
+        instance_type=args.instance_type,
+        instance_count=args.instance_count,
+        request_settings=request_settings,
+        liveness_probe=liveness_probe_settings,
+        readiness_probe=readiness_probe_settings,
+        egress_public_network_access=args.egress_public_network_access,
+    )
+
+    try:
+        print(f"Creating endpoint {endpoint_name}")
+        ml_client.begin_create_or_update(endpoint).wait()
+    except Exception as e:
+        error_msg = f"Error occured while creating endpoint - {e}"
+        logging.error(error_msg)
+        raise Exception(error_msg)
+
+    try:
+        print(f"Creating deployment {deployment}")
+        ml_client.online_deployments.begin_create_or_update(deployment).wait()
+    except Exception as e:
+        error_msg = f"Error occured while creating deployment - {e}"
+        logging.error(error_msg)
+        raise Exception(error_msg)
+
+    print(f"Deployment successful. Updating endpoint to take 100% traffic for deployment {deployment_name}")
+
+    # deployment to take 100% traffic
+    endpoint.traffic = {deployment.name: 100}
+    try:
+        ml_client.begin_create_or_update(endpoint).wait()
+        endpoint = ml_client.online_endpoints.get(endpoint.name)
+    except Exception as e:
+        error_msg = f"Error occured while updating endpoint traffic - {e}"
+        logging.error(error_msg)
+        raise Exception(error_msg)
+
+    return endpoint, deployment
+
+
+def parse_args():
+    """Return arguments."""
+    parser = argparse.ArgumentParser()
+    # add arguments
+    parser.add_argument("--hf_model_id", type=str, required=True, help="Hugging face model id")
+    parser.add_argument("--hf_task_name", type=str, required=True, help="Hugging face model task")
+    parser.add_argument(
+        "--registration_details",
+        type=Path,
+        help="Json file that contains the ID of registered model to be deployed",
+    )
+    parser.add_argument(
+        "--inference_payload",
+        type=Path,
+        help="Json file with inference endpoint payload.",
+    )
+    parser.add_argument(
+        "--endpoint_name",
+        type=str,
+        help="Name of the endpoint",
+    )
+    parser.add_argument("--deployment_name", type=str, help="Name of the the deployment")
+    parser.add_argument(
+        "--instance_type",
+        type=str,
+        help="Compute instance type to deploy model",
+        default="Standard_NC24s_v3",
+    )
+    parser.add_argument(
+        "--instance_count",
+        type=int,
+        help="Number of compute instances to deploy model",
+        default=1,
+        choices=range(1, MAX_INSTANCE_COUNT),
+    )
+    parser.add_argument(
+        "--max_concurrent_requests_per_instance",
+        type=int,
+        default=1,
+        help="Maximum concurrent requests to be handled per instance",
+    )
+    parser.add_argument(
+        "--request_timeout_ms",
+        type=int,
+        default=60000,  # 1min
+        help="Request timeout in ms.",
+    )
+    parser.add_argument(
+        "--max_queue_wait_ms",
+        type=int,
+        default=60000,  # 1min
+        help="Maximum queue wait time of a request in ms",
+    )
+    parser.add_argument(
+        "--failure_threshold_readiness_probe",
+        type=int,
+        default=10,
+        help="No of times system will try after failing the readiness probe",
+        choices=range(1, PROBE_FAILURE_THRESHOLD),
+    )
+    parser.add_argument(
+        "--success_threshold_readiness_probe",
+        type=int,
+        default=1,
+        help="The minimum consecutive successes for the readiness probe to be considered successful, after fail",
+        choices=range(1, PROBE_SUCCESS_THRESHOLD),
+    )
+    parser.add_argument(
+        "--timeout_readiness_probe",
+        type=int,
+        default=10,
+        help="The number of seconds after which the readiness probe times out",
+        choices=range(1, PROBE_TIMEOUT)
+    )
+    parser.add_argument(
+        "--period_readiness_probe",
+        type=int,
+        default=10,
+        help="How often (in seconds) to perform the readiness probe",
+        choices=range(1, PROBE_PERIOD),
+    )
+    parser.add_argument(
+        "--initial_delay_readiness_probe",
+        type=int,
+        default=10,
+        help="The number of seconds after the container has started before the readiness probe is initiated",
+        choices=range(1, PROBE_INITIAL_DELAY),
+    )
+    parser.add_argument(
+        "--failure_threshold_liveness_probe",
+        type=int,
+        default=30,
+        help="No of times system will try after failing the liveness probe",
+        choices=range(1, PROBE_FAILURE_THRESHOLD),
+    )
+    parser.add_argument(
+        "--timeout_liveness_probe",
+        type=int,
+        default=10,
+        help="The number of seconds after which the liveness probe times out",
+        choices=range(1, PROBE_TIMEOUT),
+    )
+    parser.add_argument(
+        "--period_liveness_probe",
+        type=int,
+        default=10,
+        help="How often (in seconds) to perform the liveness probe",
+        choices=range(1, PROBE_PERIOD),
+    )
+    parser.add_argument(
+        "--initial_delay_liveness_probe",
+        type=int,
+        default=10,
+        help="The number of seconds after the container has started before the liveness probe is initiated",
+        choices=range(1, PROBE_INITIAL_DELAY),
+    )
+    parser.add_argument(
+        "--egress_public_network_access",
+        type=str,
+        default="enabled",
+        help="Secures the deployment by restricting interaction between deployment and Azure resources used by it",
+    )
+    parser.add_argument(
+        "--model_deployment_details",
+        type=str,
+        help="Json file to which deployment details will be written",
+    )
+    # parse args
+    args = parser.parse_args()
+    print("args received ", args)
+
+    # Validating passed input values
+    if args.max_concurrent_requests_per_instance < 1:
+        parser.error("Arg max_concurrent_requests_per_instance cannot be less than 1")
+    if args.request_timeout_ms < 1 or args.request_timeout_ms > MAX_REQUEST_TIMEOUT:
+        parser.error(f"Arg request_timeout_ms should lie between 1 and {MAX_REQUEST_TIMEOUT}")
+    if args.max_queue_wait_ms < 1 or args.max_queue_wait_ms > MAX_REQUEST_TIMEOUT:
+        parser.error(f"Arg max_queue_wait_ms should lie between 1 and {MAX_REQUEST_TIMEOUT}")
+
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    # Model params
+    hf_model_id = args.hf_model_id
+    hf_task_name = args.hf_task_name
+    registration_details_path = args.registration_details
+
+    print("##### logger.info args #####")
+    for arg, value in args.__dict__.items():
+        print(f"{arg} => {value}")
+
+    mlclient = get_mlclient()
+
+    model_info = {}
+    with open(args.registration_details) as f:
+        model_info = json.load(f)
+    model_id = model_info['id']
+    model_name = model_info['name']
+
+    # Endpoint has following restrictions:
+    # 1. Name must begin with lowercase letter
+    # 2. Followed by lowercase letters, hyphen or numbers
+    # 3. End with a lowercase letter or number
+
+    # 1. Replace underscores and slashes by hyphens and convert them to lower case.
+    # 2. Take 21 chars from model name and append '-' & timstamp(10chars) to it
+    endpoint_name = re.sub('[^A-Za-z0-9]', '-', model_name).lower()[:21]
+    endpoint_name = f"{endpoint_name}-{int(time.time())}"
+    endpoint_name = endpoint_name
+
+    endpoint_name = args.endpoint_name if args.endpoint_name else endpoint_name
+    deployment_name = args.deployment_name if args.deployment_name else "default"
+
+    scoring_details = {
+        "model_name": hf_model_id,
+        "task_name": hf_task_name,
+    }
+
+    # used in scoring script
+    dump_path = "code/model_details.json"
+    with open(dump_path, "w") as f:
+        json.dump(scoring_details, f)
+
+    endpoint, deployment = create_endpoint_and_deployment(
+        mlclient=mlclient,
+        endpoint_name=endpoint_name,
+        deployment_name=deployment_name,
+        model_id=model_id,
+        args=args
+    )
+
+    if args.inference_payload:
+        print("Invoking inference with test payload ...")
+        try:
+            response = mlclient.online_endpoints.invoke(
+                endpoint_name=endpoint_name,
+                deployment_name=deployment_name,
+                request_file=args.inference_payload,
+            )
+            print(f"Response:\n{response}")
+        except Exception as e:
+            print(f"Invocation failed with error: {e}")
+            raise e
+
+    print("Saving deployment details ...")
+
+    # write deployment details to file
+    endpoint_type = "aml_online_inference"
+    deployment_details = {
+        "endpoint_name": endpoint.name,
+        "deployment_name": deployment.name,
+        "endpoint_uri": endpoint.__dict__["_scoring_uri"],
+        "endpoint_type": endpoint_type,
+        "instance_type": args.instance_type,
+        "instance_count": args.instance_count,
+        "max_concurrent_requests_per_instance": args.max_concurrent_requests_per_instance,
+    }
+    json_object = json.dumps(deployment_details, indent=4)
+    with open(args.model_deployment_details, "w") as outfile:
+        outfile.write(json_object)
---
+++ common/src/deepspeed_mii_deployment/script/predict.py
@@ -0,0 +1,106 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Scoring script to infer Deepspeed MII deployed model."""
+
+import json
+import torch
+import mii
+from pathlib import Path
+
+
+model = None
+PARENT_DIR = Path().parent.resolve()
+MODEL_INFO_FILE = "model_details.json"
+
+
+def get_mii_configs():
+    """Return MII config."""
+    model_info_path = PARENT_DIR / MODEL_INFO_FILE
+    with open(model_info_path) as f:
+        model_info = json.load(f)
+
+    model_name = model_info["model_name"]
+    task_name = model_info["task_name"]
+
+    return {
+        'ds_config': None,
+        'ds_optimize': True,
+        'ds_zero': False,
+        'mii_configs': {
+            'checkpoint_dict': None,
+            'deploy_rank': [0],
+            'dtype': torch.float32,
+            'enable_cuda_graph': False,
+            'hf_auth_token': None,
+            'load_with_sys_mem': False,
+            'port_number': 50050,
+            'profile_model_time': False,
+            'replace_with_kernel_inject': True,
+            'skip_model_check': False,
+            'tensor_parallel': 1,
+            'torch_dist_port': 29500
+        },
+        'model_path': '',
+        'model_name': model_name,
+        'task_name': task_name,
+    }
+
+
+def init():
+    """Init MII server."""
+    print(f"init(): configs=> \n{configs}")
+    model_path = mii.utils.full_model_path(configs.get(mii.constants.MODEL_PATH_KEY, ""))
+
+    model_name = configs[mii.constants.MODEL_NAME_KEY]
+    task = configs[mii.constants.TASK_NAME_KEY]
+
+    assert model_name is not None, "The model name should be set before calling init"
+    assert task is not None, "The task name should be set before calling init"
+
+    mii.MIIServer(task,
+                  model_name,
+                  model_path,
+                  ds_optimize=configs[mii.constants.ENABLE_DEEPSPEED_KEY],
+                  ds_zero=configs[mii.constants.ENABLE_DEEPSPEED_ZERO_KEY],
+                  ds_config=configs[mii.constants.DEEPSPEED_CONFIG_KEY],
+                  mii_configs=configs[mii.constants.MII_CONFIGS_KEY])
+
+    global model
+    model = None
+
+    # In AML deployments both the GRPC client and server are used in the same process
+    if mii.utils.is_aml():
+        model = mii.MIIClient(task, mii_configs=configs[mii.constants.MII_CONFIGS_KEY])
+        print(f"Model:\n\n {model}\n\n")
+
+
+def run(request):
+    """Invoke MII query and return response."""
+    global model
+    assert model is not None, "grpc client has not been setup when this model was created"
+    request_dict = json.loads(request)
+
+    print(f"request_dict {request_dict}\n\n")
+    query_dict = mii.utils.extract_query_dict(configs[mii.constants.TASK_NAME_KEY],
+                                              request_dict)
+    print(f"query_dict {query_dict}\n\n")
+
+    response = model.query(query_dict, **request_dict)
+    time_taken = response.time_taken
+
+    print(f"type(response): {type(response)}")
+    if hasattr(response, "response"):
+        print(f"type(response.response) => {type(response.response)}")
+    if not isinstance(response.response, str):
+        response = [r for r in response.response]
+    if not isinstance(response.response, str):
+        response = [r for r in response.response]
+    result_dict = {'responses': response.response, 'time': time_taken}
+    print(result_dict)
+    return json.dumps(result_dict)
+
+
+configs = get_mii_configs()
+print(configs)
