582e1d70e322218fe8d5b2007eacf270119c6261
Mathieu St-Louis <mastloui@microsoft.com>
2023-05-24 22:57:17 -0700

    Change copyright for run_metrics_utils

--- model_monitoring/components/src/shared_utilities/run_metrics_utils.py
+++ model_monitoring/components/src/shared_utilities/run_metrics_utils.py
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 """Contains functionality for publishing run metrics."""
 import mlflow
