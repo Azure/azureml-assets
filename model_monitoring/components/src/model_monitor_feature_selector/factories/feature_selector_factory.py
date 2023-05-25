6e34b9cefe6d82e5832de099895d732198373111
Mathieu St-Louis <mastloui@microsoft.com>
2023-05-24 23:43:42 -0700

    Linter error

--- model_monitoring/components/src/model_monitor_feature_selector/factories/feature_selector_factory.py
+++ model_monitoring/components/src/model_monitor_feature_selector/factories/feature_selector_factory.py
@@ -37,8 +37,8 @@ class FeatureSelectorFactory:
         elif feature_selector_type == FeatureSelectorType.TOP_N_BY_ATTRIBUTION.name:
             if not filter_value.isdigit():
                 raise ValueError(
-                    f"Invalid feature value. Please provide a valid integer value"
-                    + " when leveraging '{FeatureSelectorType.TOP_N_BY_ATTRIBUTION.name}'."
+                    "Invalid feature value. Please provide a valid integer value"
+                    + f" when leveraging '{FeatureSelectorType.TOP_N_BY_ATTRIBUTION.name}'."
                 )
             return FeatureSelectorTopNByAttribution(
                 filter_value=int(filter_value), feature_importance=feature_importance
