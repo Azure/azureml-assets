5f4d4c0bb22a385a8f32b94f251d09f4f5addf40
Mathieu St-Louis <mastloui@microsoft.com>
2023-05-24 23:05:05 -0700

    Change copyright for patch_mltable

--- model_monitoring/components/src/shared_utilities/patch_mltable.py
+++ model_monitoring/components/src/shared_utilities/patch_mltable.py
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 """Contains functionality to patch pyspark classes for MLTable write support."""
 
 
