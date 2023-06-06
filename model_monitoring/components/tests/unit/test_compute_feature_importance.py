8c974e8a85d8537136ded2310809903cd6bfde6a
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-06-06 13:02:42 -0700

    add header

--- model_monitoring/components/tests/unit/test_compute_feature_importance.py
+++ model_monitoring/components/tests/unit/test_compute_feature_importance.py
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 """This file contains unit tests for the Data Drift Output Metrics component."""
 
