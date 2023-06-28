851393d97c193088a40c40a81d9ae445d87e00a7
Gerard Woods <gewoods@microsoft.com>
2023-06-28 16:39:57 -0700

    Insert LLM Code RAG code

---
+++ assets/large_language_models/rag/components/src/embeddings/tasks/__init__.py
@@ -0,0 +1,4 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""Tasks for embedding components."""
