ac62e7c238d25fd76ca00d5c654d56121d2bdc3b
Ayush Mishra <61145377+novaturient95@users.noreply.github.com>
2023-06-29 01:57:28 +0530

    Add model-mgmt UT

---
+++ assets/training/model_management/tests/unittests/test_downloader.py
@@ -0,0 +1,135 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Test download."""
+
+import unittest
+from unittest.mock import patch
+from pathlib import Path
+from azureml.model.mgmt.downloader.downloader import (
+    HuggingfaceDownloader,
+    GITDownloader,
+    AzureBlobstoreDownloader,
+    download_model,
+)
+from azureml.model.mgmt.utils.common_utils import create_namespace_from_dict
+
+
+class TestDownloaders(unittest.TestCase):
+    def test_huggingface_downloader(self):
+        model_id = "username/model_name"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("huggingface_hub.hf_api.HfApi") as MockHfApi, \
+                patch("azureml.model.mgmt.downloader.download_utils._download_git_model") as MockGITDownloader:
+            mock_api = MockHfApi.return_value
+            mock_api.list_models.return_value = [
+                create_namespace_from_dict({
+                    "modelId": "username/model_name",
+                    "pipeline_tag": "task_name",
+                    "tags": ["tag1", "tag2", "tag3"],
+                    "sha": "123456789",
+                })
+            ]
+
+            downloader = HuggingfaceDownloader(model_id)
+            downloader._hf_api = mock_api
+            downloader.download_model(download_dir)
+
+            self.assertEqual(downloader.model_info.modelId, model_id)
+            self.assertEqual(downloader.model_info.pipeline_tag, "task_name")
+            self.assertEqual(downloader.model_info.sha, "123456789")
+            self.assertEqual(mock_api.list_models.call_count, 1)
+            self.assertEqual(MockGITDownloader.call_count, 1)
+
+    def test_git_downloader(self):
+        model_uri = "https://github.com/some_model"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("azureml.model.mgmt.downloader.download_utils._download_git_model"):
+            downloader = GITDownloader(model_uri)
+            download_details = downloader.download_model(download_dir)
+
+            self.assertEqual(download_details["name"], "some_model")
+            self.assertEqual(download_details["tags"], {})
+            self.assertEqual(download_details["properties"], {})
+
+    def test_azure_blobstore_downloader(self):
+        model_uri = "https://blobstorageaccount.blob.core.windows.net/models/model_folder"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("azureml.model.mgmt.downloader.download_utils._download_azure_artifacts"):
+            downloader = AzureBlobstoreDownloader(model_uri)
+            download_details = downloader.download_model(download_dir)
+
+            self.assertEqual(download_details["name"], "model_folder")
+            self.assertEqual(download_details["tags"], {})
+            self.assertEqual(download_details["properties"], {})
+
+
+class TestDownloadModel(unittest.TestCase):
+    def test_download_model_with_huggingface_source(self):
+        model_source = "Huggingface"
+        model_id = "test_model_id"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("azureml.model.mgmt.downloader.downloader.HuggingfaceDownloader") as MockDownloader:
+            mock_downloader = MockDownloader.return_value
+            mock_downloader.download_model.return_value = {
+                "name": "model_name",
+                "tags": {"tag1": "value1"},
+                "properties": {"prop1": "value1"},
+            }
+
+            result = download_model(model_source, model_id, download_dir)
+
+            self.assertEqual(result, {
+                "name": "model_name",
+                "tags": {"tag1": "value1"},
+                "properties": {"prop1": "value1"},
+            })
+            mock_downloader.download_model.assert_called_once_with(download_dir)
+
+    def test_download_model_with_git_source(self):
+        model_source = "GIT"
+        model_id = "https://github.com/some_model"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("azureml.model.mgmt.downloader.downloader.GITDownloader") as MockDownloader:
+            mock_downloader = MockDownloader.return_value
+            mock_downloader.download_model.return_value = {
+                "name": "some_model",
+                "tags": {"tag2": "value2"},
+                "properties": {"prop2": "value2"},
+            }
+
+            result = download_model(model_source, model_id, download_dir)
+
+            self.assertEqual(result, {
+                "name": "some_model",
+                "tags": {"tag2": "value2"},
+                "properties": {"prop2": "value2"},
+            })
+            mock_downloader.download_model.assert_called_once_with(download_dir)
+
+    def test_download_model_with_azureblob_source(self):
+        model_source = "AzureBlob"
+        model_id = "https://blobstorageaccount.blob.core.windows.net/models/model_folder"
+        download_dir = Path("path/to/download/dir")
+
+        with patch("azureml.model.mgmt.downloader.downloader.AzureBlobstoreDownloader") as MockDownloader:
+            mock_downloader = MockDownloader.return_value
+            mock_downloader.download_model.return_value = {
+                "name": "model_folder",
+                "tags": {"tag3": "value3"},
+                "properties": {"prop3": "value3"},
+            }
+
+            result = download_model(model_source, model_id, download_dir)
+
+            self.assertEqual(result, {
+                "name": "model_folder",
+                "tags": {"tag3": "value3"},
+                "properties": {"prop3": "value3"},
+            })
+            mock_downloader.download_model.assert_called_once_with(download_dir)
---
+++ assets/training/model_management/tests/unittests/test_preprocess.py
@@ -0,0 +1,100 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Test preprocess."""
+
+import unittest
+from unittest.mock import patch
+from azureml.model.mgmt.processors.transformers.factory import (
+    get_mlflow_convertor,
+    NLPMLflowConvertorFactory,
+    VisionMLflowConvertorFactory,
+    ASRMLflowConvertorFactory,
+    DiffusersMLflowConvertorFactory,
+    SupportedNLPTasks,
+    SupportedVisionTasks,
+    SupportedDiffusersTask,
+    SupportedTasks,
+)
+
+
+class TestFactoryModule(unittest.TestCase):
+
+    @patch('azureml.model.mgmt.processors.transformers.factory.NLPMLflowConvertorFactory')
+    def test_get_nlp_mlflow_convertor(self, mock_nlp_factory):
+        model_dir = "/path/to/model_dir"
+        output_dir = "/path/to/output_dir"
+        temp_dir = "/path/to/temp_dir"
+
+        translate_params = {"task": SupportedNLPTasks.FILL_MASK.value}
+        mock_convertor = mock_nlp_factory.create_mlflow_convertor.return_value
+        result = get_mlflow_convertor(model_dir, output_dir, temp_dir, translate_params)
+        self.assertEqual(result, mock_convertor)
+        mock_nlp_factory.create_mlflow_convertor.assert_called_once_with(
+            model_dir,
+            output_dir,
+            temp_dir,
+            translate_params,
+        )
+
+    @patch('azureml.model.mgmt.processors.transformers.factory.VisionMLflowConvertorFactory')
+    def test_get_nlp_mlflow_convertor(self, mock_vision_factory):
+        model_dir = "/path/to/model_dir"
+        output_dir = "/path/to/output_dir"
+        temp_dir = "/path/to/temp_dir"
+
+        translate_params = {"task": SupportedVisionTasks.IMAGE_CLASSIFICATION.value}
+        mock_convertor = mock_vision_factory.create_mlflow_convertor.return_value
+        result = get_mlflow_convertor(model_dir, output_dir, temp_dir, translate_params)
+        self.assertEqual(result, mock_convertor)
+        mock_vision_factory.create_mlflow_convertor.assert_called_once_with(
+            model_dir,
+            output_dir,
+            temp_dir,
+            translate_params,
+        )
+
+    @patch('azureml.model.mgmt.processors.transformers.factory.DiffusersMLflowConvertorFactory')
+    def test_get_diffusers_mlflow_convertor(self, mock_diffusers_factory):
+        model_dir = "/path/to/model_dir"
+        output_dir = "/path/to/output_dir"
+        temp_dir = "/path/to/temp_dir"
+
+        translate_params = {"task": SupportedDiffusersTask.TEXT_TO_IMAGE.value}
+        mock_convertor = mock_diffusers_factory.create_mlflow_convertor.return_value
+        result = get_mlflow_convertor(model_dir, output_dir, temp_dir, translate_params)
+        self.assertEqual(result, mock_convertor)
+        mock_diffusers_factory.create_mlflow_convertor.assert_called_once_with(
+            model_dir,
+            output_dir,
+            temp_dir,
+            translate_params,
+        )
+
+    @patch('azureml.model.mgmt.processors.transformers.factory.ASRMLflowConvertorFactory')
+    def test_get_asr_mlflow_convertor(self, mock_asr_factory):
+        model_dir = "/path/to/model_dir"
+        output_dir = "/path/to/output_dir"
+        temp_dir = "/path/to/temp_dir"
+
+        translate_params = {"task": SupportedTasks.AUTOMATIC_SPEECH_RECOGNITION.value}
+        mock_convertor = mock_asr_factory.create_mlflow_convertor.return_value
+        result = get_mlflow_convertor(model_dir, output_dir, temp_dir, translate_params)
+        self.assertEqual(result, mock_convertor)
+        mock_asr_factory.create_mlflow_convertor.assert_called_once_with(
+            model_dir,
+            output_dir,
+            temp_dir,
+            translate_params,
+        )
+
+    def test_get_mlflow_convertor_unsupported_task(self):
+        model_dir = "/path/to/model_dir"
+        output_dir = "/path/to/output_dir"
+        temp_dir = "/path/to/temp_dir"
+        translate_params = {"task": "unsupported_task"}
+
+        with self.assertRaises(Exception) as context:
+            get_mlflow_convertor(model_dir, output_dir, temp_dir, translate_params)
+
+        self.assertTrue("unsupported_task" in str(context.exception))
