0a4c417a4666702f8c288ac5e5546fb691120444
Mathieu St-Louis <mastloui@microsoft.com>
2023-07-03 08:48:38 -0700

    update

--- assets/model_monitoring/components/src/shared_utilities/run_metrics_utils.py
+++ assets/model_monitoring/components/src/shared_utilities/run_metrics_utils.py
@@ -9,8 +9,6 @@ import os
 def _get_experiment_id():
     return os.environ.get("MLFLOW_EXPERIMENT_ID")
 
-def _get_current_run_id():
-    return os.environ.get("MLFLOW_RUN_ID")
 
 def _create_filter_query(
     monitor_name: str, signal_name: str, feature_name: str, metric_name: str
