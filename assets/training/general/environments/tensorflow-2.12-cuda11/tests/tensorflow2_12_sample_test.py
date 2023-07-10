5c971d32b08adddc6861d9962aa08231b1c9ae69
Ta, Kevin <kevin.ta@intel.com>
2023-07-10 14:03:43 -0700

    compiled the IPEX and TF-oneDNN (cpu) into a single test script because pytest wasn't running when it was separated

--- assets/training/general/environments/tensorflow-2.12-cuda11/tests/tensorflow2_12_sample_test.py
+++ assets/training/general/environments/tensorflow-2.12-cuda11/tests/tensorflow2_12_sample_test.py
@@ -76,3 +76,66 @@ def test_tensorflow_2_12():
             ml_client.jobs.stream(returned_job.name)
 
     assert status == JobStatus.COMPLETED
+
+
+def test_tensorflow_2_12_cpu():
+    """Tests a sample job using tensorflow 2.12 as the environment."""
+    this_dir = Path(__file__).parent
+
+    subscription_id = os.environ.get("subscription_id")
+    resource_group = os.environ.get("resource_group")
+    workspace_name = os.environ.get("workspace")
+
+    ml_client = MLClient(
+        AzureCliCredential(), subscription_id, resource_group, workspace_name
+    )
+
+    env_name = "tensorflow-2_12-cuda11"
+
+    env_docker_context = Environment(
+        build=BuildContext(path=this_dir / BUILD_CONTEXT),
+        name=env_name,
+        description="Tensorflow 2.12 environment created from a Docker context.",
+    )
+    ml_client.environments.create_or_update(env_docker_context)
+
+    # create the command
+    job = command(
+        code=this_dir / JOB_SOURCE_CODE,  # local path where the code is stored
+        command="pip install -r requirements.txt" \
+                " && python tf_bert.py",
+        environment=f"{env_name}@latest",
+        compute=os.environ.get("cpu_cluster"),
+        display_name="tensorflow-bert-example",
+        description="A test run of the tensorflow 2_12 curated environment",
+        experiment_name="tensorflow212Experiment"
+    )
+
+    returned_job = ml_client.create_or_update(job)
+    assert returned_job is not None
+
+    # Poll until final status is reached or timed out
+    timeout = time.time() + (TIMEOUT_MINUTES * 60)
+    while time.time() <= timeout:
+        job = ml_client.jobs.get(returned_job.name)
+        status = job.status
+        if status in [JobStatus.COMPLETED, JobStatus.FAILED]:
+            break
+        time.sleep(30)  # sleep 30 seconds
+    else:
+        # Timeout
+        ml_client.jobs.cancel(returned_job.name)
+        raise Exception(f"Test aborted because the job took longer than {TIMEOUT_MINUTES} minutes. "
+                        f"Last status was {status}.")
+
+    if status == JobStatus.FAILED:
+        ml_client.jobs.download(returned_job.name)
+        if STD_LOG.exists():
+            print(f"*** BEGIN {STD_LOG} ***")
+            with open(STD_LOG, "r") as f:
+                print(f.read(), end="")
+            print(f"*** END {STD_LOG} ***")
+        else:
+            ml_client.jobs.stream(returned_job.name)
+
+    assert status == JobStatus.COMPLETED
