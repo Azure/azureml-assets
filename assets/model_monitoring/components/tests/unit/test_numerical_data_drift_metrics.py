8c524a6d8f385492cc23265bf98ea31a18c93518
Ye Tao <yeta@microsoft.com>
2023-07-14 00:38:02 -0700

    fix params

--- assets/model_monitoring/components/tests/unit/test_numerical_data_drift_metrics.py
+++ assets/model_monitoring/components/tests/unit/test_numerical_data_drift_metrics.py
@@ -10,43 +10,40 @@ import pytest
 import numpy as np
 import unittest
 
+test_cases = [
+                [100_000, 100_000, 50, 50, 0],
+                [100_000, 10_000, 50, 50, 0.19963],
+                [100_000, 100_000, 50, 100, 3.3238],
+                [100_000, 100_000, 50, 250, 13.3184],
+             ]
 
 @pytest.mark.unit
 class TestComputeDataDriftMetrics(unittest.TestCase):
     """Test class for data drift compute metrics component component and utilities."""
-
-    @pytest.mark.parameterized("x_n_obs,y_n_obs,x_mean,y_mean,expected", [
-        (100_000, 100_000, 100_000, 100_000),
-        (100_000, 10_000, 100_000, 100_000),
-        (50, 50, 50, 50),
-        (50, 50, 100, 250),
-        (0, 0.19963, 3.3238, 13.3184)])
-    def test_compute_numerical_data_drift_metrics_normalized_wasserstein_distance(
-        self, x_n_obs, y_n_obs, x_mean, y_mean, expected
-    ):
+    def test_compute_numerical_data_drift_metrics_normalized_wasserstein_distance(self):
         """Test compute normalized wasserstein distance for numerical metrics."""
-        std_dev = 15
-        column_values = ['column1']
-        numerical_threshold = 100
+        for x_n_obs, y_n_obs, x_mean, y_mean, expected in test_cases:
+            std_dev = 15
+            column_values = ['column1']
+            numerical_threshold = 100
 
-        x = np.random.normal(x_mean, std_dev, x_n_obs)
-        y = np.random.normal(y_mean, std_dev, y_n_obs)
+            x = np.random.normal(x_mean, std_dev, x_n_obs)
+            y = np.random.normal(y_mean, std_dev, y_n_obs)
 
-        x_pd_df = pd.DataFrame(data=x, columns=column_values)
-        y_pd_df = pd.DataFrame(data=y, columns=column_values)
+            x_pd_df = pd.DataFrame(data=x, columns=column_values)
+            y_pd_df = pd.DataFrame(data=y, columns=column_values)
 
-        spark = init_spark()
+            spark = init_spark()
 
-        x_df = spark.createDataFrame(x_pd_df)
-        y_df = spark.createDataFrame(y_pd_df)
+            x_df = spark.createDataFrame(x_pd_df)
+            y_df = spark.createDataFrame(y_pd_df)
 
-        output_df = compute_numerical_data_drift_measures_tests(
-            x_df,
-            y_df,
-            x_df.count(),
-            y_df.count(),
-            "NormalizedWassersteinDistance",
-            column_values,
-            numerical_threshold)
-
-        self.assertAlmostEqual(float(expected), output_df['column1'], 4)
+            output_df = compute_numerical_data_drift_measures_tests(
+                x_df,
+                y_df,
+                x_df.count(),
+                y_df.count(),
+                "NormalizedWassersteinDistance",
+                column_values,
+                numerical_threshold)
+            self.assertAlmostEqual(float(expected), output_df['column1'], 4)
