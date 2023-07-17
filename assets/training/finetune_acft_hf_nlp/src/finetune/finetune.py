275a3c5b76cf7c5418118bc6a9440b26cc72ad2b
Pavan Manoj Jonnalagadda <pavanmanojj@microsoft.com>
2023-07-17 21:24:06 +0530

    commenting the falcon model type

--- assets/training/finetune_acft_hf_nlp/src/finetune/finetune.py
+++ assets/training/finetune_acft_hf_nlp/src/finetune/finetune.py
@@ -85,7 +85,7 @@ IGNORE_MISMATCHED_SIZES_FALSE_MODELS = [
     HfModelTypes.LLAMA,
     HfModelTypes.GPT_NEOX,   # dolly
     HfModelTypes.FALCON,
-    HfModelTypes.REFINEDWEBMODEL,   # falcon
+    # HfModelTypes.REFINEDWEBMODEL,   # falcon
 ]
 
 
