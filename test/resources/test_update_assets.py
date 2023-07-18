6750cc351878165dd06d81f157f6c12ac77beec3
Louie Larson <lolarson@microsoft.com>
2023-07-18 16:03:44 -0400

    Make output directory optional

--- test/resources/test_update_assets.py
+++ test/resources/test_update_assets.py
@@ -16,21 +16,18 @@ RESOURCES_DIR = Path("resources/update")
 
 
 @pytest.mark.parametrize(
-    "test_subdir,skip_unreleased,create_tag,use_release_dir,use_output_dir",
+    "test_subdir,skip_unreleased,create_tag,use_release_dir",
     [
-        ("in-subdir", False, True, True, True),
-        ("in-parent-dir", False, False, True, True),
-        ("in-place", False, True, True, False),
-        ("in-place-no-release-dir", False, False, False, False),
-        ("manual-version", False, True, True, True),
-        ("manual-version-unreleased", False, False, True, True),
-        ("manual-version-unreleased-skip", True, False, True, True),
-        ("manual-version-no-release-dir", False, False, False, True),
-        ("with-description", False, True, True, True),
+        ("in-subdir", False, True, True),
+        ("in-parent-dir", False, False, True),
+        ("manual-version", False, True, True),
+        ("manual-version-unreleased", False, False, True),
+        ("manual-version-unreleased-skip", True, False, True),
+        ("manual-version-no-release-dir", False, False, False),
+        ("with-description", False, True, True),
     ]
 )
-def test_update_assets(test_subdir: str, skip_unreleased: bool, create_tag: bool, use_release_dir: bool,
-                       use_output_dir: bool):
+def test_update_assets(test_subdir: str, skip_unreleased: bool, create_tag: bool, use_release_dir: bool):
     """Test update_assets function.
 
     Args:
@@ -38,7 +35,6 @@ def test_update_assets(test_subdir: str, skip_unreleased: bool, create_tag: bool
         skip_unreleased (bool): Value to pass to update_assets
         create_tag (bool): Create release tag in temp repo
         use_release_dir (bool): Use release directory
-        use_output_dir (bool): Use output directory
     """
     this_dir = Path(__file__).parent
     test_dir = this_dir / RESOURCES_DIR / test_subdir
@@ -72,14 +68,6 @@ def test_update_assets(test_subdir: str, skip_unreleased: bool, create_tag: bool
                 asset_config = util.find_assets(input_dirs=temp_release_path)[0]
                 repo.create_tag(asset_config.full_name)
 
-        if use_output_dir:
-            input_dirs = main_dir
-            output_directory_root = temp_output_path
-        else:
-            shutil.copytree(main_dir, temp_output_path, dirs_exist_ok=True)
-            input_dirs = temp_output_path
-            output_directory_root = None
-
         # Create updatable expected dir
         if expected_dir.exists():
             shutil.copytree(expected_dir, temp_expected_path, dirs_exist_ok=True)
@@ -88,8 +76,8 @@ def test_update_assets(test_subdir: str, skip_unreleased: bool, create_tag: bool
                 assets.pin_env_files(expected_asset_config.extra_config_as_object())
 
         release_directory_root = temp_release_path if use_release_dir else None
-        assets.update_assets(input_dirs=input_dirs, asset_config_filename=assets.DEFAULT_ASSET_FILENAME,
-                             output_directory_root=output_directory_root, release_directory_root=release_directory_root,
+        assets.update_assets(input_dirs=main_dir, asset_config_filename=assets.DEFAULT_ASSET_FILENAME,
+                             output_directory_root=temp_output_path, release_directory_root=release_directory_root,
                              skip_unreleased=skip_unreleased, use_version_dirs=False)
 
         assert util.are_dir_trees_equal(temp_output_path, temp_expected_path, True)
