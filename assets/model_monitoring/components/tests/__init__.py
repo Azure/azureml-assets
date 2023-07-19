820e584e4bbf96e4629e2fdeece42b0d26a6cca6
Ye Tao <yeta@microsoft.com>
2023-07-18 17:41:37 -0700

    fix imports in unit tests

--- assets/model_monitoring/components/tests/__init__.py
+++ assets/model_monitoring/components/tests/__init__.py
@@ -10,3 +10,8 @@ from pathlib import Path
 # Adding model monitoring component source to path.
 src_path = os.path.join(Path(os.path.dirname(__file__)).parents[0], "src", "")
 sys.path.append(src_path)
+
+for file in os.listdir(src_path):
+    if file != "shared_utilities":
+        sub_path = os.path.join(src_path, file, "")
+        sys.path.append(sub_path)
