fa19ab531d6c88cbe7f011866e9fbfe5be32de90
somitguptaMSFT <sogupta@microsoft.com>
2023-07-27 17:39:51 -0700

    adding copyright notice

--- assets/model_monitoring/components/src/token_statistics_compute_metrics/compute_token_statistics_metrics.py
+++ assets/model_monitoring/components/src/token_statistics_compute_metrics/compute_token_statistics_metrics.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import uuid
 from pyspark.context import SparkContext
 from pyspark.sql.functions import avg, col, count, lit, sum, udf
