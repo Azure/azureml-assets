a4b9d5e5f4225eaaf19456d23b85716a7aa4f708
Raoxiang <raoxiang1996@gmail.com>
2023-07-31 23:19:12 +0800

    upgrade package

--- assets/large_language_models/dbcopilot/components/src/endpoint_deployment.py
+++ assets/large_language_models/dbcopilot/components/src/endpoint_deployment.py
@@ -7,44 +7,18 @@ import os
 import shutil
 import tempfile
 from dataclasses import asdict
-from typing import Dict
 
-from azure.ai.ml.entities import (
-    CodeConfiguration,
-    Environment,
-    IdentityConfiguration,
-    ManagedIdentityConfiguration,
-    ManagedOnlineDeployment,
-    ManagedOnlineEndpoint,
-    OnlineRequestSettings,
-)
-from azureml.rag.utils.connections import (
-    get_connection_by_id_v2,
-    workspace_connection_to_credential,
-)
-from component_base import OBOComponentBase, main_entry_point
+from component_base import main_entry_point
 from db_copilot_tool.contracts.db_copilot_config import DBCopilotConfig
-
+from endpoint_deployment_base import EndpointDeploymentBase
 
 @main_entry_point("deploy")
-class EndpointDeployment(OBOComponentBase):
+class EndpointDeployment(EndpointDeploymentBase):
     """EndpointDeployment Class."""
 
     def __init__(self) -> None:
         """Initialize the class."""
         super().__init__()
-
-    parameter_type_mapping: Dict[str, str] = {
-        "grounding_embedding_uri": "uri_folder",
-        "example_embedding_uri": "uri_folder",
-        "db_context_uri": "uri_folder",
-    }
-
-    parameter_mode_mapping: Dict[str, str] = {
-        "db_context_uri": "direct",
-        "grounding_embedding_uri": "direct",
-        "example_embedding_uri": "direct",
-    }
 
     def deploy(
         self,
@@ -64,70 +38,18 @@ class EndpointDeployment(OBOComponentBase):
         max_text_length: int = None,
         tools: str = None,
         temperature: float = 0.0,
+        top_p: float = 0.0,
+        knowledge_pieces: str = None,
     ):
         """deploy_endpoint."""
+        from utils.asset_utils import get_datastore_uri
+
         workspace = self.workspace
         # find datastore uri
-        from utils.asset_utils import get_datastore_uri, get_full_env_path
 
         datastore_uri = get_datastore_uri(workspace, asset_uri)
         logging.info(f"Datastore uri: {datastore_uri}")
-        embedding_connection_id = os.environ.get(
-            "AZUREML_WORKSPACE_CONNECTION_ID_AOAI_EMBEDDING", None
-        )
-        chat_connection_id = os.environ.get(
-            "AZUREML_WORKSPACE_CONNECTION_ID_AOAI_CHAT", None
-        )
-        secrets = {}
-        if (
-            embedding_connection_id is not None
-            and embedding_connection_id != ""
-            and chat_connection_id is not None
-            and chat_connection_id != ""
-        ):
-            for connection_type, connection_id in {
-                "embedding": embedding_connection_id,
-                "chat": chat_connection_id,
-            }.items():
-                connection = get_connection_by_id_v2(connection_id)
-                credential = workspace_connection_to_credential(connection)
-                if hasattr(credential, "key"):
-                    secrets.update(
-                        {
-                            f"{connection_type}-OPENAI-API-Key": credential.key,
-                            f"{connection_type}-OPENAI-API-BASE": connection[
-                                "properties"
-                            ].get("target", {}),
-                        }
-                    )
-                    logging.info("Using workspace connection key for OpenAI")
-        if secrets == {}:
-            keyvault = workspace.get_default_keyvault()
-            secrets = keyvault.get_secrets(
-                secrets=["OPENAI-API-Key", "OPENAI-API-BASE"]
-            )
-            secrets = {
-                f"{secret_type}-{key}": value
-                for key, value in secrets.items()
-                for secret_type in {"embedding", "chat"}
-            }
-            logging.info("Using keyvault for OpenAI")
-
-        secrets_dict = {
-            "embedding-aoai-api-key": secrets["embedding-OPENAI-API-Key"],
-            "embedding-aoai-api-base": secrets["embedding-OPENAI-API-BASE"],
-            "chat-aoai-api-key": secrets["chat-OPENAI-API-Key"],
-            "chat-aoai-api-base": secrets["chat-OPENAI-API-BASE"],
-        }
-        if selected_tables:
-            selected_tables = json.loads(selected_tables)
-            if not isinstance(selected_tables, list):
-                raise ValueError("selected_tables must be a list")
-
-        if tools:
-            tools = json.loads(tools)
-            if not isinstance(tools, list):
-                raise ValueError("tools must be a dict")
+        secrets_dict = self.get_secrets()
 
         config = DBCopilotConfig(
             grounding_embedding_uri=grounding_embedding_uri,
@@ -144,69 +66,18 @@ class EndpointDeployment(OBOComponentBase):
             max_text_length=max_text_length,
             tools=tools,
             temperature=temperature,
+            top_p=top_p,
+            knowledge_pieces=knowledge_pieces,
         )
         logging.info(f"DBCopilotConfig: {config}")
-        mir_environment = get_full_env_path(self.mlclient_credential, mir_environment)
+        # mir_environment = get_full_env_path(self.mlclient_credential, mir_environment)
         with tempfile.TemporaryDirectory() as code_dir:
             logging.info("code_dir: %s", code_dir)
             current_dir = os.path.dirname(os.path.abspath(__file__))
-            shutil.copytree(
-                os.path.join(current_dir, "db_copilot_mir/code"),
-                code_dir,
-                dirs_exist_ok=True,
-            )
+            shutil.copytree(os.path.join(current_dir, "db_copilot_mir/code"), code_dir, dirs_exist_ok=True)
             with open(os.path.join(code_dir, "secrets.json"), "w") as f:
                 json.dump(secrets_dict, f)
             logging.info("dumped secrets to secrets.json")
             with open(os.path.join(code_dir, "db_copilot_config.json"), "w") as f:
                 json.dump(asdict(config), f)
-            endpoint = ManagedOnlineEndpoint(
-                name=endpoint_name,
-                description="DB CoPilot MIR endpoint",
-                auth_mode="key",
-                identity=IdentityConfiguration(
-                    type="UserAssigned",
-                    user_assigned_identities=[
-                        ManagedIdentityConfiguration(client_id=None)
-                    ],
-                ),
-            )
-            env = (
-                mir_environment
-                if mir_environment.startswith("azureml://")
-                else Environment(image=mir_environment)
-            )
-            logging.info("Environment: %s", env)
-            deployment = ManagedOnlineDeployment(
-                name=deployment_name,
-                endpoint_name=endpoint_name,
-                environment=env,
-                environment_variables={
-                    "EMBEDDING_STORE_LOCAL_CACHE_PATH": "/tmp/embedding_store_cache"
-                },
-                code_configuration=CodeConfiguration(
-                    code=code_dir,
-                    scoring_script="score.py",
-                ),
-                instance_type="Standard_DS3_v2",
-                instance_count=1,
-                request_settings=OnlineRequestSettings(request_timeout_ms=90000),
-            )
-            logging.info("Deployment: %s", deployment)
-            ml_client = self.ml_client
-            try:
-                ml_client.online_endpoints.get(endpoint_name)
-            except Exception:
-                logging.info(f"Creating endpoint {endpoint_name}")
-                endpoint_poller = ml_client.online_endpoints.begin_create_or_update(
-                    endpoint, local=False
-                )
-                endpoint_poller.result()
-                logging.info(f"Created endpoint {endpoint_name}")
-            deployment_poller = ml_client.online_deployments.begin_create_or_update(
-                deployment=deployment, local=False
-            )
-            deployment_result = deployment_poller.result()
-            logging.info(
-                f"Created deployment {deployment_name}. Result: {deployment_result}"
-            )
+            self._deploy_endpoint(mir_environment, endpoint_name, deployment_name, code_dir)
--- assets/large_language_models/dbcopilot/components/src/example_grounding.py
+++ assets/large_language_models/dbcopilot/components/src/example_grounding.py
@@ -1,16 +1,15 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 """The component generates the ground truth for the examples."""
-import csv
 import json
-import logging
 import os
-from dataclasses import asdict
 from typing import Dict
 
-import pandas as pd
 from component_base import ComponentBase, main_entry_point
-
+from db_copilot.contract.db_core import DatabaseType
+from db_copilot_tool.contracts.embedding_config import EmbeddingConfig
+from db_copilot_tool.tools.dummy_embedding_service import DummyEmbeddingService
+from db_copilot_tool.tools.in_context_learning_agent import InContextLearningAgent
 
 @main_entry_point("ground")
 class ExampleGrounding(ComponentBase):
@@ -29,80 +28,32 @@ class ExampleGrounding(ComponentBase):
     def ground(
         self,
         output_chunk_file: str,
-        database_type: str = "sqlserver",
+        grounding_context: str,
+        include_builtin: bool = True,
         tools: str = None,
         sample_folder: str = None,
-        grounding_context: str = None,
     ):
-        """ground_examples."""
-        from db_copilot.contract.chat_core import InContextExample
-        from db_copilot.contract.db_core import DatabaseType, SQLDialect
-        from db_copilot.prompts.examples.base import get_examples
-
+        """Ground the examples."""
         if (
             grounding_context
             and os.path.exists(grounding_context)
             and os.path.exists(os.path.join(grounding_context, "db_type.json"))
         ):
             with open(os.path.join(grounding_context, "db_type.json"), "r") as f:
-                grounding_database_type = json.load(f)
-                if database_type and grounding_database_type != database_type:
-                    logging.warn("grounding database type is different from input")
-                database_type = grounding_database_type
-        dialect = SQLDialect.from_db_type(DatabaseType(database_type))
-        tools = json.loads(tools) if tools else None
-        with_extensions = (
-            True
-            if tools
-            and len([tool for tool in tools if tool.lower() != dialect.value]) > 0
-            else False
-        )
-        examples = get_examples(dialect, with_extensions=with_extensions)
-        if sample_folder:
-            for file in os.listdir(sample_folder):
-                try:
-                    with open(os.path.join(sample_folder, file), "r") as example:
-                        examples_obj = json.load(example)
-                        if isinstance(examples_obj, list):
-                            for example_obj in examples_obj:
-                                example = InContextExample(**example_obj)
-                                examples.append(example)
-                        elif isinstance(examples_obj, dict):
-                            example = InContextExample(**examples_obj)
-                            examples.append(example)
-                        else:
-                            raise ValueError(
-                                f"Invalid sample file format: {examples_obj}"
-                            )
-                except Exception as ex:
-                    logging.warn(f"Failed to load sample file {file}. Error: {ex}")
+                database_type = json.load(f)
+                db_type = DatabaseType(database_type)
+                tools = json.loads(tools) if tools else None
+                if tools:
+                    assert isinstance(tools, list) or isinstance(tools, dict)
+                examples = InContextLearningAgent.get_examples(
+                    example_uri=sample_folder,
+                    db_type=db_type,
+                    tools=tools,
+                    include_built_in=include_builtin,
+                )
+                agent = InContextLearningAgent(
+                    embedding_config=EmbeddingConfig(), examples=examples
+                )
 
-        output_file = os.path.join(output_chunk_file, "Chunks_sample.csv")
-        logging.info(f"output path is {output_file}")
-        db_chunks_df = pd.DataFrame(columns=["Metadata", "Chunk"])
-        # build embedding index
-        for example in examples:
-            db_chunks_df = pd.concat(
-                [
-                    db_chunks_df,
-                    pd.DataFrame(
-                        {
-                            "Metadata": json.dumps(
-                                {
-                                    "example": asdict(example),
-                                    "dialect": dialect,
-                                    "source": {"filename": "example_file"},
-                                }
-                            ),
-                            "Chunk": example.embed_text,
-                        },
-                        index=[0],
-                    ),
-                ],
-                ignore_index=True,
-                axis=0,
-            )  # type: ignore
-        if not os.path.exists(output_chunk_file):
-            os.makedirs(output_chunk_file)
-        db_chunks_df.to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
-        logging.info(f"saved chunk file to {output_file}")
+                assert isinstance(agent.embedding_service, DummyEmbeddingService)
+                agent.embedding_service.dump(output_chunk_file)
