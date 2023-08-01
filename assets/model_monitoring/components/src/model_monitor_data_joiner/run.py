09de18bca8fabf5b3a6c0422a5c1355143ac8bb8
Chandana Upadhyaya <chupadhy@microsoft.com>
2023-08-01 16:25:09 -0700

    [MoMo] Add empty data joiner component

---
+++ assets/model_monitoring/components/src/model_monitor_data_joiner/run.py
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Entry script for Model Monitor Data Joiner Component."""
+
+import argparse
+
+def run():
+    """Data Joiner."""
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--left_input_data", type=str, required=True)
+    parser.add_argument("--left_join_column", type=str, required=True)
+    parser.add_argument("--right_input_data", type=str, required=True)
+    parser.add_argument("--right_join_column", type=str, required=True)
+    args = parser.parse_args()
+
+    print('Sucessfully executed data joiner component.')
+
+if __name__ == "__main__":
+    run()
