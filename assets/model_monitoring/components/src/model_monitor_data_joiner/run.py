358f8a361da90b547f10a930de167e0f0eea78de
Chandana Upadhyaya <chupadhy@microsoft.com>
2023-08-03 15:04:30 -0700

    [MoMo] Add happy path scenario for data joiner

--- assets/model_monitoring/components/src/model_monitor_data_joiner/run.py
+++ assets/model_monitoring/components/src/model_monitor_data_joiner/run.py
@@ -4,7 +4,33 @@
 """Entry script for Model Monitor Data Joiner Component."""
 
 import argparse
+from shared_utilities.io_utils import (
+    init_spark,
+    read_mltable_in_spark,
+    save_spark_df_as_mltable,
+)
 
+
+def join_data(
+    left_input_data: str,
+    left_join_column: str,
+    right_input_data: str,
+    right_join_column: str,
+    joined_data: str
+):
+    # Load data
+    left_input_data_df = read_mltable_in_spark(mltable_path=left_input_data)
+    right_input_data_df = read_mltable_in_spark(mltable_path=right_input_data)
+    
+    # Join the data
+    joined_data_df = left_input_data_df.join(
+        right_input_data_df,
+        left_input_data_df[left_join_column] == right_input_data_df[right_join_column],
+        'inner'
+    )
+    
+    # Write the output
+    save_spark_df_as_mltable(joined_data_df, joined_data)
 
 def run():
     """Join data assets on given columns."""
@@ -14,8 +40,16 @@ def run():
     parser.add_argument("--left_join_column", type=str, required=True)
     parser.add_argument("--right_input_data", type=str, required=True)
     parser.add_argument("--right_join_column", type=str, required=True)
-    # args = parser.parse_args()
+    parser.add_argument("--joined_data", type=str, required=True)
+    args = parser.parse_args()
 
+    join_data(
+        args.left_input_data,
+        args.left_join_column,
+        args.right_input_data,
+        args.right_join_column,
+        args.joined_data
+    )
     print('Successfully executed data joiner component.')
 
 
