e717bedabe11a21d8b123be436a59856377a5a30
Gaurav Singh <gasi@microsoft.com>
2023-08-05 00:45:23 +0000

    fix header

--- assets/training/model_management/environments/foundation-model-inference/context/score.py
+++ assets/training/model_management/environments/foundation-model-inference/context/score.py
@@ -1,6 +1,7 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Run script to infer."""
 
 import asyncio
 import json
