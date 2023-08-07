e0d1169a676a51b6fbd319d2126926fa65866d8f
Ayush Mishra <ayushmishra@microsoft.com>
2023-08-07 18:01:49 +0530

    address comments

--- scripts/azureml-assets/azureml/assets/model/model_utils.py
+++ scripts/azureml-assets/azureml/assets/model/model_utils.py
@@ -2,11 +2,11 @@
 # Licensed under the MIT License.
 """Model utils Operations Class."""
 
-import requests
 import os
 import azureml.assets as assets
 from azure.identity import DefaultAzureCredential
 from azure.ai.ml import load_model
+from azure.ai.ml._utils._registry_utils import get_asset_body_for_registry_storage, get_registry_client
 from azureml.assets.util import logger
 from azureml.assets.config import PathType
 from azureml.assets.model.download_utils import copy_azure_artifacts, download_git_model
@@ -16,57 +16,40 @@ from pathlib import Path
 class RegistryUtils:
     """Registry utils."""
 
-    INTERNAL_REGION_ASSET_STORE_URL = "https://int.api.azureml-test.ms/assetstore/v1.0"
-    PROD_REGION_ASSET_STORE_URL = "https://eastus.api.azureml.ms/assetstore/v1.0"
-    TOKEN_CREDENTIAL_URL = "https://management.azure.com/.default"
-    CREATE_DATA_REFERENCE_METHOD = "/temporaryDataReference/createOrGet"
-    DEV_REGISTRY_NAME = "azureml-dev"
     RETRY_COUNT = 3
-
-    def get_assetstore_url(registry_name: str):
-        """Return asset URI for the registry."""
-        # USE https://eastus.api.azureml.ms/registrymanagement/v1.0/registries/{registryName}/discovery
-        if RegistryUtils.DEV_REGISTRY_NAME == registry_name:
-            return RegistryUtils.INTERNAL_REGION_ASSET_STORE_URL
-        return RegistryUtils.PROD_REGION_ASSET_STORE_URL
 
     def get_registry_data_reference(registry_name: str, model_name: str, model_version: str):
         """Fetch data reference for asset in the registry."""
-        cnt, response, error = 0, None, None
+        cnt, blob_uri, sas_uri = 0, None, None
         asset_id = f"azureml://registries/{registry_name}/models/{model_name}/versions/{model_version}"
         logger.print(f"getting data reference for asset {asset_id}")
-        uri = RegistryUtils.get_assetstore_url(registry_name) + RegistryUtils.CREATE_DATA_REFERENCE_METHOD
-        payload = {
-            "assetId": asset_id,
-            "temporaryDataReferenceId": "model_weights",
-            "temporaryDataReferenceType": "TemporaryBlobReference",
-        }
-
         while cnt < RegistryUtils.RETRY_COUNT:
             try:
-                response = RegistryUtils._get_temp_data_ref(uri, payload)
+                response = RegistryUtils._get_temp_data_ref(registry_name, model_name, model_version)
+                blob_uri = response.blob_reference_for_consumption.blob_uri
+                sas_uri = response.blob_reference_for_consumption.credential.additional_properties["sasUri"]
+                if not blob_uri or not sas_uri:
+                    raise Exception("Error in fetching BLOB or SAS URI")
                 break
             except Exception as e:
-                error = e
                 cnt += 1
-                logger.log_error(f"Exception in fetching data reference. Retry count {cnt}")
-
-        if not response:
-            raise Exception(f"Unable to fetch data reference for asset {asset_id}. Error {error}")
-
-        response = response.json()
-        blob_uri = response["blobReferenceForConsumption"]["blobUri"]
-        sas_uri = response["blobReferenceForConsumption"]["credential"]["sasUri"]
-
+                logger.log_error(f"Exception in fetching data reference. Retry count {cnt}. Error: {e}")
+        if not blob_uri or not sas_uri:
+            raise Exception(f"Unable to fetch data reference for asset {asset_id}")
         return blob_uri, sas_uri
 
-    def _get_temp_data_ref(uri, payload):
+    def _get_temp_data_ref(registry_name, model_name, model_version):
         try:
             credential = DefaultAzureCredential()
-            token = credential.get_token(RegistryUtils.TOKEN_CREDENTIAL_URL).token
-            headers = {"Authorization": f"Bearer {token}"}
-            logger.print(f"HTTP request to {uri}")
-            response = requests.post(uri, json=payload, headers=headers)
+            body = get_asset_body_for_registry_storage(registry_name, "models", model_name, model_version)
+            registry_client, resource_group, _ = get_registry_client(credential, registry_name)
+            response = registry_client.temporary_data_references.create_or_get_temporary_data_reference(
+                name=model_name,
+                version=model_version,
+                resource_group_name=resource_group,
+                registry_name=registry_name,
+                body=body,
+            )
             return response
         except Exception as e:
             logger.log_error(f"exception in fetching data reference: {e}")
