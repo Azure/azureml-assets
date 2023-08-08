83f435d9fade7e4f8651b7c4cf12ae51f0d0cb70
Gaurav Singh <svaruag@outlook.com>
2023-08-08 13:51:57 -0700

    update scoring with eos/bos (#1003)
    * update scoring with eos/bos
    
    * unused
    
    * doc
    
    ---------
    
    Co-authored-by: svaruag <gasi@microsoft.com>

--- assets/training/model_management/environments/foundation-model-inference/context/score.py
+++ assets/training/model_management/environments/foundation-model-inference/context/score.py
@@ -365,7 +365,7 @@ def get_safe_response(result):
     logger.info("Analyzing response...")
     jsonable_result = _get_jsonable_obj(result, pandas_orient="records")
     if not aacs_client:
-        return jsonable_result, 0
+        return jsonable_result
 
     result, severity = iterate(jsonable_result)
     logger.info(f"Response analyzed, severity {severity}")
@@ -576,8 +576,6 @@ def get_processed_input_data_for_chat_completion(dialog: List[str]) -> str:
     As of 2021, the Burj Khalifa in Dubai\n
     [INST]and in Africa?[/INST]"
     """
-    B_INST, E_INST = "[INST]", "[/INST]"
-    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
     # fmt: off
     DEFAULT_SYSTEM_PROMPT = """You are a helpful, respectful and honest \
 assistant. Always answer as helpfully as possible, while being safe. Your \
@@ -588,37 +586,53 @@ socially unbiased and positive in nature.
 If a question does not make any sense, or is not factually coherent, \
 explain why instead of answering something not correct. If you don't know \
 the answer to a question, please don't share false information."""
+
     # fmt: on
 
-    prompt_tokens = []
-    if dialog[0]["role"] != "system":
-        dialog = [
-                     {
-                         "role": "system",
-                         "content": DEFAULT_SYSTEM_PROMPT,
-                     }
-                 ] + dialog
-    dialog = [{
-        "role": dialog[1]["role"],
-        "content": B_SYS + dialog[0]["content"] + E_SYS + dialog[1]["content"],
-    }
-             ] + dialog[2:]
-    assert all([msg["role"] == "user" for msg in dialog[::2]]) and all(
-        [msg["role"] == "assistant" for msg in dialog[1::2]]
-    ), (
-        "model only supports 'system','user' and 'assistant' roles, "
-        "starting with user and alternating (u/a/u/a/u...)"
-    )
-    dialog_content = "".join(
-        [
-            f" {B_INST} {msg['content']} {E_INST} "
-            if msg["role"] == "user"
-            else msg["content"]
-            for msg in dialog
-        ]
-    )
-    prompt_tokens.append(dialog_content)
-    return str.join("", prompt_tokens).strip()
+    def process_dialog(messages) -> Tuple[str, List[Tuple[str, str]], str]:
+        system_prompt = DEFAULT_SYSTEM_PROMPT
+        user_assistant_messages = []  # list of (user, assistant) messages
+        user_message = None  # current user message being processed
+        last_user_message = None  # user prompt for which response is needed
+
+        for i, message in enumerate(messages):
+            role = message["role"]
+            content = message["content"]
+
+            if role == "system" and i == 0:
+                system_prompt = content
+            elif role == "user":
+                if i == len(messages) - 1:
+                    last_user_message = content
+                else:
+                    user_message = content
+            elif role == "assistant" and user_message is not None:
+                user_assistant_messages.append((user_message, content))
+                user_message = None
+
+        return system_prompt, user_assistant_messages, last_user_message
+
+    # ref: https://huggingface.co/spaces/huggingface-projects/\
+    # llama-2-7b-chat/blob/main/model.py
+    def format_chat_conv(
+            message: str,
+            chat_history: list[tuple[str, str]],
+            system_prompt: str) -> str:
+        texts = [f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n']
+        # The first user input is _not_ stripped
+        do_strip = False
+        for user_input, response in chat_history:
+            user_input = user_input.strip() if do_strip else user_input
+            do_strip = True
+            texts.append(
+                f'{user_input} [/INST] {response.strip()} </s><s>[INST] ')
+        message = message.strip() if do_strip else message
+        texts.append(f'{message} [/INST]')
+        return ''.join(texts)
+
+    sys_prompt, user_assistant_msgs, message = process_dialog(dialog)
+    chat_conv = format_chat_conv(message, user_assistant_msgs, sys_prompt)
+    return chat_conv
 
 
 def get_request_data(request_string) -> (
