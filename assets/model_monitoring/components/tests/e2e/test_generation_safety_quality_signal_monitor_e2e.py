f901fc2db320c8b9e3b91ac690bc81ed1e280294
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-08-15 11:39:52 -0700

    fix copyright

--- assets/model_monitoring/components/tests/e2e/test_generation_safety_quality_signal_monitor_e2e.py
+++ assets/model_monitoring/components/tests/e2e/test_generation_safety_quality_signal_monitor_e2e.py
@@ -1,8 +1,7 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
-"""This file contains e2e tests for the generation safety model monitor component."""
+"""This file contains e2e tests for the model data collector preprocessor component."""
 
 import pytest
 from azure.ai.ml import MLClient, Output
