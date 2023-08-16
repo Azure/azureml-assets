5232fdaa013709b69cf30bdee11b5efd8a903c45
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-08-16 12:00:52 -0700

    fix flake 8

--- assets/model_monitoring/components/tests/e2e/conftest.py
+++ assets/model_monitoring/components/tests/e2e/conftest.py
@@ -268,6 +268,7 @@ def publish_data_drift_model_monitor_component(
         ml_client.components.create_or_update(load_component(spec_path))
         print(f"Successfully published {component['name']}.")
 
+
 @pytest.fixture(scope="session", autouse=True)
 def publish_generation_safety_signal_monitor_component(
     main_worker_lock,
@@ -289,9 +290,12 @@ def publish_generation_safety_signal_monitor_component(
         if component["name"] != "generation_safety_quality_signal_monitor_spark":
             continue
         print(f"Publishing {component['name']}..")
-        component["jobs"]["compute_metrics"]["component"] = f"azureml:annotation_compute_metrics_spark:{asset_version}"
-        component["jobs"]["compute_histogram"]["component"] = f"azureml:annotation_compute_histogram_spark:{asset_version}"
-        component["jobs"]["output_signal_metrics"]["component"] = f"azureml:model_monitor_output_metrics:{asset_version}"
+        component["jobs"]["compute_metrics"]["component"] = f"azureml:annotation_compute_metrics_spark:\
+            {asset_version}"
+        component["jobs"]["compute_histogram"]["component"] = f"azureml:annotation_compute_histogram_spark:\
+            {asset_version}"
+        component["jobs"]["output_signal_metrics"]["component"] = f"azureml:model_monitor_output_metrics:\
+            {asset_version}"
         component["version"] = asset_version
 
         spec_path = os.path.join(
@@ -301,6 +305,7 @@ def publish_generation_safety_signal_monitor_component(
         write_to_yaml(spec_path, component)
         ml_client.components.create_or_update(load_component(spec_path))
         print(f"Successfully published {component['name']}.")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def publish_feature_attr_drift_signal_monitor_component(
