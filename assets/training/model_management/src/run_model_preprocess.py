69a9485aa53290c9e64e7990972ea54cb063e387
Vivek Dani <110168656+vivek-dani@users.noreply.github.com>
2023-09-13 11:38:23 +0530

    removing unused imports

--- assets/training/model_management/src/run_model_preprocess.py
+++ assets/training/model_management/src/run_model_preprocess.py
@@ -7,12 +7,11 @@ import argparse
 import os
 import json
 import shutil
-from azureml.model.mgmt.config import AppName, ComponentConstants, ModelFlavor
+from azureml.model.mgmt.config import AppName, ModelFlavor
 from azureml.model.mgmt.processors.transformers.config import HF_CONF
 from azureml.model.mgmt.processors.preprocess import run_preprocess
 from azureml.model.mgmt.processors.transformers.config import SupportedTasks
 from azureml.model.mgmt.processors.pyfunc.vision.config import Tasks
-from azureml.model.mgmt.processors.pyfunc.stable_diffusion.constants import Tasks as StableDiffusionTasks
 from azureml.model.mgmt.utils.exceptions import swallow_all_exceptions, UnsupportedTaskType
 from azureml._common.exceptions import AzureMLException
 from azureml._common._error_definition.azureml_error import AzureMLError
