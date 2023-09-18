b1784bb936c0b66a0b8fea7e0aa2f0cbf8adf76a
Yuan Zhuang <yuzhua@microsoft.com>
2023-09-17 17:29:34 -0700

    fix tests

--- assets/aml-benchmark/components/src/batch_output_formatter/main.py
+++ assets/aml-benchmark/components/src/batch_output_formatter/main.py
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import argparse
 import pandas as pd
 import os
--- assets/aml-benchmark/components/src/utils/io.py
+++ assets/aml-benchmark/components/src/utils/io.py
@@ -1,6 +1,6 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from typing import Union, Any, List, Dict
 import mltable
 import uuid
@@ -8,7 +8,7 @@ import json
 import os
 import pandas as pd
 
-from utils.helper import get_logger
+from .logging import get_logger
 
 
 logger = get_logger(__name__)
