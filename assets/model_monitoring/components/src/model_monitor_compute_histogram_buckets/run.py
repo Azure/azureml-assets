9cf7896a34bcff6d5d44e8db1dd1eb415108e214
Mathieu St-Louis <mastloui@microsoft.com>
2023-09-19 19:50:58 -0700

    fix

--- assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/run.py
+++ assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/run.py
@@ -5,8 +5,12 @@
 
 import argparse
 from histogram_buckets import compute_histogram_buckets
-from shared_utilities.io_utils import try_read_mltable_in_spark, save_spark_df_as_mltable
+from shared_utilities.io_utils import (
+    try_read_mltable_in_spark,
+    save_spark_df_as_mltable,
+)
 from shared_utilities.event_utils import post_warning_event
+
 
 def run():
     """Compute histogram."""
@@ -20,12 +24,15 @@ def run():
     df1 = try_read_mltable_in_spark(args.input_data_1, "input_data_1")
     df2 = df1
     if args.input_data_2 is not None:
-        df2 = try_read_mltable_in_spark(args.input_data_2,  "input_data_2")
+        df2 = try_read_mltable_in_spark(args.input_data_2, "input_data_2")
 
     if not df1 and not df2:
-        error_message = "Both input data is empty. Skipping histogram bucket generation."
+        error_message = (
+            "Both input data is empty. Skipping histogram bucket generation."
+        )
         print(error_message)
-        post_warning_event(error_message
+        post_warning_event(
+            error_message
             + " Please visit aka.ms/mlmonitoringhelp for more information."
         )
         return
