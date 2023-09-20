26c781d6bdeef52da25f84f213071659fa17400e
grajguru <grajguru@microsoft.com>
2023-09-20 22:30:18 +0530

    dev

--- assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/text_to_image/stable_diffusion_inpainting_mlflow_wrapper.py
+++ assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/text_to_image/stable_diffusion_inpainting_mlflow_wrapper.py
@@ -7,14 +7,15 @@ Contains MLFlow pyfunc wrapper for stable diffusion inpainting models.
 Has methods to load the model and predict.
 """
 
-from diffusers import StableDiffusionInpaintPipeline
+import logging
 import mlflow
 import os
 import pandas as pd
-import torch
-
+from diffusers import StableDiffusionInpaintPipeline
 from config import MLflowSchemaLiterals, Tasks, MLflowLiterals, BatchConstants
-from utils import image_to_str, save_image
+from utils import image_to_str, save_image, get_current_device, process_image, get_pil_image
+
+logger = logging.getLogger(__name__)
 
 
 class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
@@ -41,27 +42,21 @@ class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
         :param context: MLflow context containing artifacts that the model can use for inference
         :type context: mlflow.pyfunc.PythonModelContext
         """
-        self._batch_output_folder = os.getenv(
-            BatchConstants.BATCH_OUTPUT_PATH, default=False
-        )
+        self._batch_output_folder = os.getenv(BatchConstants.BATCH_OUTPUT_PATH, default=False)
 
         if self._task_type == Tasks.TEXT_TO_IMAGE_INPAINTING.value:
             try:
-                _map_location = "cuda" if torch.cuda.is_available() else "cpu"
                 model_dir = context.artifacts[MLflowLiterals.MODEL_DIR]
                 self._pipe = StableDiffusionInpaintPipeline.from_pretrained(model_dir)
-                self._pipe.to(_map_location)
-                print("Model loaded successfully")
+                self._pipe.to(get_current_device())
+                logger.info("Model loaded successfully")
             except Exception as e:
-                print("Failed to load the the model.")
-                print(e)
+                logger.error(f"Failed to load the the model. {str(e)}")
                 raise
         else:
             raise ValueError(f"invalid task type {self._task_type}")
 
-    def predict(
-        self, context: mlflow.pyfunc.PythonModelContext, input_data: pd.DataFrame
-    ) -> pd.DataFrame:
+    def predict(self, context: mlflow.pyfunc.PythonModelContext, input_data: pd.DataFrame) -> pd.DataFrame:
         """
         Perform inference on the input data.
 
@@ -74,11 +69,18 @@ class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
                  Images in form of base64 string.
         :rtype: pd.DataFrame
         """
-        text_prompts = input_data[MLflowSchemaLiterals.INPUT_COLUMN_PROMPT].tolist()
-        image = input_data[MLflowSchemaLiterals.INPUT_COLUMN_IMAGE].tolist()
-        mask_image = input_data[MLflowSchemaLiterals.INPUT_COLUMN_MASK_IMAGE].tolist()
+        # Decode the base64 image column
+        images = input_data.loc[:, [MLflowSchemaLiterals.INPUT_COLUMN_IMAGE]].apply(axis=1, func=process_image)
+        images = images.loc[:, 0].apply(func=get_pil_image).tolist()
 
-        assert len(text_prompts) == len(image) == len(mask_image), (
+        mask_images = input_data.loc[:, [MLflowSchemaLiterals.INPUT_COLUMN_MASK_IMAGE]].apply(
+            axis=1, func=process_image
+        )
+        mask_images = mask_images.loc[:, 0].apply(func=get_pil_image).tolist()
+
+        text_prompts = input_data.loc[:, MLflowSchemaLiterals.INPUT_COLUMN_PROMPT].tolist()
+
+        assert len(text_prompts) == len(images) == len(mask_images), (
             f"Invalid input. Number of text prompt, image and mask image are expected to be same. "
             f"But, found text prompt length {len(text_prompts)}, image length {len(image)} and "
             f"mask_image length {len(mask_image)}"
@@ -88,7 +90,7 @@ class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
         nsfw_content = []
         if self._batch_output_folder:
             # Batch endpoint
-            for text_prompt, image, mask_image in zip(text_prompts, image, mask_image):
+            for image, mask_image, text_prompt in zip(images, mask_images, text_prompts):
                 output = self._pipe(
                     prompt=text_prompt,
                     image=image,
@@ -97,20 +99,14 @@ class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
                 )
 
                 # Save image in batch output folder and append the image file name to generated_images list
-                generated_images.append(
-                    save_image(self._batch_output_folder, output.images[0])
-                )
-                nsfw_content.append(
-                    output.nsfw_content_detected[0]
-                    if output.nsfw_content_detected
-                    else None
-                )
+                generated_images.append(save_image(self._batch_output_folder, output.images[0]))
+                nsfw_content.append(output.nsfw_content_detected[0] if output.nsfw_content_detected else None)
         else:
             # Online endpoint
             outputs = self._pipe(
                 prompt=text_prompts,
-                image=image,
-                mask_image=mask_image,
+                image=images,
+                mask_image=mask_images,
                 return_dict=True,
             )
 
@@ -122,7 +118,7 @@ class StableDiffusionInpaintingMLflowWrapper(mlflow.pyfunc.PythonModel):
         df = pd.DataFrame(
             {
                 MLflowSchemaLiterals.OUTPUT_COLUMN_IMAGE: generated_images,
-                MLflowSchemaLiterals.OUTPUT_COLUMN_NSFW_FLAG: output.nsfw_content_detected,
+                MLflowSchemaLiterals.OUTPUT_COLUMN_NSFW_FLAG: nsfw_content,
             }
         )
 
--- assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/text_to_image/utils.py
+++ assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/text_to_image/utils.py
@@ -5,11 +5,38 @@
 
 import base64
 from io import BytesIO
+import io
+import logging
 import os
-import PIL
+import re
 import uuid
+import pandas as pd
+import PIL
 
+import requests
+import torch
+
+from PIL import Image, UnidentifiedImageError
 from config import DatatypeLiterals
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_pil_image(image: bytes) -> PIL.Image.Image:
+    """
+    Convert image bytes to PIL image.
+
+    :param image: image bytes
+    :type image: bytes
+    :return: PIL image object
+    :rtype: PIL.Image.Image
+    """
+    try:
+        return Image.open(io.BytesIO(image))
+    except UnidentifiedImageError as e:
+        logger.error("Invalid image format. Please use base64 encoding for input images.")
+        raise e
 
 
 def image_to_str(img: PIL.Image.Image) -> str:
@@ -23,9 +50,7 @@ def image_to_str(img: PIL.Image.Image) -> str:
     """
     buffered = BytesIO()
     img.save(buffered, format=DatatypeLiterals.IMAGE_FORMAT)
-    img_str = base64.b64encode(buffered.getvalue()).decode(
-        DatatypeLiterals.STR_ENCODING
-    )
+    img_str = base64.b64encode(buffered.getvalue()).decode(DatatypeLiterals.STR_ENCODING)
     return img_str
 
 
@@ -41,7 +66,90 @@ def save_image(output_folder: str, img: PIL.Image.Image) -> str:
     :rtype: str
     """
     filename = f"image_{uuid.uuid4()}.{DatatypeLiterals.IMAGE_FORMAT.lower()}"
-    img.save(
-        os.path.join(output_folder, filename), format=DatatypeLiterals.IMAGE_FORMAT
-    )
+    img.save(os.path.join(output_folder, filename), format=DatatypeLiterals.IMAGE_FORMAT)
     return filename
+
+
+def process_image(img: pd.Series) -> pd.Series:
+    """Process input image and return bytes.
+
+    If input image is in base64 string format, decode it to bytes.
+    If input image is in url format, download it and return bytes.
+    https://github.com/mlflow/mlflow/blob/master/examples/flower_classifier/image_pyfunc.py
+
+    :param img: pandas series with image in base64 string format or url.
+    :type img: pd.Series
+    :return: decoded image in pandas series format.
+    :rtype: Pandas Series
+    """
+    image = img[0]
+    if isinstance(image, bytes):
+        return img
+    elif isinstance(image, str):
+        if _is_valid_url(image):
+            image = requests.get(image).content
+            return pd.Series(image)
+        else:
+            try:
+                return pd.Series(base64.b64decode(image))
+            except ValueError:
+                raise ValueError(
+                    "The provided image string cannot be decoded." "Expected format is base64 string or url string."
+                )
+    else:
+        raise ValueError(
+            f"Image received in {type(image)} format which is not supported."
+            "Expected format is bytes, base64 string or url string."
+        )
+
+
+def _is_valid_url(text: str) -> bool:
+    """Check if text is url or base64 string.
+
+    :param text: text to validate
+    :type text: str
+    :return: True if url else false
+    :rtype: bool
+    """
+    regex = (
+        "((http|https)://)(www.)?"
+        + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+        + "{2,256}\\.[a-z]"
+        + "{2,6}\\b([-a-zA-Z0-9@:%"
+        + "._\\+~#?&//=]*)"
+    )
+    p = re.compile(regex)
+
+    # If the string is empty
+    # return false
+    if str is None:
+        return False
+
+    # Return if the string
+    # matched the ReGex
+    if re.search(p, text):
+        return True
+    else:
+        return False
+
+
+def get_current_device() -> torch.device:
+    """Get current cuda device.
+
+    :return: current device
+    :rtype: torch.device
+    """
+    # check if GPU is available
+    if torch.cuda.is_available():
+        try:
+            # get the current device index
+            device_idx = torch.distributed.get_rank()
+        except RuntimeError as ex:
+            if "Default process group has not been initialized".lower() in str(ex).lower():
+                device_idx = 0
+            else:
+                logger.error(str(ex))
+                raise ex
+        return torch.device(type="cuda", index=device_idx)
+    else:
+        return torch.device(type="cpu")
