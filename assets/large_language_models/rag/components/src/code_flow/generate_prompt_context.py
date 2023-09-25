cec1b31be31ec1f3df52f4f5b6dd64b472d91afe
George Dyre <georgedyre@microsoft.com>
2023-09-25 15:34:53 -0700

    Added copyright header

--- assets/large_language_models/rag/components/src/code_flow/generate_prompt_context.py
+++ assets/large_language_models/rag/components/src/code_flow/generate_prompt_context.py
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 from typing import List
 from promptflow import tool
 from promptflow_vectordb.core.contracts import SearchResultEntity
