cc388912f43c69b646c077d0e123601745053c52
vikasagrawal-ms <vikasagrawal@microsoft.com>
2023-09-27 11:53:41 +0530

    updated output_selector component

--- assets/training/model_management/src/run_output_selector.py
+++ assets/training/model_management/src/run_output_selector.py
@@ -8,29 +8,26 @@ If condition is `true`, link `output` to `input_a`.
 If condition is `false`, link `output` to `input_b`.
 """
 import argparse
-
-from azureml.core import Dataset, Run
-
+import shutil
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_a", type=str)
 parser.add_argument("--input_b", type=str)
-parser.add_argument("--condition", type=str)
+#parser.add_argument("--condition", type=str)
+parser.add_argument("--output", type=str)
 args, _ = parser.parse_known_args()
 print(f"Condition output component received args: {args}.")
 if args.input_a is None and args.input_b is None:
     raise Exception("Got 'input_a' and 'input_b' both be None.")
 
-run = Run.get_context()
-workspace = run.experiment.workspace
-condition = args.condition.lower() == "true"
-try:
-    if condition:
-        print("Linking output to input_a ...")
-        dataset = Dataset.get_by_id(workspace, id=args.input_a)
-    else:
-        print("Linking output to input_b ...")
-        dataset = Dataset.get_by_id(workspace, id=args.input_b)
-    run.output_datasets["output"].link(dataset)
-except Exception as e:
-    raise Exception(f"Link output failed with {e}.")
+#condition = args.condition.lower() == "true"
+
+if args.input_b is None:
+    print("Copy input_a to output")
+    shutil.copytree(args.input_a, args.output)
+elif args.input_a is None:
+    print("Copy input_b to output")
+    shutil.copytree(args.input_b, args.output)
+else:
+    raise Exception("Got 'input_a' and 'input_b' both be None.")
+
