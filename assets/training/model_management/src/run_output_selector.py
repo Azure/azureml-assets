6fd6666ea777186933667784e8802f185caf2552
vikasagrawal-ms <vikasagrawal@microsoft.com>
2023-09-29 11:11:54 +0530

    updated

--- assets/training/model_management/src/run_output_selector.py
+++ assets/training/model_management/src/run_output_selector.py
@@ -13,21 +13,20 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_a", type=str)
 parser.add_argument("--input_b", type=str)
-#parser.add_argument("--condition", type=str)
+parser.add_argument("--condition", type=str)
 parser.add_argument("--output", type=str)
 args, _ = parser.parse_known_args()
 print(f"Condition output component received args: {args}.")
 if args.input_a is None and args.input_b is None:
     raise Exception("Got 'input_a' and 'input_b' both be None.")
 
-#condition = args.condition.lower() == "true"
+condition = args.condition.lower() == "true"
 
-if args.input_b is None:
+if condition:
     print("Copy input_a to output")
     shutil.copytree(args.input_a, args.output)
-elif args.input_a is None:
+else:
     print("Copy input_b to output")
     shutil.copytree(args.input_b, args.output)
-else:
-    raise Exception("Got 'input_a' and 'input_b' both be None.")
+
 
