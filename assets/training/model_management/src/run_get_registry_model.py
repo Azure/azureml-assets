173c6089e4622b67d6455b7196defb745a8dd38d
vikasagrawal-ms <vikasagrawal@microsoft.com>
2023-10-05 12:35:23 +0530

    download from registry

---
+++ assets/training/model_management/src/run_get_registry_model.py
@@ -0,0 +1,34 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""
+This module link output on run conditionally.
+
+If condition is `true`, link `output` to `input_a`.
+If condition is `false`, link `output` to `input_b`.
+"""
+import argparse, json, os
+from azureml.model.mgmt.config import ComponentVariables
+from azureml.model.mgmt.utils.common_utils import get_mlclient
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--registration_details_folder", type=str)
+parser.add_argument("--mlflow_model_folder", type=str)
+
+args, _ = parser.parse_known_args()
+print(f"Condition output component received args: {args}.")
+
+with open(os.path.join(args.registration_details_folder, ComponentVariables.REGISTRATION_DETAILS_JSON_FILE)) as f:
+    registration_details = json.load(f)
+
+model_id = registration_details["id"]
+model = registration_details["name"]
+version = registration_details["version"]
+registry = model_id.split("/")[3]
+print(f"model_id is {model_id} and registry is {registry} and version is {version} and model is {model}")
+ml_client_registry = get_mlclient(registry_name=registry)
+
+mlflow_model_from_registry = ml_client_registry.models.download(
+    name=model, version=version, download_path=args.mlflow_model_folder)
+
+print("Downloaded model from registry.")
--- assets/training/model_management/src/run_output_selector.py
+++ assets/training/model_management/src/run_output_selector.py
@@ -8,25 +8,27 @@ If condition is `true`, link `output` to `input_a`.
 If condition is `false`, link `output` to `input_b`.
 """
 import argparse
-import shutil
+from azureml.core import Dataset, Run
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_a", type=str)
-parser.add_argument("--input_b", type=str)
 parser.add_argument("--condition", type=str)
-parser.add_argument("--output", type=str)
+parser.add_argument("--input_a_registration_details_folder", type=str)
+parser.add_argument("--input_b_registration_details_folder", type=str)
+parser.add_argument("--input_a_mlflow_model_folder", type=str)
+parser.add_argument("--input_b_mlflow_model_folder", type=str)
 args, _ = parser.parse_known_args()
 print(f"Condition output component received args: {args}.")
-if args.input_a is None and args.input_b is None:
-    raise Exception("Got 'input_a' and 'input_b' both be None.")
 
 condition = args.condition.lower() == "true"
-
+run = Run.get_context()
 if condition:
     print("Copy input_a to output")
-    shutil.copytree(args.input_a, args.output)
+    registration_details_folder = Dataset.File.from_files(path=args.input_a_registration_details_folder)
+    mlflow_model_folder = Dataset.File.from_files(path=args.input_a_mlflow_model_folder)
 else:
     print("Copy input_b to output")
-    shutil.copytree(args.input_b, args.output)
+    registration_details_folder = Dataset.File.from_files(path=args.input_b_registration_details_folder)
+    mlflow_model_folder = Dataset.File.from_files(path=args.input_b_mlflow_model_folder)
 
-
+run.output_datasets["registration_details_folder"].link(registration_details_folder)
+run.output_datasets["mlflow_model_folder"].link(mlflow_model_folder)
