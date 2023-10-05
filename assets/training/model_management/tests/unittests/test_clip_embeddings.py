dbdf3b9660d168b0dffd0784a7f1498390ad74a5
Rehaan Bhimani <rbhimani@microsoft.com>
2023-10-05 17:42:04 -0600

    more linter

--- assets/training/model_management/tests/unittests/test_clip_embeddings.py
+++ assets/training/model_management/tests/unittests/test_clip_embeddings.py
@@ -17,10 +17,10 @@ from azureml.model.mgmt.processors.pyfunc.clip.clip_embeddings_mlflow_wrapper im
 
 
 class TestCLIPEmbeddings:
-    """Test class for clip embeddings helper functions"""
+    """Test class for clip embeddings helper functions."""
 
     def test_valid_input_clip(self):
-        """Test that the validate_input function for CLIP Embeddings model allows invalid input"""
+        """Test that the validate_input function for CLIP Embeddings model allows invalid input."""
         input_cases = InputCases()
 
         has_image, has_text = CLIPEmbeddingsMLFlowModelWrapper.validate_input(input_cases.valid_text_input())
@@ -44,7 +44,7 @@ class TestCLIPEmbeddings:
         assert has_text
 
     def test_invalid_input_clip(self):
-        """Test that the validate_input function for CLIP Embeddings model catches valid input"""
+        """Test that the validate_input function for CLIP Embeddings model catches valid input."""
         input_cases = InputCases()
 
         with pytest.raises(ValueError):
@@ -58,8 +58,9 @@ class TestCLIPEmbeddings:
 
 
 class InputCases:
-    """Class for generating valid and invalid inputs for CLIP Embeddings"""
+    """Class for generating valid and invalid inputs for CLIP Embeddings."""
     def __init__(self):
+        """Initialize text and image data"""
         self.text_list = [
             "text 1", "text 2", "text 3"
         ]
@@ -70,7 +71,7 @@ class InputCases:
         ]
 
     def valid_text_input(self):
-        """Return dataframe where all 'image' column is empty string and 'text' column has values"""
+        """Return dataframe where all 'image' column is empty string and 'text' column has values."""
         text_data = [["", text] for text in self.text_list]
         test_df_text = pd.DataFrame(
             data=text_data,
@@ -79,7 +80,7 @@ class InputCases:
         return test_df_text
 
     def valid_image_input(self):
-        """Return dataframe where all 'text' column is empty string and 'image' column has values"""
+        """Return dataframe where all 'text' column is empty string and 'image' column has values."""
         image_data = [[img, ""] for img in self.image_list]
         test_df_image = pd.DataFrame(
             data=image_data,
@@ -88,7 +89,7 @@ class InputCases:
         return test_df_image
 
     def valid_combination_input(self):
-        """Return dataframe where all 'image' column and 'text' column have values"""
+        """Return dataframe where all 'image' column and 'text' column have values."""
         combine_data = [[self.image_list[i], self.text_list[i]] for i in range(0, len(self.image_list))]
         test_df_combine = pd.DataFrame(
             data=combine_data,
@@ -97,7 +98,7 @@ class InputCases:
         return test_df_combine
 
     def valid_text_input_nan(self):
-        """Return dataframe where all 'image' column is NaN and 'text' column has values"""
+        """Return dataframe where all 'image' column is NaN and 'text' column has values."""
         test_df_text = pd.DataFrame(
             columns=["image"],
         )
@@ -105,7 +106,7 @@ class InputCases:
         return test_df_text
 
     def valid_image_input_nan(self):
-        """Return dataframe where all 'text' column is NaN and 'image' column has values"""
+        """Return dataframe where all 'text' column is NaN and 'image' column has values."""
         test_df_image = pd.DataFrame(
             columns=["text"],
         )
@@ -113,19 +114,19 @@ class InputCases:
         return test_df_image
 
     def invalid_text_input(self):
-        """Return dataframe where all 'image' column is empty string and 'text' column has missing values"""
+        """Return dataframe where all 'image' column is empty string and 'text' column has missing values."""
         test_df = self.valid_text_input()
         test_df['text'].iloc[1] = ""
         return test_df
 
     def invalid_image_input(self):
-        """Return dataframe where all 'text' column is empty string and 'image' column has missing values"""
+        """Return dataframe where all 'text' column is empty string and 'image' column has missing values."""
         test_df = self.valid_image_input()
         test_df['image'].iloc[1] = ""
         return test_df
 
     def invalid_combination_input(self):
-        """Return dataframe where 'text' column and 'image' column have missing values"""
+        """Return dataframe where 'text' column and 'image' column have missing values."""
         test_df = self.valid_combination_input()
         test_df["image"].iloc[1] = ""
         test_df["text"].iloc[1] = ""
