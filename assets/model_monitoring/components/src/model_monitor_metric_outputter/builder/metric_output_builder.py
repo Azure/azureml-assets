1ab10a41b089849c6929db0774c15e4ee34a5a95
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-10-11 12:52:28 -0700

    remove print

--- assets/model_monitoring/components/src/model_monitor_metric_outputter/builder/metric_output_builder.py
+++ assets/model_monitoring/components/src/model_monitor_metric_outputter/builder/metric_output_builder.py
@@ -107,8 +107,6 @@ class MetricOutputBuilder:
                             metric_name=metric_name,
                             groups=groups)
         else:
-            print( metric[VALUE] is not None)
-            print( metric[VALUE])
             if VALUE in metric and metric[VALUE] is not None:
                 cur_dict[VALUE] = metric[VALUE]
             if THRESHOLD in metric and metric[THRESHOLD] is not None:
