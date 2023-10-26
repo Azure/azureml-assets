4f4591ed92dbdfe819e91eeee41f5318862685be
Shreeya Sharma <shrsha@microsoft.com>
2023-10-26 08:53:48 -0700

    update code to fix flake8 issues

--- assets/model_monitoring/components/tests/unit/test_df_utils.py
+++ assets/model_monitoring/components/tests/unit/test_df_utils.py
@@ -1,27 +1,28 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 from shared_utilities.df_utils import (
-    get_common_columns,
     get_numerical_columns,
     get_categorical_columns,
     is_categorical,
     is_numerical
 )
-import pytest
 import pandas as pd
 
+
 def test_get_numerical_columns():
-    column_dtype_map = {
-    'col1': 'int',
-    'col2': 'float',
-    'col3': 'double',
-    'col4': 'decimal',
-    'col5': 'string'
-    }
+    column_dtype_map = {'col1': 'int',
+                        'col2': 'float',
+                        'col3': 'double',
+                        'col4': 'decimal',
+                        'col5': 'string'
+                        }
     baseline_df = pd.DataFrame({
-    'col1': [1, 2, 2, 4, 4],
-    'col2': [1.1, 2.2, 3.3, 4.4, 5.5],
-    'col3': [1.11, 2.22, 3.33, 4.44, 5.55],
-    'col4': [1.111, 2.222, 3.333, 4.444, 5.555]
-    })
+                                'col1': [1, 2, 2, 4, 4],
+                                'col2': [1.1, 2.2, 3.3, 4.4, 5.5],
+                                'col3': [1.11, 2.22, 3.33, 4.44, 5.55],
+                                'col4': [1.111, 2.222, 3.333, 4.444, 5.555]
+                                })
     numerical_columns = get_numerical_columns(column_dtype_map, baseline_df)
     assert numerical_columns == ['col1', 'col2', 'col3', 'col4']
 
@@ -29,29 +30,31 @@ def test_get_numerical_columns():
 def test_get_categorical_columns():
     # Test with all numerical columns
     column_dtype_map = {
-    'col1': 'int',
-    'col2': 'float',
-    'col3': 'double',
-    'col4': 'decimal'
-    }
+                        'col1': 'int',
+                        'col2': 'float',
+                        'col3': 'double',
+                        'col4': 'decimal'
+                        }
     baseline_df = pd.DataFrame({
-    'col1': [1, 2, 2, 4, 4],
-    'col2': [1.1, 2.2, 3.3, 4.4, 5.5],
-    'col3': [1.11, 2.22, 3.33, 4.44, 5.55],
-    'col4': [1.111, 2.222, 3.333, 4.444, 5.555]
-    })
-    categorical_columns = get_categorical_columns(column_dtype_map, baseline_df)
+                                'col1': [1, 2, 2, 4, 4],
+                                'col2': [1.1, 2.2, 3.3, 4.4, 5.5],
+                                'col3': [1.11, 2.22, 3.33, 4.44, 5.55],
+                                'col4': [1.111, 2.222, 3.333, 4.444, 5.555]
+                                })
+    categorical_columns = get_categorical_columns(column_dtype_map,
+                                                  baseline_df)
     assert categorical_columns == []
     
     # Test with int being same value
     column_dtype_map = {
-    'col1': 'int'
-    }
+                        'col1': 'int'
+                        }
     baseline_df = pd.DataFrame({
-    'col1': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1, 1, 1, 1, 1]
-    })
-    categorical_columns = get_categorical_columns(column_dtype_map, baseline_df)
+                                'col1': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                                         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+                                })
+    categorical_columns = get_categorical_columns(column_dtype_map,
+                                                  baseline_df)
     assert categorical_columns == ['col1']
 
     # Test with all categorical columns
@@ -63,7 +66,8 @@ def test_get_categorical_columns():
         'col1': ['a', 'b', 'c', 'd', 'e'],
         'col2': [True, False, True, False, True]
     })
-    categorical_columns = get_categorical_columns(column_dtype_map, baseline_df)
+    categorical_columns = get_categorical_columns(column_dtype_map,
+                                                  baseline_df)
     assert categorical_columns == ['col1', 'col2']
 
 
@@ -79,7 +83,8 @@ def test_is_categorical():
     # Test with integer column with high distinct value ratio
     baseline_column = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     assert is_categorical(baseline_column) == False
-    
+
+
 def test_is_numerical():
     # Test with integer column with high distinct value ratio
     baseline_column = pd.Series([1, 2, 3, 4, 5])
