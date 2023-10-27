8fdb78c0d5d876aff2b6bb2547ff9a6177247a7d
Riti Sharma <52715641+sharma-riti@users.noreply.github.com>
2023-10-27 09:51:00 -0700

    Add BLIP/BLIP2 model card and converter for image-to-text & vqa (#1566)
    * add blip image-to-text model
    
    * add model asset card for blip
    
    * fix formatting
    
    * add converter and model card

--- assets/models/system/salesforce-blip2-opt-2-7b-image-to-text/description.md
+++ assets/models/system/salesforce-blip2-opt-2-7b-image-to-text/description.md
@@ -6,8 +6,8 @@
 
 Inference type|Python sample (Notebook)|CLI with YAML
 |--|--|--|
-Real time|<a href='https://aka.ms/azureml-infer-sdk-blip2-image-to-text' target='_blank'>image-to-text-online-endpoint.ipynb</a>|<a href='https://aka.ms/azureml-infer-cli-blip2-image-to-text' target='_blank'>image-to-text-online-endpoint.sh</a>
-Batch |<a href='https://aka.ms/azureml-infer-batch-sdk-blip2-image-to-text' target='_blank'>image-to-text-batch-endpoint.ipynb</a>|<a href='https://aka.ms/azureml-infer-batch-cli-blip2-image-to-text' target='_blank'>image-to-text-batch-endpoint.sh</a>
+Real time|<a href='https://aka.ms/azureml-infer-online-sdk-blip-image-to-text' target='_blank'>image-to-text-online-endpoint.ipynb</a>|<a href='https://aka.ms/azureml-infer-online-cli-blip-image-to-text' target='_blank'>image-to-text-online-endpoint.sh</a>
+Batch |<a href='https://aka.ms/azureml-infer-batch-sdk-blip-image-to-text' target='_blank'>image-to-text-batch-endpoint.ipynb</a>|<a href='https://aka.ms/azureml-infer-batch-cli-blip-image-to-text' target='_blank'>image-to-text-batch-endpoint.sh</a>
 
 ### Sample inputs and outputs (for real-time inference)
 
@@ -35,10 +35,10 @@ Note:
 ```json
 [
    {
-      "text": "a carton of milk sitting on a table"
+      "text": "a stream running through a forest with rocks and trees"
    },
    {
-      "text": "a bottle of milk and a carton of milk"
+      "text": "a grassy hillside with trees and a sunset"
    }
 ]
 ```
--- assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/blip/mlflow_wrapper.py
+++ assets/training/model_management/src/azureml/model/mgmt/processors/pyfunc/blip/mlflow_wrapper.py
@@ -8,8 +8,9 @@ from PIL import Image
 import pandas as pd
 import tempfile
 
-from transformers import AutoProcessor, Blip2ForConditionalGeneration
-from config import MLflowSchemaLiterals, MLflowLiterals, Tasks
+from transformers import (AutoProcessor, BlipForConditionalGeneration,
+                          BlipForQuestionAnswering, Blip2ForConditionalGeneration)
+from config import MLflowSchemaLiterals, MLflowLiterals, Tasks, HfBlipModelId
 from typing import List
 
 try:
@@ -20,22 +21,26 @@ except ImportError:
     pass
 
 
-class BLIP2MLFlowModelWrapper(mlflow.pyfunc.PythonModel):
-    """MLflow model wrapper for BLIP2 model."""
+class BLIPMLFlowModelWrapper(mlflow.pyfunc.PythonModel):
+    """MLflow model wrapper for BLIP model family."""
 
     def __init__(
         self,
         task_type: str,
+        model_id: str
     ) -> None:
         """Initialize MLflow wrapper class.
 
         :param task_type: Task type used in training.
         :type task_type: str
+        :param model_id: HF model id for BLIP family.
+        :type model_id: str
         """
         super().__init__()
         self._processor = None
         self._model = None
         self._task_type = task_type
+        self._model_id = model_id
 
     def load_context(self, context: mlflow.pyfunc.PythonModelContext) -> None:
         """
@@ -44,12 +49,23 @@ class BLIP2MLFlowModelWrapper(mlflow.pyfunc.PythonModel):
         :param context: MLflow context containing artifacts that the model can use for inference
         :type context: mlflow.pyfunc.PythonModelContext
         """
-        if self._task_type == (Tasks.IMAGE_TO_TEXT.value):
+        if self._task_type in [Tasks.IMAGE_TO_TEXT.value, Tasks.VISUAL_QUESTION_ANSWERING.value]:
             try:
                 model_dir = context.artifacts[MLflowLiterals.MODEL_DIR]
                 self._processor = AutoProcessor.from_pretrained(model_dir)
-                self._model = Blip2ForConditionalGeneration.from_pretrained(
-                    model_dir)
+
+                if self._model_id == HfBlipModelId.BLIP_IMAGE_TO_TEXT.value:
+                    self._model = BlipForConditionalGeneration.from_pretrained(
+                        model_dir)
+                elif self._model_id == HfBlipModelId.BLIP_VQA.value:
+                    self._model = BlipForQuestionAnswering.from_pretrained(
+                        model_dir)
+                elif self._model_id in [HfBlipModelId.BLIP2.value]:
+                    self._model = Blip2ForConditionalGeneration.from_pretrained(
+                        model_dir)
+                else:
+                    raise ValueError(f"invalid model id {self._model_id}")
+
                 self._device = get_current_device()
                 self._model.to(self._device)
 
@@ -84,9 +100,17 @@ class BLIP2MLFlowModelWrapper(mlflow.pyfunc.PythonModel):
                 .tolist()
             )
 
-            generated_text_list = self.run_inference_batch(
-                image_path_list=image_path_list,
-            )
+            if self._task_type == Tasks.IMAGE_TO_TEXT.value:
+                generated_text_list = self.run_inference_batch(
+                    image_path_list=image_path_list,
+                )
+            elif self._task_type == Tasks.VISUAL_QUESTION_ANSWERING.value:
+                generated_text_list = self.run_inference_batch(
+                    image_path_list=image_path_list,
+                    question_list=input_data["text"].tolist()
+                )
+            else:
+                raise ValueError(f"invalid task type {self._task_type}")
 
         df_result = pd.DataFrame(
             columns=[
@@ -100,19 +124,31 @@ class BLIP2MLFlowModelWrapper(mlflow.pyfunc.PythonModel):
 
     def run_inference_batch(
         self,
-        image_path_list: List
+        image_path_list: List,
+        question_list: List = None
     ) -> List[str]:
         """Perform inference on batch of input images.
 
         :param image_path_list: list of image paths for inferencing.
         :type image_path_list: List
+        :param question_list: list of questions for VQA Task. This is ignored for Image-To-Text task type.
+        :type question_list: List
         :return: List of generated texts
         :rtype: List of strings
         """
         image_list = [Image.open(img_path) for img_path in image_path_list]
 
-        inputs = self._processor(images=image_list,
-                                 return_tensors="pt").to(self._device)
+        if self._task_type == Tasks.IMAGE_TO_TEXT.value:
+            inputs = self._processor(images=image_list,
+                                     return_tensors="pt").to(self._device)
+        elif self._task_type == Tasks.VISUAL_QUESTION_ANSWERING.value:
+            inputs = self._processor(images=image_list,
+                                     text=question_list,
+                                     padding=True,
+                                     return_tensors="pt").to(self._device)
+        else:
+            raise ValueError(f"invalid task type {self._task_type}")
+
         generated_ids = self._model.generate(**inputs)
         generated_text_list = self._processor.batch_decode(generated_ids, skip_special_tokens=True)
 
