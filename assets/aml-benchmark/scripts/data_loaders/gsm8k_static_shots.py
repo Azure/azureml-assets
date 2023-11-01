83f052283f29fd267a552b4912eb0a06fd512da6
Eric Wright <erwright@microsoft.com>
2023-10-31 21:09:47 -0700

    remove errant file, linting

--- assets/aml-benchmark/scripts/data_loaders/gsm8k_static_shots.py
+++ assets/aml-benchmark/scripts/data_loaders/gsm8k_static_shots.py
@@ -19,14 +19,8 @@ _CITATION = """\
 """
 
 _DESCRIPTION = """\
-We explore how generating a chain of thought -- a series of intermediate reasoning steps -- significantly \
-improves the ability of large language models to perform complex reasoning. In particular, we show how \
-such reasoning abilities emerge naturally in sufficiently large language models via a simple method called \
-chain of thought prompting, where a few chain of thought demonstrations are provided as exemplars in prompting. \
-Experiments on three large language models show that chain of thought prompting improves performance on a range \
-of arithmetic, commonsense, and symbolic reasoning tasks. The empirical gains can be striking. For instance, \
-prompting a 540B-parameter language model with just eight chain of thought exemplars achieves state of the art \
-accuracy on the GSM8K benchmark of math word problems, surpassing even finetuned GPT-3 with a verifier.
+This dataset contains eight examples of math word problems, with answers, for full chain-of-thought prompting.
+The original data is from Wei et al., Appendix G.
 """
 
 _GSM_STATIC_SHOT_DATA = [
@@ -87,7 +81,7 @@ class GSM8kStaticShots(datasets.GeneratorBasedBuilder):
         datasets.BuilderConfig(
             name="fewshot",
             version=datasets.Version("1.0.0"),
-            description=f"GSM8K - Static shots"
+            description="GSM8K - Static shot data from Wei et al"
         )
     ]
 
