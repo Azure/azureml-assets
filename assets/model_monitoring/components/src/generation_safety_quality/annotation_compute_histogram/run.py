6c1b95b6c51616a24a006bbc183c63988b2906fe
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-11-01 15:44:14 -0700

    throw Not ImplementedError

--- assets/model_monitoring/components/src/generation_safety_quality/annotation_compute_histogram/run.py
+++ assets/model_monitoring/components/src/generation_safety_quality/annotation_compute_histogram/run.py
@@ -1534,7 +1534,7 @@ def apply_annotation(
 ):
     """Apply annotation to all samples in the production_dataset."""
     if "chat_history" in [prompt_column_name, completion_column_name, context_column_name, ground_truth_column_name]:
-        raise ValueError("chat_history column is not currently supported and cannot be used as specified column. ")
+        raise NotImplementedError("chat_history column is not currently supported and cannot be used as specified column. ")
 
     production_df = io_utils.read_mltable_in_spark(production_dataset)
     if production_df.count() == 0:
