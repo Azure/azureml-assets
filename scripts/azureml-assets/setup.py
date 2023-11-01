965a6f7e26c240a3ebdfb19e40f8ff8cdbb443d5
Alex Kalita <alexka@microsoft.com>
2023-11-01 13:20:53 -0700

    Update setup.py to include package files

--- scripts/azureml-assets/setup.py
+++ scripts/azureml-assets/setup.py
@@ -11,6 +11,7 @@ setup(
    description="Utilities for publishing assets to Azure Machine Learning system registries.",
    author="Microsoft Corp",
    packages=find_packages(),
+   include_package_data=True,
    install_requires=[
       "GitPython>=3.1",
       "ruamel.yaml==0.17.21",
