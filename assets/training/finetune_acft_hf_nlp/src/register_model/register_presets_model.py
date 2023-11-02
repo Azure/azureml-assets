ad9fd29889049f6a4637d1b32c64e85bcd7e7254
Pavan Manoj Jonnalagadda <pavanmanojj@microsoft.com>
2023-11-02 19:05:44 +0530

    reformatted file

--- assets/training/finetune_acft_hf_nlp/src/register_model/register_presets_model.py
+++ assets/training/finetune_acft_hf_nlp/src/register_model/register_presets_model.py
@@ -24,7 +24,10 @@ logger = get_logger_app(__name__)
 def get_dataset_relative_path(dataset):
     steps = dataset._dataflow._get_steps()
     step_arguments = steps[0].arguments
-    source = [(store['datastoreName'], store['path']) for store in step_arguments['datastores']]
+    source = [
+        (store["datastoreName"], store["path"])
+        for store in step_arguments["datastores"]
+    ]
     data_paths = [x[1] for x in source]
     return data_paths[0]
 
@@ -37,37 +40,47 @@ def create_v1_dataset(run_id, ws):
     rel_path = f"azureml/{run_id}/mlflow_model_folder"
     # pdb.set_trace()
     datastore = Datastore.get(ws, datastore_name)
-    finetuned_model_dataset = Dataset.File.from_files(path=[(datastore, rel_path)], validate=False)
+    finetuned_model_dataset = Dataset.File.from_files(
+        path=[(datastore, rel_path)], validate=False
+    )
     finetuned_model_dataset = finetuned_model_dataset.register(
-        workspace=ws,
-        name="register_model_dataset_" + run_id
+        workspace=ws, name="register_model_dataset_" + run_id
     )
     return finetuned_model_dataset
 
 
 def get_model_dataset(run_details, is_v2):
     if is_v2:
-        for d in run_details['outputDatasets']:
-            if d['outputDetails']['outputName'] == "output_model":
-                return d['dataset']
+        for d in run_details["outputDatasets"]:
+            if d["outputDetails"]["outputName"] == "output_model":
+                return d["dataset"]
         raise Exception("Unable to find output_model port.")
     else:
-        return run_details['inputDatasets'][0]['dataset']
+        return run_details["inputDatasets"][0]["dataset"]
 
 
 def get_modelregistry_url(workspace):
     from azureml._restclient.assets_client import AssetsClient
+
     assets_client = AssetsClient(workspace.service_context)
     modelregistry_url = assets_client.get_cluster_url()
-    uri = '/modelmanagement/{}/subscriptions/{}/resourceGroups/{}/providers/' \
-          'Microsoft.MachineLearningServices/workspaces/{}/models'.format("v1.0",
-                                                                   workspace.subscription_id,
-                                                                   workspace.resource_group,
-                                                                   workspace.name)
+    uri = (
+        "/modelmanagement/{}/subscriptions/{}/resourceGroups/{}/providers/"
+        "Microsoft.MachineLearningServices/workspaces/{}/models".format(
+            "v1.0", workspace.subscription_id, workspace.resource_group, workspace.name
+        )
+    )
     return modelregistry_url + uri
 
 
-def submit_rest_request(request_type, request_uri, request_body, params=None, headers=None, use_auto_version=False):
+def submit_rest_request(
+    request_type,
+    request_uri,
+    request_body,
+    params=None,
+    headers=None,
+    use_auto_version=False,
+):
     # TODO: This relies on the v1 SDK. Handling will need to be adapted to shift to v2
 
     # retry_count = RETRY_NUM_FOR_409 if use_auto_version else 1
@@ -76,33 +89,66 @@ def submit_rest_request(request_type, request_uri, request_body, params=None, he
     while True:
         logger.info(f"Attempt number {i + 1} for model registration.")
         try:
-            resp = ClientBase._execute_func(request_type, request_uri, params=params, headers=headers, json=request_body)
+            resp = ClientBase._execute_func(
+                request_type,
+                request_uri,
+                params=params,
+                headers=headers,
+                json=request_body,
+            )
             resp.raise_for_status()
             return resp
         except requests.exceptions.HTTPError:
             # check if model has been registered already
             if resp.status_code == 409:
-                logger.warning('This model id (name:version combination) already exists.')
-                logger.info('Checking if this model has already been registered by current RunId previous attempts/retries.')
-                if not use_auto_version and is_model_registered_already(request_uri, request_body, params, headers):
-                    logger.info('Good news! This model has already been registered by current RunId.')
+                logger.warning(
+                    "This model id (name:version combination) already exists."
+                )
+                logger.info(
+                    "Checking if this model has already been registered by current RunId previous attempts/retries."
+                )
+                if not use_auto_version and is_model_registered_already(
+                    request_uri, request_body, params, headers
+                ):
+                    logger.info(
+                        "Good news! This model has already been registered by current RunId."
+                    )
                     return resp
                 i += 1
-                logger.error('This model id has already been used for a different model registration.')
+                logger.error(
+                    "This model id has already been used for a different model registration."
+                )
                 if i == retry_count:
-                    logger.error('Exceeded all retry attempts.')
+                    logger.error("Exceeded all retry attempts.")
                 else:
-                    logger.error('Attempting to retry again with autoVersion set to True.')
-                    time.sleep(randint(1000, 5000)/1000) # sleep randomly from 1 second to 5 seconds
+                    logger.error(
+                        "Attempting to retry again with autoVersion set to True."
+                    )
+                    time.sleep(
+                        randint(1000, 5000) / 1000
+                    )  # sleep randomly from 1 second to 5 seconds
                     continue
 
-            raise Exception('Received bad response from POST Model request:\n'
-                            'Response Code: {}\n'
-                            'Headers: {}\n'
-                            'Content: {}'.format(resp.status_code, resp.headers, resp.content))
+            raise Exception(
+                "Received bad response from POST Model request:\n"
+                "Response Code: {}\n"
+                "Headers: {}\n"
+                "Content: {}".format(resp.status_code, resp.headers, resp.content)
+            )
 
 
-def register_model(workspace, model_name, model_version, dataset_id, run_id, is_v2, tags=None, properties=None, description=None, model_format="CUSTOM"):
+def register_model(
+    workspace,
+    model_name,
+    model_version,
+    dataset_id,
+    run_id,
+    is_v2,
+    tags=None,
+    properties=None,
+    description=None,
+    model_format="CUSTOM",
+):
     """Register a model with the provided workspace.
 
     :param workspace: The workspace to register the model with.
@@ -131,16 +177,20 @@ def register_model(workspace, model_name, model_version, dataset_id, run_id, is_
                 raise ValueError("Tags must be a dict")
             tags = json.loads(json.dumps(tags))
         except ValueError:
-            raise ValueError('Error with JSON serialization for tags, '
-                            'be sure they are properly formatted.')
+            raise ValueError(
+                "Error with JSON serialization for tags, "
+                "be sure they are properly formatted."
+            )
     if properties:
         try:
             if not isinstance(properties, dict):
                 raise ValueError("Properties must be a dict")
             properties = json.loads(json.dumps(properties))
         except ValueError:
-            raise ValueError('Error with JSON serialization for properties, '
-                            'be sure they are properly formatted.')
+            raise ValueError(
+                "Error with JSON serialization for properties, "
+                "be sure they are properly formatted."
+            )
 
     request_url = get_modelregistry_url(workspace)
 
@@ -152,27 +202,35 @@ def register_model(workspace, model_name, model_version, dataset_id, run_id, is_
         "mimeType": "application/x-python",
         "properties": properties,
         "kvTags": tags,
-        "modelFormat": model_format
+        "modelFormat": model_format,
     }
 
     use_auto_version = is_v2 and model_version is None
 
-    request_params = {'autoVersion': 'true' if use_auto_version else 'false'}
+    request_params = {"autoVersion": "true" if use_auto_version else "false"}
     if not use_auto_version:
         request_body["version"] = model_version
 
-    request_headers = {'Content-Type': 'application/json'}
+    request_headers = {"Content-Type": "application/json"}
 
     request_headers.update(workspace._auth_object.get_authentication_header())
 
-    logger.info('Starting register model request')
+    logger.info("Starting register model request")
     logger.info(f"Request body: {request_body}")
-    resp = submit_rest_request(get_requests_session().post, request_url, request_body,
-                                request_params, request_headers, use_auto_version=use_auto_version)
-    logger.info('Done register model request')
+    resp = submit_rest_request(
+        get_requests_session().post,
+        request_url,
+        request_body,
+        request_params,
+        request_headers,
+        use_auto_version=use_auto_version,
+    )
+    logger.info("Done register model request")
     resp_json = resp.json()
-    if 'name' in resp_json and 'version' in resp_json:
-        logger.info(f"Registered model {resp_json['name']} with version {resp_json['version']}")
+    if "name" in resp_json and "version" in resp_json:
+        logger.info(
+            f"Registered model {resp_json['name']} with version {resp_json['version']}"
+        )
     return resp_json
 
 
@@ -185,7 +243,7 @@ def registermodel_entrypoint(
     registered_model_version=None,
     is_v2=False,
     create_v1_dataset_for_registration=True,
-    properties=None
+    properties=None,
 ):
     logger.info("Starting registermodel")
 
@@ -214,7 +272,7 @@ def registermodel_entrypoint(
         ws = run._experiment.workspace
 
         # get pipeline run id
-        run_id = run_details['runId']
+        run_id = run_details["runId"]
         top_level_run = run
         while top_level_run.parent:
             top_level_run = top_level_run.parent
@@ -278,7 +336,9 @@ def registermodel_entrypoint(
     # base_model_name = get_model_engine(model_json_path).lower()
 
     # # Manifest file for engine/controller and pipe
-    engine_controller_manifest_path = "azureml/{}/output_model/manifest.enginecontroller.json".format(run_id)
+    engine_controller_manifest_path = (
+        "azureml/{}/output_model/manifest.enginecontroller.json".format(run_id)
+    )
     pipe_manifest_path = "azureml/{}/output_model/manifest.pipe.json".format(run_id)
 
     # Model properties is a dict({str : str}), all values must be converted to string type.
@@ -311,19 +371,20 @@ def registermodel_entrypoint(
     # else:
     #     properties["modelArchitecture"] = get_model_architecture(base_model_name)
 
-
     # model_details["modelName"] = model_name
 
     # logging.info("Registering model with properties: {}".format(properties))
 
-    resp_json = register_model(workspace=ws,
-                        model_name=model_name,
-                        model_version=registered_model_version,
-                        dataset_id=dataset_id,
-                        run_id=top_level_run.id,
-                        is_v2=is_v2,
-                        properties=properties,
-                        model_format="PRESETS")
+    resp_json = register_model(
+        workspace=ws,
+        model_name=model_name,
+        model_version=registered_model_version,
+        dataset_id=dataset_id,
+        run_id=top_level_run.id,
+        is_v2=is_v2,
+        properties=properties,
+        model_format="PRESETS",
+    )
 
     # if 'version' in resp_json:
     #     model_details["modelVersion"] = int(resp_json['version'])
