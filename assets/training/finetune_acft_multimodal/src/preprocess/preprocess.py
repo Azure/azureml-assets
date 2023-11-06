2973aa0fe355ac333d6c86b248e4cfe5479d083e
shubhamiit <41925087+shubhamiit@users.noreply.github.com>
2023-11-06 13:26:44 +0530

    fix cpyright header

--- assets/training/finetune_acft_multimodal/src/preprocess/preprocess.py
+++ assets/training/finetune_acft_multimodal/src/preprocess/preprocess.py
@@ -1,6 +1,5 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 """Read the args from preprocess component"""
 
