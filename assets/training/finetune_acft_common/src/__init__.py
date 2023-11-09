8959b0dfeaa0fbaa9e5d9434f3183ad71add6dd9
Rupal Jain <rupaljain@microsoft.com>
2023-11-09 19:43:35 +0530

    Adding common validation component

---
+++ assets/training/finetune_acft_common/src/__init__.py
@@ -0,0 +1,5 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Contains python files corrrsponding to the image components."""
