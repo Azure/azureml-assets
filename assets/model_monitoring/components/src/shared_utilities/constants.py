cb780e4abd8940931239e09433ceee58254f95c4
Richard Li <richli@microsoft.com>
2023-11-09 10:27:08 +0800

    add tag name to constants

--- assets/model_monitoring/components/src/shared_utilities/constants.py
+++ assets/model_monitoring/components/src/shared_utilities/constants.py
@@ -90,4 +90,7 @@ MDC_CHAT_HISTORY_COLUMN = 'chat_history'
 MDC_CORRELATION_ID_COLUMN = 'correlationid'
 MDC_DATA_COLUMN = 'data'
 MDC_DATAREF_COLUMN = 'dataref'
+
 SCHEMA_INFER_ROW_COUNT = 10
+
+AML_MOMO_INPUT_NOT_FOUND_TAG = "azureml.modelmonitor.input.not.found"
