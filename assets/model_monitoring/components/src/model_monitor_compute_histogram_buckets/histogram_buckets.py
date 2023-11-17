b3103707c8d3ab883b755ed147f951e2dade660b
Vivienne Tang <yuata@microsoft.com>
2023-11-17 13:50:58 -0800

    fix e2e

--- assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/histogram_buckets.py
+++ assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/histogram_buckets.py
@@ -24,7 +24,7 @@ def compute_numerical_bins(
     # Generate histograms only for columns in both baseline and target dataset
     common_columns_dict = get_common_columns(df1, df2)
     numerical_columns = get_numerical_cols_with_df(common_columns_dict, df1)
-    if numerical_columns is None or numerical_columns.isEmpty():
+    if numerical_columns is None or len(numerical_columns) <= 0:
         raise ValueError("Could not find common columns for compute histogram buckets.")
     # Numerical column histogram generation
     baseline_count = df1.count()
