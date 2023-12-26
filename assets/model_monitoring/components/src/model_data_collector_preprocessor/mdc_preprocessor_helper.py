9607b05aa6d823bb13f10f9ab26af6e0b7ae62d7
Richard Li <richli@microsoft.com>
2023-12-26 14:59:37 +0800

    refactor get_file_list()

--- assets/model_monitoring/components/src/model_data_collector_preprocessor/mdc_preprocessor_helper.py
+++ assets/model_monitoring/components/src/model_data_collector_preprocessor/mdc_preprocessor_helper.py
@@ -6,7 +6,9 @@
 import os
 from typing import List, Union
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 import json
+import calendar
 from azure.core.credentials import AzureSasCredential
 from azure.identity import ClientSecretCredential
 from pyspark.sql import SparkSession
@@ -16,23 +18,140 @@ from model_data_collector_preprocessor.store_url import StoreUrl
 def get_file_list(start_datetime: datetime, end_datetime: datetime, store_url: StoreUrl = None,
                   input_data: str = None) -> List[str]:
     """Get the available file list for the given time window under the input_data folder."""
+    def date_range(start, end, step):
+        cur = start
+        while cur < end:
+            yield cur
+            cur += step
+
+    def is_same_year(start, end) -> bool:
+        return start.year == end.year
+
+    def is_same_month(start, end) -> bool:
+        return is_same_year(start, end) and start.month == end.month
+
+    def is_same_day(start, end) -> bool:
+        return is_same_month(start, end) and start.day == end.day
+
+    def is_start_of_year(d) -> bool:
+        return (d.month, d.day, d.hour) == (1, 1, 0)
+
+    def is_end_of_year(d) -> bool:
+        return (d.month, d.day, d.hour) == (12, 31, 23)
+
+    def is_start_of_month(d) -> bool:
+        return (d.day, d.hour) == (1, 0)
+
+    def is_end_of_month(d) -> bool:
+        _, month_days = calendar.monthrange(d.year, d.month)
+        return (d.day, d.hour) == (month_days, 23)
+
+    def is_start_of_day(d) -> bool:
+        return d.hour == 0
+
+    def is_end_of_day(d) -> bool:
+        return d.hour == 23
+
+    def same_year(start, end) -> List[str]:
+        if is_same_month(start, end):
+            return same_month(start, end)
+        return cross_month(start, end)
+
+    def same_month(start, end) -> List[str]:
+        if is_same_day(start, end):
+            return same_day(start, end)
+        return cross_day()
+
+    def same_day(start, end) -> List[str]:
+        if is_start_of_day(start) and is_end_of_day(end):
+            return [store_url.get_abfs_url(f"{start.strftime('%Y/%m/%d')}/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{start.strftime('%Y/%m/%d')}") else []
+        return cross_hour(start, end)
+
+    def start_of_year(y) -> List[str]:
+        if is_end_of_year(y):
+            return [store_url.get_abfs_url(f"{y.strftime('%Y')}/*/*/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{y.strftime('%Y')}") else []
+        return same_year(datetime(y.year, 1, 1, 0, 0, 0), y)
+
+    def end_of_year(y) -> List[str]:
+        if is_start_of_year(y):
+            return [store_url.get_abfs_url(f"{y.strftime('%Y')}/*/*/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{y.strftime('%Y')}") else []
+        return same_year(y, datetime(y.year, 12, 31, 23, 59, 59))
+
+    def start_of_month(m) -> List[str]:
+        if is_end_of_month(m):
+            return [store_url.get_abfs_url(f"{m.strftime('%Y/%m')}/*/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{m.strftime('%Y/%m')}") else []
+        return same_month(datetime(m.year, m.month, 1, 0, 0, 0), m)
+
+    def end_of_month(m) -> List[str]:
+        if is_start_of_month(m):
+            return [store_url.get_abfs_url(f"{m.strftime('%Y/%m')}/*/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{m.strftime('%Y/%m')}") else []
+        _, month_days = calendar.monthrange(m.year, m.month)
+        return same_month(m, datetime(m.year, m.month, month_days, 23, 59, 59))
+
+    def start_of_day(d) -> List[str]:
+        if is_end_of_day(d):
+            return [store_url.get_abfs_url(f"{d.strftime('%Y/%m/%d')}/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{d.strftime('%Y/%m/%d')}") else []
+        return same_day(datetime(d.year, d.month, d.day, 0, 0, 0), d)
+
+    def end_of_day(d) -> List[str]:
+        if is_start_of_day(d):
+            return [store_url.get_abfs_url(f"{d.strftime('%Y/%m/%d')}/*/*.jsonl")] \
+                if store_url.is_folder_exists(f"{d.strftime('%Y/%m/%d')}") else []
+        return same_day(d, datetime(d.year, d.month, d.day, 23, 59, 59))
+
+    def cross_year(start, end) -> List[str]:
+        middle_years = [
+            store_url.get_abfs_url(f"{y.strftime('%Y')}/*/*/*/*.jsonl") for y in range(start.year+1, end.year)
+            if store_url.is_folder_exists(f"{y.strftime('%Y')}")
+        ]
+        return end_of_year(start) + middle_years + start_of_year(end)
+
+    def cross_month(start: datetime, end) -> List[str]:
+        _start = (start + relativedelta(months=1)).replace(day=1, hour=1)
+        _end = end.replace(day=1, hour=0)  # skip last month
+        middle_months = [
+            store_url.get_abfs_url(f"{m.strftime('%Y/%m')}/*/*/*.jsonl")
+            for m in date_range(_start, _end, relativedelta(months=1))
+            if store_url.is_folder_exists(f"{m.strftime('%Y/%m')}")
+        ]
+        return end_of_month(start) + middle_months + start_of_month(end)
+
+    def cross_day(start, end) -> List[str]:
+        _start = (start + timedelta(days=1)).replace(hour=1)
+        _end = end.replace(hour=0)  # skip last day
+        middle_days = [
+            store_url.get_abfs_url(f"{d.strftime('%Y/%m/%d')}/*/*.jsonl")
+            for d in date_range(_start, _end, timedelta(days=1))
+            if store_url.is_folder_exists(f"{d.strftime('%Y/%m/%d')}")
+        ]
+        return end_of_day(start) + middle_days + start_of_day(end)
+
+    def cross_hour(start, end) -> List[str]:
+        _start = start.replace(minute=0)
+        _end = end.replace(minutes=59)
+        return [
+            store_url.get_abfs_url(f"{h.strftime('%Y/%m/%d/%H')}/*.jsonl")
+            for h in date_range(_start, _end, timedelta(hours=1))
+            if store_url.is_folder_exists(f"{h.strftime('%Y/%m/%d/%H')}")
+        ]
+
     store_url = store_url or StoreUrl(input_data)
     if store_url.is_local_path():
         # for local testing
         return _get_local_file_list(start_datetime, end_datetime, store_url._base_url)
 
-    file_list = []
     if end_datetime.minute == 0 and end_datetime.second == 0:
         # if end_datetime is a whole hour, the last hour folder is not needed
         end_datetime -= timedelta(seconds=1)
-    cur_datetime = start_datetime
-    # TODO check day folder and use */*.jsonl if day folder exists
-    while cur_datetime <= end_datetime:
-        if store_url.is_folder_exists(f"{cur_datetime.strftime('%Y/%m/%d/%H')}"):
-            cur_folder = store_url.get_abfs_url(f"{cur_datetime.strftime('%Y/%m/%d/%H')}")
-            file_list.append(f"{cur_folder}/*.jsonl")
-        cur_datetime += timedelta(hours=1)
-    return file_list
+    if is_same_year(start_datetime, end_datetime):
+        return same_year(start_datetime, end_datetime)
+    return cross_year(start_datetime, end_datetime)
 
 
 def set_data_access_config(spark: SparkSession, input_data: str = None, store_url: StoreUrl = None):
