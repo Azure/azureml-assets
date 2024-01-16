5c9e4fd1eb14af89e18f2ca1151f6c0f394930a5
Alan Poblette <alanpoblette@microsoft.com>
2024-01-16 14:32:27 -0800

    syntax

--- assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/histogram_buckets.py
+++ assets/model_monitoring/components/src/model_monitor_compute_histogram_buckets/histogram_buckets.py
@@ -17,6 +17,7 @@ from pyspark.sql.types import (
 from shared_utilities.io_utils import init_spark
 from shared_utilities.momo_exceptions import InvalidInputError
 
+
 def compute_numerical_bins(
     df1: pyspark_sql.DataFrame,
     df2: pyspark_sql.DataFrame,
