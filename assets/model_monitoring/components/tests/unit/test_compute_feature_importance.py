6f4b9ea46477be97ad590e7402d1d98ce85d2ad5
Yuan Cheng <yuacheng@microsoft.com>
2024-01-18 21:35:02 -0800

    fix feature importance Circular reference

--- assets/model_monitoring/components/tests/unit/test_compute_feature_importance.py
+++ assets/model_monitoring/components/tests/unit/test_compute_feature_importance.py
@@ -9,6 +9,7 @@ from feature_importance_metrics.compute_feature_attribution_drift import (
     drop_metadata_columns, calculate_attribution_drift)
 import pytest
 import pandas as pd
+import datetime
 
 
 @pytest.fixture
@@ -22,10 +23,14 @@ def get_fraud_data():
         "ACCOUNTID": ["A1176337474875483", "A1343835256155075",
                       "A1343835256155076", "A1706480214256418"],
         "TRANSACTIONAMOUNT": [146161.99, 57487.200000000004,  227728.76, 59340.0],
-        "TIMESTAMP": ["2023-01-30T16:25:17.000Z",
-                      "2023-01-30T16:58:44.000Z",
-                      "2023-01-30T22:46:37.000Z",
-                      "2023-01-30T22:46:37.000Z"],
+        "TIMESTAMPSTR": ["2023-01-30T16:25:17.000Z",
+                         "2023-01-30T16:58:44.000Z",
+                         "2023-01-30T22:46:37.000Z",
+                         "2023-01-30T22:46:37.000Z"],
+        "TIMESTAMP": [pd.Timestamp('2023-12-04T17:59:32'),
+                      pd.Timestamp('2023-12-04T17:59:32'),
+                      pd.Timestamp('2023-12-04T17:59:32'),
+                      pd.Timestamp('2023-12-04T17:59:32')],
         "TRANSACTIONAMOUNTUSD": [169547.9084, 57487.200000000004, 266442.6492, 67647.6],
         "ISPROXYIP": [False, True, False, False],
         "DIGITALITEMCOUNT": [15, 15, 15, 15],
@@ -64,16 +69,17 @@ class TestComputeFeatureImportanceMetrics:
 
     def test_mark_categorical_column(self, get_fraud_data):
         """Test deteremine task type for classification scenario."""
-        categorical_features_lgbm = ["TRANSACTIONID", "ACCOUNTID", "TIMESTAMP", "ISPROXYIP"]
+        categorical_features_lgbm = ["TRANSACTIONID", "ACCOUNTID", "TIMESTAMPSTR", "TIMESTAMP", "ISPROXYIP"]
         numerical_features = ["TRANSACTIONAMOUNT", "TRANSACTIONAMOUNTUSD", "DIGITALITEMCOUNT"]
         target_column = "IS_FRAUD"
 
         mark_categorical_column(get_fraud_data, target_column, categorical_features_lgbm, numerical_features)
-
+        # timestamp/date should be mark as int
+        assert get_fraud_data.dtypes['TIMESTAMP'] == "int64"
         # categorical columns should mark as category
+        assert get_fraud_data.dtypes['TIMESTAMPSTR'] == "category"
         assert get_fraud_data.dtypes['TRANSACTIONID'] == "category"
         assert get_fraud_data.dtypes['ACCOUNTID'] == "category"
-        assert get_fraud_data.dtypes['TIMESTAMP'] == "category"
         assert get_fraud_data.dtypes['ISPROXYIP'] == "category"
         # non-categorical columns should not mark as category
         assert get_fraud_data.dtypes['TRANSACTIONAMOUNT'] == "float64"
