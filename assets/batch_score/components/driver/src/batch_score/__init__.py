375d4823b9262db138eb36c3e7a9f7d9571eaf6e
Ye Tao <yeta@microsoft.com>
2024-01-23 20:12:39 -0800

    fix tiktoken

--- assets/batch_score/components/driver/src/batch_score/__init__.py
+++ assets/batch_score/components/driver/src/batch_score/__init__.py
@@ -4,3 +4,9 @@
 # flake8: noqa: F401
 
 """__init__."""
+
+import subprocess
+import sys
+
+# TODO: remove this hack after dedicated environment is published.
+subprocess.check_call([sys.executable, "-m", "pip", "install", 'tiktoken==0.5.2'])
