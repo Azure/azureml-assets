cf116382a4ea65b164f9ce34f105d15e3b659249
Ye Tao <yeta@microsoft.com>
2024-01-23 20:39:46 -0800

    fix

--- assets/batch_score/components/driver/src/batch_score/__init__.py
+++ assets/batch_score/components/driver/src/batch_score/__init__.py
@@ -9,4 +9,4 @@ import subprocess
 import sys
 
 # TODO: remove this hack after dedicated environment is published.
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'tiktoken==0.5.2'])
+subprocess.check_call([sys.executable, "-m", "pip", "install", 'tiktoken==0.5.2', 'StrEnum==0.4.15'])
