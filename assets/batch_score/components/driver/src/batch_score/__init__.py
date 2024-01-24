656f37ab24fbe6b9d25675850ba51cb604758fdc
Ye Tao <yeta@microsoft.com>
2024-01-23 21:04:44 -0800

    fix

--- assets/batch_score/components/driver/src/batch_score/__init__.py
+++ assets/batch_score/components/driver/src/batch_score/__init__.py
@@ -9,4 +9,7 @@ import subprocess
 import sys
 
 # TODO: remove this hack after dedicated environment is published.
-subprocess.check_call([sys.executable, "-m", "pip", "install", 'tiktoken==0.5.2', 'StrEnum==0.4.15'])
+subprocess.check_call([sys.executable,"-m", "pip", "install",
+                       'PyDispatcher==2.0.7',
+                       'StrEnum==0.4.15',
+                       'tiktoken==0.5.2'])
