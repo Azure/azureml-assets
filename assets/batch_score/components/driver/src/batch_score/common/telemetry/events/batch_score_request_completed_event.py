c3121ba85c7a8a9eefbb8b8bd12ef42fbf6adf12
adba-msft <80413769+adba-msft@users.noreply.github.com>
2024-01-24 21:42:13 -0800

    Port over the latest batch score changes from the ADO repository (#2176)
    * copy from ADO
    
    * fix linter errors
    
    * bump version in metadata.json
    
    * bump version in spec.yaml

--- assets/batch_score/components/driver/src/batch_score/common/telemetry/events/batch_score_request_completed_event.py
+++ assets/batch_score/components/driver/src/batch_score/common/telemetry/events/batch_score_request_completed_event.py
@@ -6,6 +6,8 @@
 from dataclasses import dataclass, field
 from .batch_score_event import BatchScoreEvent
 
+from .. import logging_utils as lu
+
 
 # TODO: Add comments to describe each field
 @dataclass
@@ -17,10 +19,10 @@ class BatchScoreRequestCompletedEvent(BatchScoreEvent):
         """Get the name of the event."""
         return "BatchScore.Request.Completed"
 
-    minibatch_id: str = field(init=True, default=None)
+    minibatch_id: str = field(init=True, default_factory=lu.get_mini_batch_id)
     input_row_id: str = field(init=True, default=None)
     x_ms_client_request_id: str = field(init=True, default=None)
-    worker_id: str = field(init=True, default=None)
+    worker_id: str = field(init=True, default_factory=lu.get_worker_id)
     segmented_request_id: int = field(init=True, default=None)
     scoring_url: str = field(init=True, default=None)
     is_successful: bool = field(init=True, default=None)
