a88a020108a36d07f61d5c8ee6fe34de14360022
svaruag <gasi@microsoft.com>
2024-01-30 10:15:57 -0800

    add more logs

--- assets/aml-benchmark/components/src/aml_benchmark/utils/logging.py
+++ assets/aml-benchmark/components/src/aml_benchmark/utils/logging.py
@@ -49,19 +49,27 @@ def get_logger(filename: str) -> logging.Logger:
     Create and configure a logger based on the provided filename.
 
     This function creates a logger with the specified filename and configures it
-    by setting the logging level to INFO, adding a StreamHandler to the logger,
-    and specifying a specific log message format.
+    by setting the logging level to INFO, adding a StreamHandler and a FileHandler to the logger,
+    and specifying a specific log message format for both handlers.
 
     :param filename: The name of the file associated with the logger.
     :return: The configured logger.
     """
     logger = logging.getLogger(filename)
     logger.setLevel(logging.INFO)
+
+    # StreamHandler for stdout, temporary for debugging
     stream_handler = logging.StreamHandler()
     logger.addHandler(stream_handler)
+
+    # FileHandler for logging to a file
+    file_handler = logging.FileHandler(filename)
+    logger.addHandler(file_handler)
 
     formatter = logging.Formatter(
         "[%(asctime)s - %(name)s - %(levelname)s] - %(message)s"
     )
     stream_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
     return logger
