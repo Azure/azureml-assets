9442bfa1d89aab9e1af200d1719cc3442f243e3a
Adarsh Ramanathan <adrama@microsoft.com>
2024-02-01 13:04:20 -0800

    update tests

--- scripts/promptflow-ci/promptflow_ci.py
+++ scripts/promptflow-ci/promptflow_ci.py
@@ -156,7 +156,7 @@ if __name__ == "__main__":
     parser.add_argument('--flow_submit_mode', type=str, default="sync")
     parser.add_argument('--run_time', type=str, default="default-mir")
     parser.add_argument('--skipped_flows', type=str,
-                        default="bring_your_own_data_qna,template_chat_flow,template_eval_flow")
+                        default="template_chat_flow,template_eval_flow")
     # Skip bring_your_own_data_qna test, the flow has a bug.
     # Bug 2773738: Add retry when ClientAuthenticationError
     # https://msdata.visualstudio.com/Vienna/_workitems/edit/2773738
