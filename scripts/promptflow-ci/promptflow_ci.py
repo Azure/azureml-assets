e5f634467eb7ea8292d8d4f4b21ccf70c7d3fdbc
Adarsh Ramanathan <adrama@microsoft.com>
2024-02-01 13:16:52 -0800

    get oid for rbac

--- scripts/promptflow-ci/promptflow_ci.py
+++ scripts/promptflow-ci/promptflow_ci.py
@@ -138,6 +138,9 @@ def check_flow_run_status(
 
 
 if __name__ == "__main__":
+    import os
+    os.system('az ad signed-in-user show')
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--tenant_id", type=str)
     parser.add_argument("--client_id", type=str)
