2910175871f4fb66a78892865eb3c0aaa3c6a545
EricWrightAtWork <erwright@microsoft.com>
2024-02-12 14:51:52 -0800

    add utils and tests

---
+++ assets/aml-benchmark/components/src/aml_benchmark/utils/evaluation_utils.py
@@ -0,0 +1,36 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ---------------------------------------------------------
+
+"""Common evaluation utilities for benchmark components."""
+
+import re
+
+def extract_text_from_markdown_tag(input_string: str, tag_type='python') -> str:
+    """
+    Extract text between markdown code tags.
+
+    If the tag pattern search returns no matches, the input string is returned.
+    """
+    pattern = f"```{tag_type}(.*?)```"
+    m = re.search(pattern, input_string, flags=re.DOTALL)
+    if not m:
+        pattern_partial = f"```{tag_type}(.*?)"
+        m = re.search(pattern_partial, input_string, flags=re.DOTALL)
+    return m.group(1) if m else input_string
+
+def extract_python_function(input_string: str, partial=False) -> str:
+    """
+    Extract a Python function from the input string.
+
+    This function handles two cases: partial and full. In the partial case, we assume the model provides the body
+    of the function, but not the declaration and argument list. In the full case, it's assumed the model
+    provides the full function definition.
+
+    If the code pattern search returns no matches, the input string is returned.
+    """
+    code_pattern = "(.*?)" if partial else "(.*?def.*?)"
+    pattern = f"{code_pattern}(\nclass|\ndef|\n#|\nif|\nfor|\nwhile|\nprint|$)"
+    m = re.search(pattern, input_string, flags=re.DOTALL)
+    return m.group(1) if m else input_string
---
+++ assets/aml-benchmark/tests/test_evaluation_utils.py
@@ -0,0 +1,33 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests for common evaluation utilities."""
+
+import pytest
+import sys
+
+from .test_utils import get_src_dir
+
+sys.path.append(get_src_dir())
+print(get_src_dir())
+from aml_benchmark.utils.evaluation_utils import extract_python_function, extract_text_from_markdown_tag
+
+@pytest.mark.parametrize(
+        'tag_open,tag_close',
+        [('```python', '```'), ('```python', ''), ('', '')]
+)
+def test_extract_markdown_tags(tag_open, tag_close):
+    python_func_text = 'def foo():\n    return 0\n'
+    response = tag_open + python_func_text + tag_close
+    response_parsed = extract_text_from_markdown_tag(response, tag_type='python')
+    assert response_parsed == python_func_text
+
+@pytest.mark.parametrize(
+        'is_partial,extra_text',
+        [(False, ''), (False, 'def new_func():\n'), (True, ''), (True, 'def new_func():\n')]
+)
+def test_extract_python_func(is_partial, extra_text):
+    python_func_text = 'import os\ndef foo():\n    return 0\n' if not is_partial else '    return 0\n'
+    response = python_func_text + extra_text
+    response_parsed = extract_python_function(response, partial=is_partial)
+    assert response_parsed == python_func_text
