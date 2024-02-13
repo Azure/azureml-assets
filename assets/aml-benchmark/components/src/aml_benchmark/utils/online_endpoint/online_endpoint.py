301b739233f3a7bd1c44f8d1e43cce72b06216c5
ccozianu <ccozianu@microsoft.com>
2024-02-13 09:31:59 -0800

    Ccozianu/4office debug (#2316)
    * add more logs
    
    * fix stdout logs
    
    * Make sure we recover details
    
    * Make sure we recover details (#2238)
    
    * SystemLog: prefix logging
    
    * Ccozi/temp logging fix (#2246)
    
    * Make sure we recover details
    
    * SystemLog: prefix logging
    
    * Adding more detailed logging
    
    * Fixing typo
    
    * fix NPE in logging
    
    * Cleaned up somehwat dependence on status definitions
    
    * handle folder
    
    * Added finetuned_start_timeout_seconds
    
    * Fix typo in main invocation
    
    * Add SystemLog: prefix for scorer
    
    * Fixed type error on timeout
    
    * Go to a different endpoint for connections listkeys call in batch_benchmark_score
    
    * Synced all batch-benchmark-inference patch version
    
    ---------
    
    Co-authored-by: svaruag <gasi@microsoft.com>

--- assets/aml-benchmark/components/src/aml_benchmark/utils/online_endpoint/online_endpoint.py
+++ assets/aml-benchmark/components/src/aml_benchmark/utils/online_endpoint/online_endpoint.py
@@ -438,7 +438,7 @@ class OnlineEndpoint:
         # https://management.azure.com/subscriptions/95e0ae98-c287-4f51-b23e-4e2317b7e169/resourcegroups/EYESON.HERON.PROD.b816bb35-33ec-4d3f-82b2-6cb692a5679e/providers/Microsoft.MachineLearningServices/workspaces/amlworkspaceyu3zl4oks34ye/connections/b-689314967ddd45/listsecrets?api-version=2023-02-01-preview
         final_url=f"https://management.azure.com/subscriptions/{self.curr_workspace.subscription_id}"\
                    f"/resourcegroups/{self.curr_workspace.resource_group}"\
-                   f"/providers/Microsoft.MachineLearningServices/workspaces/{self.curr_workspace.workspace_name}"\
+                   f"/providers/Microsoft.MachineLearningServices/workspaces/{self.curr_workspace.name}"\
                    f"/connections/{self._connections_name}/listsecrets?api-version=2023-02-01-preview"
         resp = self._send_post_request(final_url, {
             "Authorization": f"Bearer {bearer_token}",
