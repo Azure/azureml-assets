112a56472cc2f0fcc40c4513df5ed23b8e8fa6c7
Alan Poblette <alanpoblette@microsoft.com>
2024-02-12 19:09:17 -0800

    fix UT

--- assets/model_monitoring/components/tests/unit/test_trace_aggregator.py
+++ assets/model_monitoring/components/tests/unit/test_trace_aggregator.py
@@ -2,12 +2,11 @@
 # Licensed under the MIT License.
 """This file contains unit tests for the trace aggregator."""
 
-
+from pyspark.sql import SparkSession
 from src.model_data_collector_preprocessor.trace_aggregator import (
     process_spans_into_aggregated_traces,
     _get_aggregated_trace_log_spark_df_schema
 )
-from tests.e2e.utils.io_utils import create_pyspark_dataframe
 import pytest
 
 
@@ -15,8 +14,12 @@ import pytest
 class TestTraceAggregator:
     """Test class for Trace Aggregator."""
 
+    def init_spark(self)-> SparkSession:
+        return SparkSession.builder.appName("test").getOrCreate()
+
     def test_trace_aggregator_empty(self):
         """Test scenario where data is empty."""
-        processed_span_logs = create_pyspark_dataframe([], _get_aggregated_trace_log_spark_df_schema().fieldNames())
+        spark = self.init_spark()
+        processed_span_logs = spark.createDataFrame([], _get_aggregated_trace_log_spark_df_schema())
         trace_logs = process_spans_into_aggregated_traces(processed_span_logs)
         assert trace_logs.isEmpty()
