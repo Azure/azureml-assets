70b39129e83cf7fce85711a3d4223b5ca0b85377
Alan Poblette <alanpoblette@microsoft.com>
2024-02-16 14:29:16 -0800

    syntax

--- assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
+++ assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
@@ -71,23 +71,23 @@ class TestGenAISparkPreprocessor:
         ["{\"framework\":\"LLM\",\"inputs\":\"in\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"1\",\"trace_id\":\"01\",\"trace_state\":\"[]\"}"] +
         [datetime(2024, 2, 5, 0, 2, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 1,0), "OK", "01", "1", "llm", "LLM", "in", "out"], 
+        [datetime(2024, 2, 5, 0, 1, 0), "OK", "01", "1", "llm", "LLM", "in", "out"],
         ["{\"framework\":\"LLM\",\"inputs\":\"in\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"2\",\"trace_id\":\"01\",\"trace_state\":\"[]\"}"] +
         [datetime(2024, 2, 5, 0, 4, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 3,0), "OK", "01", "2", "llm", "LLM", "in", "out"], 
+        [datetime(2024, 2, 5, 0, 3, 0), "OK", "01", "2", "llm", "LLM", "in", "out"],
         ["{\"framework\":\"LLM\",\"inputs\":\"in\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"3\",\"trace_id\":\"02\",\"trace_state\":\"[]\"}"] +
         [datetime(2024, 2, 5, 0, 6, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 5,0), "OK", "02", "3", "llm", "LLM", "in", "out"], 
+        [datetime(2024, 2, 5, 0, 5, 0), "OK", "02", "3", "llm", "LLM", "in", "out"],
         ["{\"framework\":\"LLM\",\"inputs\":\"in\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"4\",\"trace_id\":\"02\",\"trace_state\":\"[]\"}"] +
         [datetime(2024, 2, 5, 0, 8, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 7,0), "OK", "02", "4", "llm", "LLM", "in", "out"], 
+        [datetime(2024, 2, 5, 0, 7, 0), "OK", "02", "4", "llm", "LLM", "in", "out"],
         ["{\"framework\":\"LLM\",\"inputs\":\"in\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"5\",\"trace_id\":\"02\",\"trace_state\":\"[]\"}"] +
         [datetime(2024, 2, 5, 0, 12, 0), "[]", "[]", "name", "4"] +
-        [datetime(2024, 2, 5, 0, 11,0), "OK", "02", "5", "llm", "LLM", "in", "out"], 
+        [datetime(2024, 2, 5, 0, 11, 0), "OK", "02", "5", "llm", "LLM", "in", "out"],
     ]
 
     _preprocessed_schema_no_input = StructType([
@@ -114,23 +114,23 @@ class TestGenAISparkPreprocessor:
         ["{\"framework\":\"LLM\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"1\",\"trace_id\":\"01\"}"] +
         [datetime(2024, 2, 5, 0, 2, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 1,0), "OK", "01", "1", "llm", "LLM", None, "out"], 
+        [datetime(2024, 2, 5, 0, 1, 0), "OK", "01", "1", "llm", "LLM", None, "out"],
         ["{\"framework\":\"LLM\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"2\",\"trace_id\":\"01\"}"] +
         [datetime(2024, 2, 5, 0, 4, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 3,0), "OK", "01", "2", "llm", "LLM", None, "out"], 
+        [datetime(2024, 2, 5, 0, 3, 0), "OK", "01", "2", "llm", "LLM", None, "out"],
         ["{\"framework\":\"LLM\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"3\",\"trace_id\":\"02\"}"] +
         [datetime(2024, 2, 5, 0, 6, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 5,0), "OK", "02", "3", "llm", "LLM", None, "out"], 
+        [datetime(2024, 2, 5, 0, 5, 0), "OK", "02", "3", "llm", "LLM", None, "out"],
         ["{\"framework\":\"LLM\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"4\",\"trace_id\":\"02\"}"] +
         [datetime(2024, 2, 5, 0, 8, 0), "[]", "[]", "name", None] +
-        [datetime(2024, 2, 5, 0, 7,0), "OK", "02", "4", "llm", "LLM", None, "out"], 
+        [datetime(2024, 2, 5, 0, 7, 0), "OK", "02", "4", "llm", "LLM", None, "out"],
         ["{\"framework\":\"LLM\",\"output\":\"out\",\"span_type\":\"llm\"}"] +
         ["{\"span_id\":\"5\",\"trace_id\":\"02\"}"] +
         [datetime(2024, 2, 5, 0, 12, 0), "[]", "[]", "name", "4"] +
-        [datetime(2024, 2, 5, 0, 11,0), "OK", "02", "5", "llm", "LLM", None, "out"],
+        [datetime(2024, 2, 5, 0, 11, 0), "OK", "02", "5", "llm", "LLM", None, "out"],
     ]
 
     @pytest.mark.parametrize(
