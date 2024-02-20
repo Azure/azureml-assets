14a4d63b584c0dc50b263f51c399801d8b214a17
Alan Poblette <alanpoblette@microsoft.com>
2024-02-19 20:23:55 -0800

    add trace aggregator UT

--- assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
+++ assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
@@ -12,7 +12,8 @@ import os
 import sys
 from datetime import datetime
 from src.model_data_collector_preprocessor.genai_run import (
-    _genai_uri_folder_to_preprocessed_spark_df
+    _genai_uri_folder_to_preprocessed_spark_df,
+    process_spans_into_aggregated_traces,
 )
 from src.model_data_collector_preprocessor.genai_preprocessor_df_schemas import (
     _get_preprocessed_span_logs_df_schema
@@ -174,6 +175,170 @@ class TestGenAISparkPreprocessor:
 
         for field in _get_preprocessed_span_logs_df_schema().fieldNames():
             assert field in actual_df.columns
+
+    _preprocessed_log_schema = StructType([
+        # TODO: The user_id and session_id may not be available in v1.
+        StructField('attributes', StringType(), False),
+        StructField('end_time', TimestampType(), False),
+        StructField('events', StringType(), False),
+        StructField('framework', StringType(), False),
+        StructField('input', StringType(), False),
+        StructField('links', StringType(), False),
+        StructField('name', StringType(), False),
+        StructField('output', StringType(), False),
+        StructField('parent_id', StringType(), True),
+        # StructField('session_id', StringType(), True),
+        StructField('span_id', StringType(), False),
+        StructField('span_type', StringType(), False),
+        StructField('start_time', TimestampType(), False),
+        StructField('status', StringType(), False),
+        StructField('trace_id', StringType(), False),
+        # StructField('user_id', StringType(), True),
+    ])
+
+    _span_log_data = [
+        ["{}", datetime(2024, 2, 5, 0, 8, 0), "[]", "FLOW", "in", "[]", "name",  "out", None] +
+        ["1", "llm", datetime(2024, 2, 5, 0, 1, 0), "OK", "01"],
+        ["{}", datetime(2024, 2, 5, 0, 5, 0), "[]", "RAG", "in", "[]", "name",  "out", "1"] +
+        ["2", "llm", datetime(2024, 2, 5, 0, 2, 0), "OK", "01"],
+        ["{}", datetime(2024, 2, 5, 0, 4, 0), "[]", "INTERNAL", "in", "[]", "name",  "out", "2"] +
+        ["3", "llm", datetime(2024, 2, 5, 0, 3, 0), "OK", "01"],
+        ["{}", datetime(2024, 2, 5, 0, 7, 0), "[]", "LLM", "in", "[]", "name",  "out", "1"] +
+        ["4", "llm", datetime(2024, 2, 5, 0, 6, 0), "OK", "01"]
+    ]
+
+    # pass data that might have extra information like context, resource, etc
+    _preprocessed_log_schema_extra = StructType([
+        # TODO: The user_id and session_id may not be available in v1.
+        StructField('attributes', StringType(), False),
+        StructField('context', StringType(), True),
+        StructField('c_resources', StringType(), True),
+        StructField('end_time', TimestampType(), False),
+        StructField('events', StringType(), False),
+        StructField('framework', StringType(), False),
+        StructField('input', StringType(), False),
+        StructField('links', StringType(), False),
+        StructField('name', StringType(), False),
+        StructField('output', StringType(), False),
+        StructField('parent_id', StringType(), True),
+        # StructField('session_id', StringType(), True),
+        StructField('span_id', StringType(), False),
+        StructField('span_type', StringType(), False),
+        StructField('start_time', TimestampType(), False),
+        StructField('status', StringType(), False),
+        StructField('trace_id', StringType(), False),
+        # StructField('user_id', StringType(), True),
+    ])
+
+    _span_log_data_extra = [
+        ["resource", "c", "{}", datetime(2024, 2, 5, 0, 8, 0), "[]", "FLOW", "in", "[]", "name",  "out", None] +
+        ["1", "llm", datetime(2024, 2, 5, 0, 1, 0), "OK", "01"],
+        ["resource", "c", "{}", datetime(2024, 2, 5, 0, 5, 0), "[]", "RAG", "in", "[]", "name",  "out", "1"] +
+        ["2", "llm", datetime(2024, 2, 5, 0, 2, 0), "OK", "01"],
+        ["resource", "c", "{}", datetime(2024, 2, 5, 0, 4, 0), "[]", "INTERNAL", "in", "[]", "name",  "out", "2"] +
+        ["3", "llm", datetime(2024, 2, 5, 0, 3, 0), "OK", "01"],
+        ["resource", "c", "{}", datetime(2024, 2, 5, 0, 7, 0), "[]", "LLM", "in", "[]", "name",  "out", "1"] +
+        ["4", "llm", datetime(2024, 2, 5, 0, 6, 0), "OK", "01"]
+    ]
+
+    _root_span_str_extra = '{"attributes": "resource", "context": "c", "c_resources": "{}", "end_time": "2024-02-' + \
+        '05T00:08:00", "events": "[]", "framework": "FLOW", "input": "in", "links": "[]", "name": "name", "output"' + \
+        ': "out", "parent_id": null, "span_id": "1", "span_type": "llm", "start_time": "2024-02-05T00:01:00", "sta' + \
+        'tus": "OK", "trace_id": "01", "children": ["{\\"attributes\\": \\"resource\\", \\"context\\": \\"c\\",' + \
+        ' \\"c_resources\\": \\"{}\\", \\"end_time\\": \\"2024-02-05T00:05:00\\", \\"events\\": \\"[]\\", \\"fram' + \
+        'ework\\": \\"RAG\\", \\"input\\": \\"in\\", \\"links\\": \\"[]\\", \\"name\\": \\"name\\", \\"output\\":' + \
+        ' \\"out\\", \\"parent_id\\": \\"1\\", \\"span_id\\": \\"2\\", \\"span_type\\": \\"llm\\", \\"start_tim' + \
+        'e\\": \\"2024-02-05T00:02:00\\", \\"status\\": \\"OK\\", \\"trace_id\\": \\"01\\", \\"children\\": [\\"' + \
+        '{\\\\\\"attributes\\\\\\": \\\\\\"resource\\\\\\", \\\\\\"context\\\\\\": \\\\\\"c\\\\\\", \\\\\\"c_resou' + \
+        'rces\\\\\\": \\\\\\"{}\\\\\\", \\\\\\"end_time\\\\\\": \\\\\\"2024-02-05T00:04:00\\\\\\", \\\\\\"event' + \
+        's\\\\\\": \\\\\\"[]\\\\\\", \\\\\\"framework\\\\\\": \\\\\\"INTERNAL\\\\\\", \\\\\\"input\\\\\\": \\\\\\"' + \
+        'in\\\\\\", \\\\\\"links\\\\\\": \\\\\\"[]\\\\\\", \\\\\\"name\\\\\\": \\\\\\"name\\\\\\", \\\\\\"outpu' + \
+        't\\\\\\": \\\\\\"out\\\\\\", \\\\\\"parent_id\\\\\\": \\\\\\"2\\\\\\", \\\\\\"span_id\\\\\\": \\\\\\"' + \
+        '3\\\\\\", \\\\\\"span_type\\\\\\": \\\\\\"llm\\\\\\", \\\\\\"start_time\\\\\\": \\\\\\"2024-02-05T00:03:0' + \
+        '0\\\\\\", \\\\\\"status\\\\\\": \\\\\\"OK\\\\\\", \\\\\\"trace_id\\\\\\": \\\\\\"01\\\\\\", \\\\\\"childr' + \
+        'en\\\\\\": []}\\"]}", "{\\"attributes\\": \\"resource\\", \\"context\\": \\"c\\", \\"c_resources\\": \\"' + \
+        '{}\\", \\"end_time\\": \\"2024-02-05T00:07:00\\", \\"events\\": \\"[]\\", \\"framework\\": \\"LLM\\", \\"' + \
+        'input\\": \\"in\\", \\"links\\": \\"[]\\", \\"name\\": \\"name\\", \\"output\\": \\"out\\", \\"parent_' + \
+        'id\\": \\"1\\", \\"span_id\\": \\"4\\", \\"span_type\\": \\"llm\\", \\"start_time\\": \\"2024-02-05T00:0' + \
+        '6:00\\", \\"status\\": \\"OK\\", \\"trace_id\\": \\"01\\", \\"children\\": []}"]}'
+
+    _root_span_str = '{"attributes": "{}", "end_time": "2024-02-05T00:08:00", "events": "[]", "framework": "FLOW",' + \
+        ' "input": "in", "links": "[]", "name": "name", "output": "out", "parent_id": null, "span_id":' + \
+        ' "1", "span_type": "llm", "start_time": "2024-02-05T00:01:00", "status": "OK", "trace_id": "0' + \
+        '1", "children": ["{\\"attributes\\": \\"{}\\", \\"end_time\\": \\"2024-02-05T00:05:00\\", \\"e' + \
+        'vents\\": \\"[]\\", \\"framework\\": \\"RAG\\", \\"input\\": \\"in\\", \\"links\\": \\"[]\\", \\"' + \
+        'name\\": \\"name\\", \\"output\\": \\"out\\", \\"parent_id\\": \\"1\\", \\"span_id\\": \\"2\\"' + \
+        ', \\"span_type\\": \\"llm\\", \\"start_time\\": \\"2024-02-05T00:02:00\\", \\"status\\": \\"OK\\"' + \
+        ', \\"trace_id\\": \\"01\\", \\"children\\": [\\"{\\\\\\"attributes\\\\\\": \\\\\\"{}\\\\\\"' + \
+        ', \\\\\\"end_time\\\\\\": \\\\\\"2024-02-05T00:04:00\\\\\\", \\\\\\"events\\\\\\": \\\\\\"[]\\\\\\"' + \
+        ', \\\\\\"framework\\\\\\": \\\\\\"INTERNAL\\\\\\", \\\\\\"input\\\\\\": \\\\\\"in\\\\\\", \\\\\\"li' + \
+        'nks\\\\\\": \\\\\\"[]\\\\\\", \\\\\\"name\\\\\\": \\\\\\"name\\\\\\", \\\\\\"output\\\\\\": \\\\\\"' + \
+        'out\\\\\\", \\\\\\"parent_id\\\\\\": \\\\\\"2\\\\\\", \\\\\\"span_id\\\\\\": \\\\\\"3\\\\\\", \\\\\\"' + \
+        'span_type\\\\\\": \\\\\\"llm\\\\\\", \\\\\\"start_time\\\\\\": \\\\\\"2024-02-05T00:03:00\\\\\\",' + \
+        ' \\\\\\"status\\\\\\": \\\\\\"OK\\\\\\", \\\\\\"trace_id\\\\\\": \\\\\\"01\\\\\\", \\\\\\"childre' + \
+        'n\\\\\\": []}\\"]}", "{\\"attributes\\": \\"{}\\", \\"end_time\\": \\"2024-02-05T00:07:00\\", \\"even' + \
+        'ts\\": \\"[]\\", \\"framework\\": \\"LLM\\", \\"input\\": \\"in\\", \\"links\\": \\"[]\\", \\"name\\"' + \
+        ': \\"name\\", \\"output\\": \\"out\\", \\"parent_id\\": \\"1\\", \\"span_id\\": \\"4\\", \\"span_typ' + \
+        'e\\": \\"llm\\", \\"start_time\\": \\"2024-02-05T00:06:00\\", \\"status\\": \\"OK\\", \\"trace_id\\":' + \
+        ' \\"01\\", \\"children\\": []}"]}'
+
+    _trace_log_data = [
+            ["01", None, None, datetime(2024, 2, 5, 0, 1, 0)] +
+            [datetime(2024, 2, 5, 0, 8, 0), "in", "out", _root_span_str],
+    ]
+
+    _trace_log_schema = StructType(
+        [
+            StructField("trace_id", StringType(), False),
+            StructField("user_id", StringType(), True),
+            StructField("session_id", StringType(), True),
+            StructField("start_time", TimestampType(), False),
+            StructField("end_time", TimestampType(), False),
+            StructField("input", StringType(), False),
+            StructField("output", StringType(), False),
+            StructField("root_span", StringType(), True),
+        ]
+    )
+
+    _trace_log_data_extra = [
+            ["01", None, None, datetime(2024, 2, 5, 0, 1, 0)] +
+            [datetime(2024, 2, 5, 0, 8, 0), "in", "out", _root_span_str_extra],
+    ]
+
+    @pytest.mark.parametrize(
+            "span_input_logs, span_input_schema, expected_trace_logs, expected_trace_schema",
+            [
+                ([], _preprocessed_log_schema, [], _trace_log_schema),
+                (_span_log_data, _preprocessed_log_schema, _trace_log_data, _trace_log_schema),
+                (
+                    _span_log_data_extra, _preprocessed_log_schema_extra,
+                    _trace_log_data_extra, _trace_log_schema),
+            ]
+    )
+    def test_trace_aggregator(
+            self, genai_preprocessor_test_setup,
+            span_input_logs, span_input_schema, expected_trace_logs, expected_trace_schema):
+        """Test scenario where spans has real data."""
+        spark = self._init_spark()
+        # infer schema only when we have data.
+        processed_spans_df = spark.createDataFrame(span_input_logs, span_input_schema)
+        expected_traces_df = spark.createDataFrame(expected_trace_logs, expected_trace_schema)
+
+        print("processed logs:")
+        processed_spans_df.show()
+        processed_spans_df.printSchema()
+
+        print("expected trace logs:")
+        expected_traces_df.show()
+        expected_traces_df.printSchema()
+
+        actual_trace_df = process_spans_into_aggregated_traces(processed_spans_df)
+
+        print("actual trace logs:")
+        actual_trace_df.show()
+        actual_trace_df.printSchema()
+
+        assert_spark_dataframe_equal(actual_trace_df, expected_traces_df)
 
 
 def assert_spark_dataframe_equal(df1, df2):
