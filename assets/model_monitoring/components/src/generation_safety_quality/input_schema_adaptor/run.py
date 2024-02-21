20101e339847ccd5ac9324e0afe34b5d7e4c6f28
Alan Poblette <alanpoblette@microsoft.com>
2024-02-21 11:03:00 -0800

    add barebones component

---
+++ assets/model_monitoring/components/src/generation_safety_quality/input_schema_adaptor/run.py
@@ -0,0 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Entry script for GSQ Input Schema Adaptor Spark Component."""
+
+# place holder file, will add content later
