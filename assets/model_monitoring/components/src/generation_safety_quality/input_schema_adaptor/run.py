dd484a3981488d3c08b48e61422a67d2691c9e07
Alan Poblette <alanpoblette@microsoft.com>
2024-02-21 11:23:05 -0800

    add simple component logic

--- assets/model_monitoring/components/src/generation_safety_quality/input_schema_adaptor/run.py
+++ assets/model_monitoring/components/src/generation_safety_quality/input_schema_adaptor/run.py
@@ -3,4 +3,27 @@
 
 """Entry script for GSQ Input Schema Adaptor Spark Component."""
 
-# place holder file, will add content later
+import argparse
+
+from shared_utilities.io_utils import (
+    save_spark_df_as_mltable,
+    try_read_mltable_in_spark_with_error,
+)
+
+
+def run():
+    """Adapt the production dataset schema to match GSQ input schema if necessary."""
+    # Parse argument
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--production_dataset", type=str, required=True)
+    parser.add_argument("--adapted_production_data", type=str, required=True)
+
+    args = parser.parse_args()
+
+    production_data_df = try_read_mltable_in_spark_with_error(args.production_dataset, "production_dataset")
+
+    save_spark_df_as_mltable(production_data_df, args.adapted_production_data)
+
+
+if __name__ == "__main__":
+    run()
