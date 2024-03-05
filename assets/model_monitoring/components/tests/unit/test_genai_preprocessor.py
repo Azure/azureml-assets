5e3468d056981e1a20f3c5ce6c3b8072897f4b0e
Alan Poblette <alanpoblette@microsoft.com>
2024-03-05 09:54:12 -0800

    Revert "expect null values"
    This reverts commit 2e040f37a8eb2032f0bcb837b7f44602f9bd5969.

--- assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
+++ assets/model_monitoring/components/tests/unit/test_genai_preprocessor.py
@@ -367,13 +367,13 @@ class TestGenAISparkPreprocessor:
 
     _trace_log_schema = StructType(
         [
-            StructField("trace_id", StringType(), True),
+            StructField("trace_id", StringType(), False),
             StructField("user_id", StringType(), True),
             StructField("session_id", StringType(), True),
-            StructField("start_time", TimestampType(), True),
-            StructField("end_time", TimestampType(), True),
-            StructField("input", StringType(), True),
-            StructField("output", StringType(), True),
+            StructField("start_time", TimestampType(), False),
+            StructField("end_time", TimestampType(), False),
+            StructField("input", StringType(), False),
+            StructField("output", StringType(), False),
             StructField("root_span", StringType(), True),
         ]
     )
