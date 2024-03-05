8bcb856332a0c84ff2574314ba599135b8ab765d
Alan Poblette <alanpoblette@microsoft.com>
2024-03-05 10:12:28 -0800

    use flatmap instead and add start_time filter

--- assets/model_monitoring/components/src/model_data_collector_preprocessor/trace_aggregator.py
+++ assets/model_monitoring/components/src/model_data_collector_preprocessor/trace_aggregator.py
@@ -4,6 +4,7 @@
 
 
 from pyspark.sql import DataFrame
+from dateutil import parser
 from pyspark.sql.functions import collect_list, struct
 from shared_utilities.span_tree_utils import SpanTree, SpanTreeNode
 from model_data_collector_preprocessor.genai_preprocessor_df_schemas import (
@@ -19,18 +20,19 @@ def _aggregate_span_logs_to_trace_logs(grouped_row):
     span_list = [SpanTreeNode(row) for row in grouped_row.span_rows]
     tree = SpanTree(span_list)
     if tree.root_span is None:
-        output_dict = {}
+        return []
     else:
         output_dict = tree.root_span.to_dict(datetime_to_str=False)
         output_dict['root_span'] = tree.to_json_str()
-    return tuple(output_dict.get(fieldName, None) for fieldName in output_schema.fieldNames())
+        return [output_dict.get(fieldName, None) for fieldName in output_schema.fieldNames()]
 
 
-def process_spans_into_aggregated_traces(span_logs: DataFrame, require_trace_data: bool) -> DataFrame:
+def process_spans_into_aggregated_traces(span_logs: DataFrame, require_trace_data: bool, data_window_start: str, data_window_end: str) -> DataFrame:
     """Group span logs into aggregated trace logs."""
     spark = init_spark()
     output_trace_schema = _get_aggregated_trace_log_spark_df_schema()
 
+    # TODO: figure out optional output behavior and change to that.
     if not require_trace_data:
         print("Skip processing of spans into aggregated traces.")
         return spark.createDataFrame(data=[], schema=output_trace_schema)
@@ -43,14 +45,15 @@ def process_spans_into_aggregated_traces(span_logs: DataFrame, require_trace_dat
         ).alias('span_rows')
     )
 
-    # see if we can return a list of spans and return empty list instead of null rows.
     all_aggregated_traces = grouped_spans_df \
         .rdd \
-        .map(_aggregate_span_logs_to_trace_logs) \
+        .flatMap(_aggregate_span_logs_to_trace_logs) \
         .toDF(output_trace_schema)
 
-    # remove any null root_span rows
-    all_aggregated_traces = all_aggregated_traces.dropna(how="all")
+    # TODO: add end_time parsing when necessary
+    data_window_start_as_datetime = parser.parse(data_window_start)
+    all_aggregated_traces = all_aggregated_traces.filter(
+        all_aggregated_traces.start_time >= data_window_start_as_datetime)
 
     print("Aggregated Trace DF:")
     all_aggregated_traces.show()
