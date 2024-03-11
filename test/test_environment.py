5b55d7c2e51a6a90a8fe3f4dcbbd2a2bbef1285b
sarthaks95 <13473111+sarthaks95@users.noreply.github.com>
2024-03-11 19:31:35 +0530

    Fix for pinning package version with extras (#2476)
    * Fix for pinning package version with extras
    
    * Adding unit tests, Updating CHANGELOG, setup.py
    
    * Fixing gate
    
    ---------
    
    Co-authored-by: Sarthak Singhal <sarsinghal@microsoft.com>

---
+++ test/test_environment.py
@@ -0,0 +1,27 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Test environment folder scripts."""
+
+from azureml.assets.environment import pin_packages
+from azureml.assets.environment.pin_package_versions import (
+    create_package_finder,
+    get_latest_package_version,
+    PYPI_URL,
+)
+
+package_finder = create_package_finder([PYPI_URL])
+
+
+def test_pin_packages_no_extras():
+    """Test pin packages without extras."""
+    contents = "azureml-core=={{latest-pypi-version}}"
+    expected = f"azureml-core=={get_latest_package_version('azureml-core', package_finder)}"
+    assert pin_packages(contents) == expected
+
+
+def test_pin_packages_with_extras():
+    """Test pin packages with extras."""
+    contents = "azureml-metrics[all]=={{latest-pypi-version}}"
+    expected = f"azureml-metrics[all]=={get_latest_package_version('azureml-metrics', package_finder)}"
+    assert pin_packages(contents) == expected
