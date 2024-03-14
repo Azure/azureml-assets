8699da0f0fad14483271adbc90a2be9cf791e0f1
Sunjoli Aggarwal <sunjoli.aggarwal@gmail.com>
2024-03-14 12:08:51 -0700

    Batch Score 03/13/24 Component Release (#2498)
    * Release changes
    
    * Update
    
    * Fix linter
    
    * Add back metadata json
    
    * Fix metadata version
    
    ---------
    
    Co-authored-by: Sunjoli Aggarwal <sunaggar@microsoft.com>

--- assets/batch_score/components/driver/tests/unit/mir/test_mir_scoring_client.py
+++ assets/batch_score/components/driver/tests/unit/mir/test_mir_scoring_client.py
@@ -4,7 +4,7 @@
 """This file contains the unit tests for MIR scoring client."""
 
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 from src.batch_score.common.configuration.configuration import Configuration
 from src.batch_score.mir.scoring.mir_http_response_handler import MirHttpResponseHandler
@@ -28,52 +28,13 @@ url_2 = "https://scoring_url_2"
 def test_init(scoring_url, configuration, expected_scoring_url):
     """Test init."""
     # Arrange and Act
-    scoring_client = _get_scoring_client(
+    scoring_client = MirScoringClient(
+        header_provider=MagicMock(),
         configuration=configuration,
+        tally_handler=MagicMock(),
         scoring_url=scoring_url)
 
     # Assert
     assert scoring_client is not None
-    assert scoring_client._generic_scoring_client is not None
-    assert type(scoring_client._generic_scoring_client._http_response_handler) is MirHttpResponseHandler
-    assert scoring_client._generic_scoring_client._scoring_url == expected_scoring_url
-
-
-@pytest.mark.asyncio
-async def test_score_once():
-    """Test score_once."""
-    # Arrange
-    scoring_client = _get_scoring_client()
-    with patch.object(scoring_client._generic_scoring_client, "score") as mock_score_once:
-        mock_score_once.return_value = MagicMock()
-
-        # Act
-        result = await scoring_client.score_once(
-            session=MagicMock(),
-            scoring_request=MagicMock(),
-            timeout=MagicMock(),
-            worker_id="1")
-
-        # Assert
-        assert mock_score_once.assert_called_once
-        assert result == mock_score_once.return_value
-
-
-def test_validate_auth():
-    """Test validate_auth."""
-    # Arrange
-    scoring_client = _get_scoring_client()
-    with patch.object(scoring_client._generic_scoring_client, "validate_auth") as mock_validate_auth:
-        # Act
-        scoring_client.validate_auth()
-
-        # Assert
-        assert mock_validate_auth.assert_called_once
-
-
-def _get_scoring_client(configuration=None, scoring_url=None):
-    return MirScoringClient(
-        header_provider=MagicMock(),
-        configuration=configuration or Configuration(),
-        tally_handler=MagicMock(),
-        scoring_url=scoring_url)
+    assert type(scoring_client._http_response_handler) is MirHttpResponseHandler
+    assert scoring_client._scoring_url == expected_scoring_url
