4fec430546c7be5fce84d82a9a4f737f9a5169a1
Chuyuan Qu <chuyuanqu@microsoft.com>
2024-03-19 13:55:18 -0700

    resolving comments

--- assets/Langchain/environments/general-langchain-app-deployment/context/entrypoint.sh
+++ assets/Langchain/environments/general-langchain-app-deployment/context/entrypoint.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-# Print commands and their arguments as they are executed
 set -x
 
 cd $AZUREML_MODEL_DIR
 
-# Count the number of directories within the specified directory
 dir_count=$(find "$AZUREML_MODEL_DIR" -mindepth 1 -maxdepth 1 -type d | wc -l)
 
 if [ "$dir_count" -eq 1 ]; then
-    # Get the name of the directory
     app_folder=$(find "$AZUREML_MODEL_DIR" -mindepth 1 -maxdepth 1 -type d)
-    # Change to that directory
     cd "$app_folder"
     echo "Changed directory to $(pwd)"
 fi
 ls
 
-# Install any dependencies (if not already installed)
 poetry install --no-interaction --no-ansi
 
-# Start the Uvicorn server
 exec uvicorn app.server:app --host 0.0.0.0 --port 8080
