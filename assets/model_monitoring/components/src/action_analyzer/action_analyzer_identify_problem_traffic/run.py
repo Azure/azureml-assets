633acd589b6849ab43a9f21452e3c13ce1bb7eb1
Yuan Cheng <yuacheng@microsoft.com>
2024-03-19 16:46:55 -0700

    syntax

--- assets/model_monitoring/components/src/action_analyzer/action_analyzer_identify_problem_traffic/run.py
+++ assets/model_monitoring/components/src/action_analyzer/action_analyzer_identify_problem_traffic/run.py
@@ -317,7 +317,7 @@ def run():
 
     print("Violated metrics found: ", violated_metrics)
 
-    signal_scored_data_df = try_read_mltable_in_spark("azureml://subscriptions/1aefdc5e-3a7c-4d71-a9f9-f5d3b03be19a/resourcegroups/yuachengtestrg/workspaces/ai-proj-eastus/datastores/workspaceblobstore/paths/azureml/5b3c73e9-8e67-47e2-a46e-5087b2288110/evaluation/", "signal_scored_data")
+    signal_scored_data_df = try_read_mltable_in_spark(args.signal_scored_data, "signal_scored_data")
     print("gsq output df")
     signal_scored_data_df.show()
 
