3b30bed8e57cfe7af727c31e1efc2ca2a88fa08c
Chirag <chiragbhatt@microsoft.com>
2024-03-21 10:19:29 +0530

    updated command conponents

--- assets/training/aoai/proxy_components/src/common/azure_openai_client_manager.py
+++ assets/training/aoai/proxy_components/src/common/azure_openai_client_manager.py
@@ -3,7 +3,6 @@
 
 """Azure OpenAI client manager."""
 
-from azureml.core import Run
 from azure.identity import ManagedIdentityCredential
 from azure.mgmt.cognitiveservices import CognitiveServicesManagementClient
 from azure.mgmt.cognitiveservices.models import ApiKeys
@@ -20,6 +19,7 @@ class AzureOpenAIClientManager:
     """Class for **authentication** related information used for the run."""
 
     ENV_CLIENT_ID_KEY = "DEFAULT_IDENTITY_CLIENT_ID"
+    MLFLOW_TRACKING_URI = "MLFLOW_TRACKING_URI"
     api_version = "2023-12-01-preview"
 
     def __init__(self, endpoint_name, endpoint_resource_group: Optional[str], endpoint_subscription: Optional[str]):
@@ -27,17 +27,33 @@ class AzureOpenAIClientManager:
         self.endpoint_name = endpoint_name
         self.endpoint_resource_group = endpoint_resource_group
         self.endpoint_subscription = endpoint_subscription
-        self.run_context = Run.get_context()
+        workspace_subscription, workspace_resource_group = self._get_workspace_subscription_id_resource_group()
+
         if endpoint_subscription is None:
             logger.info("AOAI resource subscription id is empty, will default to workspace subscription")
-            self.endpoint_subscription = self.run_context.experiment.workspace.subscription_id
+            self.endpoint_subscription = workspace_subscription
+            if self.endpoint_subscription is None:
+                raise Exception("endpoint_subscription is None")
+
         if endpoint_resource_group is None:
             logger.info("AOAI resource resource group is empty, will default to workspace resource group")
-            self.endpoint_resource_group = self.run_context.experiment.workspace.resource_group
-
+            self.endpoint_resource_group = workspace_resource_group
+            if self.endpoint_resource_group is None:
+                raise Exception("endpoint_resource_group is None")
+        
     def _get_client_id(self) -> str:
         """Get the client id."""
         return os.environ.get(AzureOpenAIClientManager.ENV_CLIENT_ID_KEY, None)
+    
+    def _get_workspace_subscription_id_resource_group(self) -> str:
+        """Get current subscription id."""
+        uri = os.environ.get(AzureOpenAIClientManager.MLFLOW_TRACKING_URI, None)
+        if uri is None:
+            return None
+        uri_segments = uri.split("/")
+        subscription_id = uri_segments[uri_segments.index("subscriptions") + 1]
+        resource_group = uri_segments[uri_segments.index("resourceGroups") + 1]
+        return subscription_id, resource_group
 
     def _get_credential(self) -> ManagedIdentityCredential:
         """Get the credential."""
