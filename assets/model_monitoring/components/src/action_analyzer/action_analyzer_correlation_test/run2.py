3762d47a8cbe6635125057345a6e377b75b044ed
Yuan Cheng <yuacheng@microsoft.com>
2024-03-22 18:17:02 -0700

    changes

--- assets/model_monitoring/components/src/action_analyzer/action_analyzer_correlation_test/run2.py
+++ assets/model_monitoring/components/src/action_analyzer/action_analyzer_correlation_test/run2.py
@@ -11,7 +11,7 @@ from pyspark.sql.types import (
     StringType,
     FloatType
 )
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, udf
 from shared_utilities.io_utils import (
     try_read_mltable_in_spark,
     save_spark_df_as_mltable,
@@ -28,7 +28,10 @@ from shared_utilities.constants import (
     GOOD_GROUP_NAME,
     BAD_GROUP_NAME,
     ACTION_METRICS_COLUMN,
-    QUERY_INTENTION_COLUMN
+    QUERY_INTENTION_COLUMN,
+    TRACE_ID_LIST_COLUMN,
+    GROUP_COLUMN,
+    PROPERTIES_COLUMN
 )
 import statistics
 from scipy.stats import mannwhitneyu
@@ -46,7 +49,7 @@ def get_output_schema() -> StructType:
     schema = StructType(
         [
             StructField(TTEST_GROUP_ID_COLUMN, StringType(), True),
-            StructField(GROUP_COLUMN, StringType(), True),
+            StructField(TRACE_ID_LIST_COLUMN, StringType(), True),
             StructField(QUERY_INTENTION_COLUMN, StringType(), True),
             StructField(CONFIDENCE_SCORE_COLUMN, FloatType(), True)
         ]
@@ -69,13 +72,23 @@ def get_unique_values_by_column(df, column):
     return unique_values
 
 
-def generate_actions(df, ttest_groups, violated_metrics):
+@udf(returnType=StringType())
+def get_debugging_column(action_metrics, properties):
+    """Get debugging information for t-test"""
+    properties_dict = json.loads(properties)
+    return f"{action_metrics} - {properties_dict[PROMPT_COLUMN]}"
+
+
+def generate_actions(df, violated_metrics):
     """Perform correlation test to generate action data."""
+    ttest_groups = get_unique_values_by_column(df, TTEST_GROUP_ID_COLUMN)
+    print("===All ttest groups===")
+    print(ttest_groups)
     actions_data = []
     for ttest_group_id in ttest_groups:
         df_filtered = df.filter(col(TTEST_GROUP_ID_COLUMN) == ttest_group_id)
         for metrics in violated_metrics:
-            print("Metrics: ", metrics)
+            print("\nMetrics: ", metrics)
             good_group_name = GOOD_GROUP_NAME.replace("{metrics}", metrics)
             bad_group_name = BAD_GROUP_NAME.replace("{metrics}", metrics)
             good_group_df = df_filtered.filter(col(GROUP_COLUMN) == good_group_name)
@@ -98,27 +111,31 @@ def perform_correlation_test(good_group_df,
                              actions_data,
                              query_intention):
     """Pefrom correlation test for two groups of data."""
-        print("good answer questions: ")
-        print(good_group_df.select(ACTION_METRICS_COLUMN, PROMPT_COLUMN).collect())
-        print("bad answer questions: ")
-        print(bad_group_df.select(ACTION_METRICS_COLUMN, PROMPT_COLUMN).collect())
+    print("\nCorrelation test for topic: ", query_intention)
+    print("good queries: ")
+    good_group_df = good_group_df.withColumn("Debugging", get_debugging_column(col(ACTION_METRICS_COLUMN), col(PROPERTIES_COLUMN)))
+    print("\n".join(good_group_df.select("Debugging").rdd.flatMap(lambda x: x).collect()))
+    print("bad queries: ")
+    bad_group_df = bad_group_df.withColumn("Debugging", get_debugging_column(col(ACTION_METRICS_COLUMN), col(PROPERTIES_COLUMN)))
+    print("\n".join(bad_group_df.select("Debugging").rdd.flatMap(lambda x: x).collect()))
 
-        good_answer_scores = good_group_df.select(ACTION_METRICS_COLUMN).rdd.flatMap(lambda x: x).collect()
-        bad_answer_scores = bad_group_df.select(ACTION_METRICS_COLUMN).rdd.flatMap(lambda x: x).collect()
+    good_answer_scores = good_group_df.select(ACTION_METRICS_COLUMN).rdd.flatMap(lambda x: x).collect()
+    bad_answer_scores = bad_group_df.select(ACTION_METRICS_COLUMN).rdd.flatMap(lambda x: x).collect()
 
-        t_stat, p_value = perform_ttest(good_answer_scores, bad_answer_scores)
-        bad_mean = statistics.mean(bad_answer_scores)
-        print("Mean value of bad group: ", bad_mean)
-        if t_stat > 0 and p_value < P_VALUE_THRESHOLD and bad_mean < 3.0:
-            print("Generating action for group: ", query_intention)
-            # entry: [ttest_group_id, group_name, query_intention, confidence_score]
-            entry = [
-                ttest_group_id,
-                bad_group_name,
-                query_intention,
-                float(1.0 - p_value)
-            ]
-            actions_data.append(entry)
+    t_stat, p_value = perform_ttest(good_answer_scores, bad_answer_scores)
+    bad_mean = statistics.mean(bad_answer_scores)
+    print("Mean value of bad group: ", bad_mean)
+    if t_stat > 0 and p_value < P_VALUE_THRESHOLD and bad_mean < 3.0:
+        print("Generating action for group: ", query_intention)
+        # entry: [ttest_group_id, trace_list, query_intention, confidence_score]
+        trace_list = ",".join(bad_group_df.select(TRACE_ID_COLUMN).rdd.flatMap(lambda x: x).collect())
+        entry = [
+            ttest_group_id,
+            trace_list,
+            query_intention,
+            float(1.0 - p_value)
+        ]
+        actions_data.append(entry)
     return actions_data
 
 
@@ -164,18 +181,13 @@ def run():
     # filter out invalid metrics score
     data_with_action_metric_score_df = data_with_action_metric_score_df.filter(col(ACTION_METRICS_COLUMN) > INVALID_METRICS_SCORE)  # noqa: E501
 
-    ttest_groups = get_unique_values_by_column(data_with_action_metric_score_df, TTEST_GROUP_ID_COLUMN)
-    
-    print("===All ttest groups===")
-    print(ttest_groups)
-
     # get actions
-    action_rows = generate_actions(data_with_action_metric_score_df, ttest_groups, violated_metrics)
+    action_rows = generate_actions(data_with_action_metric_score_df, violated_metrics)
 
     # Output Schema:
-    # +--------------+-----+---------------+----------------+
-    # |ttest_group_id|group|query_intention|confidence_score|
-    # +--------------+-----+---------------+----------------+
+    # +--------------+----------+---------------+----------------+
+    # |ttest_group_id|trace_list|query_intention|confidence_score|
+    # +--------------+----------+---------------+----------------+
     save_action_data(action_rows, args.action_data)
 
 
--- assets/model_monitoring/components/src/shared_utilities/constants.py
+++ assets/model_monitoring/components/src/shared_utilities/constants.py
@@ -130,23 +130,27 @@ PROMPT_COLUMN = "prompt"
 COMPLETION_COLUMN = "completion"
 CONTEXT_COLUMN = "context"
 TRACE_ID_COLUMN = "trace_id"
-SPAN_ID_COLUMN = "span_id"
-ROOT_QUESTION_COLUMN = "root_question"
-TOPIC_LIST_COLUMN = "topic_list"
-GROUP_LIST_COLUMN = "group_list"
+ROOT_PROMPT_COLUMN = "root_prompt"
 VIOLATED_METRICS_COLUMN = "violated_metrics"
 INDEX_CONTENT_COLUMN = "index_content"
 INDEX_SCORE_COLUMN = "index_score"
-INDEX_SCORE_LLM_COLUMN = "index_score_llm"
 INDEX_ID_COLUMN = "index_id"
 ROOT_SPAN_COLUMN = "root_span"
-BAD_GROUP_COLUMN = "bad_group"
-GOOD_GROUP_COLUMN = "good_group"
+GOOD_GROUP_NAME = "{metrics}_good"
+BAD_GROUP_NAME = "{metrics}_bad"
 CONFIDENCE_SCORE_COLUMN = "confidence_score"
 ACTION_ID_COLUMN = "action_id"
 RETRIEVAL_QUERY_TYPE_COLUMN = "retrieval_query_type"
 RETRIEVAL_TOP_K_COLUMN = "retrieval_top_k"
 DEFAULT_TOPIC_NAME = "disparate"
+GROUP_COLUMN = "query_group"
+QUERY_INTENTION_COLUMN = "query_intention"
+ACTION_METRICS_COLUMN = "action_metrics"
+PROPERTIES_COLUMN = "action_analyzer_properties"
+TTEST_GROUP_ID_COLUMN = "ttest_group_id"
+TRACE_ID_LIST_COLUMN = "trace_id_list"
+ACTION_QUERY_INTENTION_COLUMN = "action_query_intention"
+ACTION_ANALYZER_ACTION_TAG = "momo_action_analyzer_has_action"
 
 GSQ_METRICS_LIST = ["Fluency", "Coherence", "Groundedness", "Relevance", "Similarity"]
 GOOD_METRICS_VALUE = 5
@@ -161,7 +165,7 @@ MEAN_THRESHOLD = 3
 INDEX_ACTION_TYPE = "Index Action"
 ACTION_DESCRIPTION = "The application's response quality is low due to suboptimal index retrieval. Please update the index with ID '{index_id}' to address this issue."  # noqa
 MAX_SAMPLE_SIZE = 20
-DEFAULT_RETRIEVAL_SCORE = 0
+INVALID_METRICS_SCORE = 0
 
 
 # util
