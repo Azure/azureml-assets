00fcec242e4c83ff800f26d9973d07a80191ef19
Alan Poblette <alanpoblette@microsoft.com>
2024-03-25 16:25:48 -0700

    don't fail for Null values

--- assets/model_monitoring/components/src/model_monitor_output_metrics/entities/signals/data_quality_signal.py
+++ assets/model_monitoring/components/src/model_monitor_output_metrics/entities/signals/data_quality_signal.py
@@ -111,8 +111,10 @@ class DataQualitySignal(Signal):
             )
             run_metric = {
                 "runId": run_id,
-                "targetValue": float(metric["baseline_metric_value"]),
-                "baselineValue": float(metric["target_metric_value"]),
+                "targetValue": None if metric["baseline_metric_value"] is None\
+                    else float(metric["baseline_metric_value"]),
+                "baselineValue": None if metric["target_metric_value"] is None\
+                    else float(metric["target_metric_value"]),
             }
             run_metric = add_value_if_present(
                 metric, "threshold_value", run_metric, "threshold"
