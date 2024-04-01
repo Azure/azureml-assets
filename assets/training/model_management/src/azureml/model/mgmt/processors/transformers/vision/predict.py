bc027e0699a52e2aef8868d84e9590dca6395e8c
rdondera-microsoft <98922913+rdondera-microsoft@users.noreply.github.com>
2024-04-01 16:29:46 -0700

    Do not save images to disk before inference (#2539)
    * Do not save images to disk in converter for CLIP for open-vocabulary classification.
    
    * Fix CLIP embeddings.
    
    * Fix BLIP.
    
    * Fix classification models.
    
    * Fix detection models.
    
    * Minor cleanup and todo for using dataset as input to model inference.
    
    * Fix blank line.

--- assets/training/model_management/src/azureml/model/mgmt/processors/transformers/vision/predict.py
+++ assets/training/model_management/src/azureml/model/mgmt/processors/transformers/vision/predict.py
@@ -3,26 +3,22 @@
 
 """Huggingface predict file for Image classification MLflow model."""
 
+import io
 import logging
 
 import pandas as pd
-import tempfile
-
 import torch
 
-from datasets import load_dataset
-from transformers import (
-    AutoImageProcessor,
-    AutoModelForImageClassification,
-    TrainingArguments,
-    Trainer,
-)
-from typing import List, Dict, Any, Tuple
+from typing import Any, Dict, Generator, List
+
+from datasets import Dataset
+from PIL import Image
+from transformers import TrainingArguments, Trainer
 
 try:
     # Use try/except since vision_utils is added as part of model export and not available when initializing
     # model wrapper for save_model().
-    from vision_utils import create_temp_file, process_image_pandas_series
+    from vision_utils import process_image
 except ImportError:
     pass
 
@@ -70,7 +66,7 @@ def predict(input_data: pd.DataFrame, task, model, tokenizer, **kwargs) -> pd.Da
     image is in base64 String format.
     :param task: Task type of the model.
     :type task: HFTaskLiterals
-    :param tokenizer: Preprocessing configuration loader.
+    :param tokenizer: Image preprocessing object.
     :type tokenizer: transformers.AutoImageProcessor
     :param model: Pytorch model weights.
     :type model: transformers.AutoModelForImageClassification
@@ -78,71 +74,10 @@ def predict(input_data: pd.DataFrame, task, model, tokenizer, **kwargs) -> pd.Da
     :rtype: Pandas DataFrame with columns ['filename', 'probs', 'labels'] for classification and
     ['filename', 'boxes'] for object detection, instance segmentation
     """
-    # Decode the base64 image column
-    decoded_images = input_data.loc[:, [MLflowSchemaLiterals.INPUT_COLUMN_IMAGE]].apply(
-        axis=1, func=process_image_pandas_series
-    )
-
-    # arguments for Trainer
-    test_args = TrainingArguments(
-        output_dir=".",
-        do_train=False,
-        do_predict=True,
-        per_device_eval_batch_size=kwargs.get(MLflowLiterals.BATCH_SIZE_KEY, 1),
-        dataloader_drop_last=False,
-        remove_unused_columns=False,
-    )
-
-    # To Do: change image height and width based on kwargs.
-
-    with tempfile.TemporaryDirectory() as tmp_output_dir:
-        image_path_list = decoded_images.iloc[:, 0].map(lambda row: create_temp_file(row, tmp_output_dir)[0]).tolist()
-        conf_scores = run_inference_batch(
-            test_args,
-            image_processor=tokenizer,
-            model=model,
-            image_path_list=image_path_list,
-            task_type=task,
-        )
-
-    df_result = pd.DataFrame(
-        columns=[
-            MLflowSchemaLiterals.OUTPUT_COLUMN_PROBS,
-            MLflowSchemaLiterals.OUTPUT_COLUMN_LABELS,
-        ]
-    )
-
-    labels = kwargs.get(MLflowLiterals.TRAIN_LABEL_LIST)
-    labels = [labels.tolist()] * len(conf_scores)
-    df_result[MLflowLiterals.PROBS], df_result[MLflowLiterals.LABELS] = (
-        conf_scores.tolist(),
-        labels,
-    )
-    return df_result
-
-
-def run_inference_batch(
-    test_args: TrainingArguments,
-    image_processor: AutoImageProcessor,
-    model: AutoModelForImageClassification,
-    image_path_list: List,
-    task_type: HFTaskLiterals,
-) -> Tuple[torch.tensor]:
-    """Perform inference on batch of input images.
-
-    :param test_args: Training arguments path.
-    :type test_args: transformers.TrainingArguments
-    :param image_processor: Preprocessing configuration loader.
-    :type image_processor: transformers.AutoImageProcessor
-    :param model: Pytorch model weights.
-    :type model: transformers.AutoModelForImageClassification
-    :param image_path_list: list of image paths for inferencing.
-    :type image_path_list: List
-    :param task_type: Task type of the model.
-    :type task_type: HFTaskLiterals
-    :return: Predicted probabilities
-    :rtype: Tuple of torch.tensor
-    """
+    # Make generator that reads all the input images and collator that produces B*C*H*W tensors.
+    def image_generator_fn() -> Generator[Dict[str, Any], None, None]:
+        for image in input_data[HFMiscellaneousConstants.DEFAULT_IMAGE_KEY]:
+            yield {"image": Image.open(io.BytesIO(process_image(image)))}
 
     def collate_fn(examples: List[Dict[str, Any]]) -> Dict[str, torch.tensor]:
         """Collator function for eval dataset.
@@ -152,23 +87,43 @@ def run_inference_batch(
         :return: Dictionary of pixel values in torch tensor format.
         :rtype: Dict
         """
-        images = [data[HFMiscellaneousConstants.DEFAULT_IMAGE_KEY] for data in examples]
-        return image_processor(images, return_tensors="pt")
+        images = [data["image"] for data in examples]
+        return tokenizer(images, return_tensors="pt")
 
-    inference_dataset = load_dataset("imagefolder", data_files={"val": image_path_list})
-    inference_dataset = inference_dataset["val"]
+    # TODO: use in-memory dataset throughout the code (eg instead of list of images).
 
-    # Initialize the trainer
+    # TODO: change image height and width based on kwargs.
+    # Do inference (output: logits).
+    inference_dataset = Dataset.from_generator(image_generator_fn)
     trainer = Trainer(
         model=model,
-        args=test_args,
-        tokenizer=image_processor,
+        args=TrainingArguments(
+            output_dir=".",
+            do_train=False,
+            do_predict=True,
+            per_device_eval_batch_size=kwargs.get(MLflowLiterals.BATCH_SIZE_KEY, 1),
+            dataloader_drop_last=False,
+            remove_unused_columns=False,
+        ),
         data_collator=collate_fn,
     )
     results = trainer.predict(inference_dataset)
-    if task_type == HFTaskLiterals.MULTI_CLASS_IMAGE_CLASSIFICATION:
+
+    # Calculate probabilities from logits.
+    if task == HFTaskLiterals.MULTI_CLASS_IMAGE_CLASSIFICATION:
         probs = torch.nn.functional.softmax(torch.from_numpy(results.predictions), dim=1).numpy()
-    elif task_type == HFTaskLiterals.MULTI_LABEL_CLASSIFICATION:
+    elif task == HFTaskLiterals.MULTI_LABEL_CLASSIFICATION:
         sigmoid = torch.nn.Sigmoid()
         probs = sigmoid(torch.from_numpy(results.predictions)).numpy()
-    return probs
+
+    # Convert to Pandas dataframe and return.
+    df_result = pd.DataFrame(
+        columns=[
+            MLflowSchemaLiterals.OUTPUT_COLUMN_PROBS,
+            MLflowSchemaLiterals.OUTPUT_COLUMN_LABELS,
+        ]
+    )
+    df_result[MLflowLiterals.PROBS] = probs.tolist()
+    df_result[MLflowLiterals.LABELS] = [kwargs.get(MLflowLiterals.TRAIN_LABEL_LIST).tolist()] * len(probs)
+
+    return df_result
