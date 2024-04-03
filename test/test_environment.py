ddfe982d3e298ab6dd911404eb9e30ab2c5fa659
nvijayrania <nvijayrania@microsoft.com>
2024-04-03 13:56:28 +0530

    Exclude yanked versions

--- test/test_environment.py
+++ test/test_environment.py
@@ -3,6 +3,7 @@
 
 """Test environment folder scripts."""
 
+import requests
 from azureml.assets.environment import pin_packages
 from azureml.assets.environment.pin_package_versions import (
     create_package_finder,
@@ -25,3 +26,18 @@ def test_pin_packages_with_extras():
     contents = "azureml-metrics[all]=={{latest-pypi-version}}"
     expected = f"azureml-metrics[all]=={get_latest_package_version('azureml-metrics', package_finder)}"
     assert pin_packages(contents) == expected
+
+def get_latest_version_from_pypi(package_name):
+    """ Get latest package version from pypi"""
+    url = f"https://pypi.org/pypi/{package_name}/json"
+    latest_version = requests.get(url).json()["info"]["version"]
+    return latest_version
+
+
+def test_yanked_version_exclusion():
+    """ Test latest-pypi-version to exclude yanked versions"""
+    package_names = ["azureml-core", "azureml-acft-image-components", "azureml-metrics"]
+    for package_name in package_names:
+        expected = get_latest_version_from_pypi(package_name)
+        actual = get_latest_package_version(package_name, package_finder)
+        assert expected == actual
