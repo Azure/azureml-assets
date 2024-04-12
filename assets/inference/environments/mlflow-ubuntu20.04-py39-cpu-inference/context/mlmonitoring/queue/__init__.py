0ad638bf3bd32954e2c74fcaaf3e81e55060506e
Walter Martin <wamartin@microsoft.com>
2024-04-12 10:23:08 -0700

    Add MLflow 3.9 base image

---
+++ assets/inference/environments/mlflow-ubuntu20.04-py39-cpu-inference/context/mlmonitoring/queue/__init__.py
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from .queue import init_queue, teardown_queue, get_queue
+
+__all__ = ["init_queue", "teardown_queue", "get_queue"]
