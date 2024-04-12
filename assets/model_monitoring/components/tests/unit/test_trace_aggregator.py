9c73480a1034deb048fe7cb0437c2dd36c7ec04b
Alan Poblette <alanpoblette@microsoft.com>
2024-04-12 11:36:52 -0700

    fix syntax

--- assets/model_monitoring/components/tests/unit/test_trace_aggregator.py
+++ assets/model_monitoring/components/tests/unit/test_trace_aggregator.py
@@ -310,8 +310,8 @@ class TestGenAISparkPreprocessor:
              datetime(2024, 2, 5, 6), datetime(2024, 2, 5, 7)),
             # request_id data
             # TODO: uncomment when we want to test replacing trace_id with request_id
-            # (_span_log_data_request_id, _preprocessed_log_schema, _trace_log_data_request_id, _trace_log_schema, True,
-            #  datetime(2024, 2, 5, 9), datetime(2024, 2, 5, 10))
+            # (_span_log_data_request_id, _preprocessed_log_schema, _trace_log_data_request_id, _trace_log_schema,
+            #  True, datetime(2024, 2, 5, 9), datetime(2024, 2, 5, 10))
         ]
     )
     def test_trace_aggregator(
