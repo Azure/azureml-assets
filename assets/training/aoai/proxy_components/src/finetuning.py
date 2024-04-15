daad30de9e998cda429b3a8615666c819c91e85d
CHIRAG BHATT <chiragbhatt@microsoft.com>
2024-04-15 23:22:50 +0530

    fixed code quality'

--- assets/training/aoai/proxy_components/src/finetuning.py
+++ assets/training/aoai/proxy_components/src/finetuning.py
@@ -19,21 +19,22 @@ import pandas as pd
 
 logger = get_logger(__name__)
 
+
 class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
     """Fine-tune proxy class to submit fine-tune job to AOAI."""
 
     def __init__(self, aoai_client_manager: AzureOpenAIClientManager):
         super().__init__(aoai_client_manager.endpoint_name,
-                    aoai_client_manager.endpoint_resource_group,
-                    aoai_client_manager.endpoint_subscription)
+                         aoai_client_manager.endpoint_resource_group,
+                         aoai_client_manager.endpoint_subscription)
         self.aoai_client = aoai_client_manager.get_azure_openai_client()
         self.training_file_id = None
         self.validation_file_id = None
         self.finetuning_job_id = None
 
     def submit_job(self, training_file_path: str, validation_file_path: Optional[str], model: str, n_epochs: Optional[int],
-                    batch_size: Optional[int], learning_rate_multiplier: Optional[float], suffix=Optional[str]):
-        
+                   batch_size: Optional[int], learning_rate_multiplier: Optional[float], suffix=Optional[str]):
+
         logger.info("Step 1: Uploading data to AzureOpenAI resource")
         self.upload_files(training_file_path, validation_file_path)
 
@@ -62,39 +63,44 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
         if self.validation_file_id is not None:
             delete_file_metadata = self.aoai_client.files.delete(file_id=self.validation_file_id)
             logger.debug(f"file id: {delete_file_metadata.id} deleted : {delete_file_metadata.deleted}")
-        
+
     def upload_files(self, train_file_path: str, validation_file_path: str = None):
         """Upload training and validation files to azure openai."""
-        
-        train_file_name, validation_file_name = utils.get_train_validation_filename(train_file_path, validation_file_path)
+
+        train_file_name, validation_file_name = utils.get_train_validation_filename(train_file_path,\
+                                                                                    validation_file_path)
         train_data, validation_data = utils.get_train_validation_data(train_file_path, validation_file_path)
 
         if validation_file_path is None:
-            logger.debug(f"validation file not provided, train data will be split in {utils.train_dataset_split_ratio} ratio to create validation data")
+            logger.debug(f"validation file not provided, train data will be split in\
+                         {utils.train_dataset_split_ratio} ratio to create validation data")
 
         logger.debug(f"uploading training file : {train_file_name}")
-        train_metadata = self.aoai_client.files.create(file=(train_file_name, train_data, 'application/json'), purpose='fine-tune')
+        train_metadata = self.aoai_client.files.create(file=(train_file_name, train_data, 'application/json'), 
+                                                       purpose='fine-tune')
         self.training_file_id = train_metadata.id
         self._wait_for_processing(train_metadata.id)
         logger.info("training file uploaded")
 
         logger.debug(f"uploading validation file : {validation_file_name}")
-        validation_metadata = self.aoai_client.files.create(file=(validation_file_name, validation_data, 'application/json'), purpose='fine-tune')
+        validation_metadata = self.aoai_client.files.create(file=(validation_file_name, validation_data, 'application/json'),\
+                                                            purpose='fine-tune')
         self.validation_file_id = validation_metadata.id
         self._wait_for_processing(validation_metadata.id)
         logger.info("validation file uploaded")
 
     def _wait_for_processing(self, file_id):
         upload_file_metadata = self.aoai_client.files.wait_for_processing(file_id)
-        logger.info(f"file status is : {upload_file_metadata.status} for file name : {upload_file_metadata.filename}")
+        filename = upload_file_metadata.filename
+        logger.info(f"file status is : {upload_file_metadata.status} for file name : {filename}")
 
         if upload_file_metadata.status == "error":
             error_reason = upload_file_metadata.model_dump()["error"]
-            error_string = f"Processing file failed for {upload_file_metadata.filename}, file id: {upload_file_metadata.id}, reason: {error_reason}"
+            error_string = f"Processing file failed for {filename}, file id: {upload_file_metadata.id}, reason: {error_reason}"
             logger.error(error_string)
             raise Exception(error_string)
 
-    def submit_finetune_job(self,model, n_epochs, batch_size, learning_rate_multiplier, suffix=None):
+    def submit_finetune_job(self, model, n_epochs, batch_size, learning_rate_multiplier, suffix=None):
         """Submit fine-tune job to AOAI."""
         logger.debug(f"Starting fine-tune job, model: {model}, n_epochs: {n_epochs},\
                      batch_size: {batch_size}, learning_rate_multiplier: {learning_rate_multiplier},\
@@ -107,7 +113,7 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
             validation_file=self.validation_file_id,
             hyperparameters=hyperparameters,
             suffix=suffix)
-        
+
         logger.debug(f"started finetuning job in Azure OpenAI resource. Job id: {finetune_job.id}")
 
         self.finetuning_job_id = finetune_job.id
@@ -139,7 +145,7 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
 
     def cancel_job(self):
         """Cancel finetuning job in Azure OpenAI resource."""
-        logger.debug(f"job cancellation has been triggered, cancelling finetuning job")
+        logger.debug("job cancellation has been triggered, cancelling job")
 
         self.delete_files()
 
@@ -192,7 +198,7 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
         return last_event_message
 
     def get_hyperparameters_dict(self, n_epochs, batch_size, learning_rate_multiplier) -> Hyperparameters:
-        
+
         hyperparameters: Hyperparameters = {}
 
         if n_epochs:
@@ -209,22 +215,8 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
             hyperparameters["learning_rate_multiplier"] = learning_rate_multiplier
         else:
             logger.info("learning rate multiplier not passed, it will be determined dynamically")
-        
-        return hyperparameters
-        """Log events like training started, running nth epoch etc."""
-        job_events = self.aoai_client.fine_tuning.jobs.list_events(self.job_id).data
-        event_message_list = []
-        for job_event in job_events:
-            if last_event == job_event.message:
-                break
-            event_message_list.append(job_event.message)
 
-        if len(event_message_list) > 0:
-            last_event = event_message_list[0]
-            event_message_list.reverse()
-            for event_message in event_message_list:
-                logger.debug(event_message)
-        return last_event
+        return hyperparameters
 
 
 def main():
@@ -257,8 +249,8 @@ def main():
         logger.info("Starting Finetuning in Azure OpenAI resource")
 
         finetuned_model_id = finetune_component.submit_job(
-            training_file_path = args.training_file_path,
-            validation_file_path = args.validation_file_path,
+            training_file_path=args.training_file_path,
+            validation_file_path=args.validation_file_path,
             model=args.model,
             n_epochs=args.n_epochs,
             batch_size=args.batch_size,
