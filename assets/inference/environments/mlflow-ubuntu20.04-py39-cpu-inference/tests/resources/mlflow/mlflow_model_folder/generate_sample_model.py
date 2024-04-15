328599ea5ddc535f5c5fd4ec72cdb509fb769049
Walter Martin <wamartin@microsoft.com>
2024-04-15 12:40:14 -0700

    add unit testing
    Signed-off-by: Walter Martin <wamartin@microsoft.com>
    
    temporarily delete model files to be able to push
    
    Signed-off-by: Walter Martin <wamartin@microsoft.com>

---
+++ assets/inference/environments/mlflow-ubuntu20.04-py39-cpu-inference/tests/resources/mlflow/mlflow_model_folder/generate_sample_model.py
@@ -0,0 +1,25 @@
+import mlflow # using mlflow==1.24.0, also needs protobuf==3.20.3, numpy==1.22.2
+# from mlflow.models import infer_signature
+import pandas as pd
+import os
+
+# define a custom model
+class MyModel(mlflow.pyfunc.PythonModel):
+    def predict(self, context, model_input):
+        return self.my_custom_function(model_input)
+
+    def my_custom_function(self, model_input):
+        # do something with the model input
+        return pd.DataFrame([0])
+
+# save the model
+my_model = MyModel()
+# if you have an error about YAML, delete mlruns directory
+with mlflow.start_run():
+    from pathlib import Path
+    import posixpath
+    path = "./temporary"
+    path = posixpath.normpath(path)
+    input_df = pd.DataFrame([[0, 1]], columns=["a", "b"])
+    signature = mlflow.models.infer_signature(input_df, my_model.predict(None, input_df))
+    mlflow.pyfunc.log_model(artifact_path=path, python_model=my_model, signature=signature, input_example=input_df)
---
+++ assets/inference/environments/mlflow-ubuntu20.04-py39-cpu-inference/tests/resources/mlflow/mlflow_no_params/get_formatted_example.py
@@ -0,0 +1,24 @@
+# using mlflow==1.29.0, mlflow-skinny==1.29.0, azureml-mlflow==1.45.0
+from mlflow.models.signature import infer_signature
+from sklearn import datasets, linear_model
+import mlflow
+from azureml.opendatasets import Diabetes
+
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True, as_frame=True)
+regr = linear_model.LinearRegression()
+regr.fit(diabetes_X, diabetes_y)
+
+model_name = "sklearn_no_params"
+
+data = diabetes_X
+model = regr
+output = model.predict(data)
+signature = infer_signature(data, output)
+
+with mlflow.start_run() as run:
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="no_params",
+        signature=signature,
+        input_example=data,
+    )
