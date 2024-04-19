253ba7ea477abe773c1ec832fc1989c70f52d8e9
pmanoj <pavanmanojj@microsoft.com>
2024-04-20 01:33:05 +0530

    Pmanoj/scripts/copy tokenizer files chat completion (#2731)
    * components for chat completion
    
    * copying tokenizer files to model folder for chat completion
    
    * upgrading the component versions

--- assets/training/finetune_acft_hf_nlp/src/model_converter/model_converter_utils.py
+++ assets/training/finetune_acft_hf_nlp/src/model_converter/model_converter_utils.py
@@ -154,10 +154,10 @@ def load_and_merge_peft_lora_model(model_path: str, component_args: Namespace, f
 def copy_tokenizer_files_to_model_folder(mlflow_model_folder: str, task_name: str):
     """Copy tokenizer files to model folder.
 
-    Copying only for Text Gen tasks as this is needed for vLLM inference engine.
+    Copying for Text Gen and Chat Completion tasks as this is needed for vLLM inference engine.
     It expects tokenizer and model files in same folder i.e. "data/model".
     """
-    if task_name not in [Tasks.TEXT_GENERATION]:
+    if task_name not in [Tasks.TEXT_GENERATION, Tasks.CHAT_COMPLETION]:
         logger.info("Not copying tokenizer files to model folder for {}".format(task_name))
         return
 
