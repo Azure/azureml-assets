a3d6ee17900b9719c3020522b950afc69fae974d
Ge Gao <gega@microsoft.com>
2024-04-25 14:01:29 +0800

    get portal_url from result instead of construct them

--- scripts/promptflow-ci/utils/flow_utils.py
+++ scripts/promptflow-ci/utils/flow_utils.py
@@ -108,11 +108,17 @@ def get_run_id_and_url(res, sub, rg, ws):
             if match:
                 run_id = match.group(1)
                 portal_url = (
-                    f"https://ml.azure.com/prompts/flow/bulkrun/run/{run_id}/details"
+                    f"https://ml.azure.com/prompts/bulkrun/{run_id}/details"
                     f"?wsid=/subscriptions/{sub}/resourceGroups/{rg}/providers"
                     f"/Microsoft.MachineLearningServices/workspaces/{ws}"
                     )
-                log_debug(f"runId: {run_id}")
+                log_debug(f"!!!!! runId: {run_id}")
+        
+        if ('"portal_url":' in line):
+            match = re.search(r'"portal_url": "(.*?)",', line)
+            if match:
+                portal_url = match.group(1)
+                log_debug(f"!!!!! portal_url: {portal_url}")
     return run_id, portal_url
 
 
