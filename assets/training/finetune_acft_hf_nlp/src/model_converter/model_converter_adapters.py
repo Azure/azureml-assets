35f06aa1ac5f2cbb66d28487250bcafad258d33a
Anubha Jain <anubhajain@microsoft.com>
2024-06-03 16:58:22 +0530

    Resolving comments

--- assets/training/finetune_acft_hf_nlp/src/model_converter/model_converter_adapters.py
+++ assets/training/finetune_acft_hf_nlp/src/model_converter/model_converter_adapters.py
@@ -284,9 +284,7 @@ class Pytorch_to_OSS_MlFlow_ModelConverter(ModelConverter, PyTorch_to_MlFlow_Mod
         # Temp Fix:
         # specific check for t5 text-classification so that base model dependencies doesn't get pass
         # and use transformers version 4.40.0 from infer dependencies
-        if self.is_t5_text_classification_finetune(model.config.model_type):
-            self.remove_unwanted_packages(self.mlflow_model_save_path)
-        else:
+        if not self.is_t5_text_classification_finetune(model.config.model_type):
             conda_file_path = Path(self.ft_pytorch_model_path, MLFlowHFFlavourConstants.CONDA_YAML_FILE)
             if conda_file_path.is_file():
                 self.mlflow_save_model_kwargs.update({"conda_env": str(conda_file_path)})
@@ -317,5 +315,9 @@ class Pytorch_to_OSS_MlFlow_ModelConverter(ModelConverter, PyTorch_to_MlFlow_Mod
         self.download_license_file(self.model_name, self.ft_pytorch_model_path, self.mlflow_model_save_path)
         self.add_model_signature()
         self.copy_finetune_config(self.ft_pytorch_model_path, self.mlflow_model_save_path)
+
+        # Temp fix for t5 text-classification
+        if self.is_t5_text_classification_finetune(model.config.model_type):
+            self.remove_unwanted_packages(self.mlflow_model_save_path)
 
         logger.info("Saved MLFlow model using OSS flavour.")
