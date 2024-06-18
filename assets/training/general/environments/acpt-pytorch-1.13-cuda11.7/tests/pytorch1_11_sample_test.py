edab5c743bf1a36c640706398cecaf5562bd7157
cassieesvelt <cassieesvelt@gmail.com>
2024-06-18 11:31:56 -0700

    fix flake errors

--- assets/training/general/environments/acpt-pytorch-1.13-cuda11.7/tests/pytorch1_11_sample_test.py
+++ assets/training/general/environments/acpt-pytorch-1.13-cuda11.7/tests/pytorch1_11_sample_test.py
@@ -40,13 +40,13 @@ def test_pytorch_1_13():
     # create the command
     job = command(
         code=this_dir / JOB_SOURCE_CODE,  # local path where the code is stored
-        command="pip install -r requirements.txt" \
-                " && python pretrain_glue.py --tensorboard_log_dir \"/outputs/runs/\"" \
-                " --deepspeed ds_config.json --num_train_epochs 5 --output_dir outputs --disable_tqdm 1" \
-                " --local_rank $RANK --evaluation_strategy \"epoch\" --logging_strategy \"epoch\"" \
-                " --per_device_train_batch_size 93 --gradient_accumulation_steps 1" \
-                " --per_device_eval_batch_size 93 --learning_rate 3e-05 --adam_beta1 0.8 --adam_beta2 0.999" \
-                " --weight_decay 3e-07 --warmup_steps 500 --fp16 --logging_steps 1000" \
+        command="pip install -r requirements.txt"
+                " && python pretrain_glue.py --tensorboard_log_dir \"/outputs/runs/\""
+                " --deepspeed ds_config.json --num_train_epochs 5 --output_dir outputs --disable_tqdm 1"
+                " --local_rank $RANK --evaluation_strategy \"epoch\" --logging_strategy \"epoch\""
+                " --per_device_train_batch_size 93 --gradient_accumulation_steps 1"
+                " --per_device_eval_batch_size 93 --learning_rate 3e-05 --adam_beta1 0.8 --adam_beta2 0.999"
+                " --weight_decay 3e-07 --warmup_steps 500 --fp16 --logging_steps 1000"
                 " --model_checkpoint \"bert-large-uncased\"",
         outputs={
             "output": Output(
