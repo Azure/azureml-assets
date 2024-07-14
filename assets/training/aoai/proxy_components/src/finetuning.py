918421c99a990d77be6cb59978419436a484deb0
cjayanth <cjayanth@microsoft.com>
2024-07-15 00:05:26 +0530

    white spaces removed again fix

--- assets/training/aoai/proxy_components/src/finetuning.py
+++ assets/training/aoai/proxy_components/src/finetuning.py
@@ -171,9 +171,9 @@ class AzureOpenAIFinetuning(AzureOpenAIProxyComponent):
             validation_file=self.validation_file_id,
             hyperparameters=hyperparameters,
             extra_headers=hyperparameters_1p,
-            suffix=suffix)       
+            suffix=suffix)
         logger.debug(f"started finetuning job in Azure OpenAI resource. Job id: {finetune_job.id}")
-        logger.debug(f"Response of the finetune create call : {str(finetune_job)}")
+        logger.debug(f"Response of finetune create call : {str(finetune_job)}")
 
         return finetune_job.id
 
