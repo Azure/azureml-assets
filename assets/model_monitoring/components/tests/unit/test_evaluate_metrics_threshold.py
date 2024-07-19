719d2503000dba27aad501bcdd6b57e867977964
Yuan Cheng <yuacheng@microsoft.com>
2024-07-19 15:27:52 -0700

    Fix evaluate metrics empty breached data

--- assets/model_monitoring/components/tests/unit/test_evaluate_metrics_threshold.py
+++ assets/model_monitoring/components/tests/unit/test_evaluate_metrics_threshold.py
@@ -75,6 +75,28 @@ metrics_not_breached = [
         ]
 metrics_not_breached_df = create_pyspark_dataframe(metrics_not_breached, columns)
 
+metrics_str = [
+            (TWO_SAMPLE_KOLMOGOROV_SMIRNOV_TEST_METRIC_NAME, "0.367", "0.5"),
+            (PEARSONS_CHI_SQUARED_TEST_METRIC_NAME, "0.367", "0.5"),
+            (NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN_METRIC_NAME, "0.367", "0.5"),
+            (AVERAGE_COHERENCE_SCORE_METRIC_NAME, "3.0", "20.0"),
+            (AVERAGE_GROUNDEDNESS_SCORE_METRIC_NAME, "3.0", "5.0"),
+            (AVERAGE_FLUENCY_SCORE_METRIC_NAME, "3.0", "100"),
+            ("dropped_metric", "value", "threshold"),
+            ("dropped_metric_2", "three", "five"),
+        ]
+metrics_str_df = create_pyspark_dataframe(metrics_str, columns)
+
+expected_breached_str = [
+        (TWO_SAMPLE_KOLMOGOROV_SMIRNOV_TEST_METRIC_NAME, "0.367", "0.5"),
+        (PEARSONS_CHI_SQUARED_TEST_METRIC_NAME, "0.367", "0.5"),
+        (NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN_METRIC_NAME, "0.367", "0.5"),
+        (AVERAGE_COHERENCE_SCORE_METRIC_NAME, "3.0", "20.0"),
+        (AVERAGE_GROUNDEDNESS_SCORE_METRIC_NAME, "3.0", "5.0"),
+        (AVERAGE_FLUENCY_SCORE_METRIC_NAME, "3.0", "100.0"),
+    ]
+expected_breached_str_df = create_pyspark_dataframe(expected_breached_str, columns)
+
 null_data = [
     (TWO_SAMPLE_KOLMOGOROV_SMIRNOV_TEST_METRIC_NAME, None, None),
     (PEARSONS_CHI_SQUARED_TEST_METRIC_NAME, 0.8, None),
@@ -85,11 +107,12 @@ null_data = [
 ]
 test_null_df = create_pyspark_dataframe(null_data, columns)
 
+# For this setup, the metric_value and threshold_value columns are StringType()
 valid_clean_data = [
     (TWO_SAMPLE_KOLMOGOROV_SMIRNOV_TEST_METRIC_NAME, 0.8, 0.5),
     (PEARSONS_CHI_SQUARED_TEST_METRIC_NAME, 0.8, 0.9),
     (NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN_METRIC_NAME, 0.5, 0.5),
-    # this is not a likely case but test non-empty strings
+    # The metrics value column in gsq metrics table is string type becasue there are null/empty values
     (AVERAGE_FLUENCY_SCORE_METRIC_NAME, "3.0", '3.0'),
 ]
 test_clean_df = create_pyspark_dataframe(valid_clean_data, columns)
@@ -97,6 +120,29 @@ test_clean_df = create_pyspark_dataframe(valid_clean_data, columns)
 spark = SparkSession.builder.getOrCreate()
 emptyRDD = spark.sparkContext.emptyRDD()
 
+clean_str_data = [
+    (AVERAGE_COHERENCE_SCORE_METRIC_NAME, None, "5.0"),
+    (AVERAGE_GROUNDEDNESS_SCORE_METRIC_NAME, "3.0", ""),
+    (AVERAGE_FLUENCY_SCORE_METRIC_NAME, "3.0", "threshold"),
+    (AVERAGE_SIMILARITY_SCORE_METRIC_NAME, "5.0", "5.0"),
+    (AVERAGE_RELEVANCE_SCORE_METRIC_NAME, "3.0", "4.0"),
+]
+clean_str_df = create_pyspark_dataframe(clean_str_data, columns)
+
+expected_clean_str_data = [
+    (AVERAGE_FLUENCY_SCORE_METRIC_NAME, "3.0", "threshold"),
+    (AVERAGE_SIMILARITY_SCORE_METRIC_NAME, "5.0", "5.0"),
+    (AVERAGE_RELEVANCE_SCORE_METRIC_NAME, "3.0", "4.0"),
+]
+expected_clean_str_df = create_pyspark_dataframe(expected_clean_str_data, columns)
+
+clean_double_data = [
+    (TWO_SAMPLE_KOLMOGOROV_SMIRNOV_TEST_METRIC_NAME, 0.367, 0.5),
+    (PEARSONS_CHI_SQUARED_TEST_METRIC_NAME, 0.367, 0.5),
+    (NORMALIZED_DISCOUNTED_CUMULATIVE_GAIN_METRIC_NAME, 0.367, 0.5),
+    (AGGREGATED_COHERENCE_PASS_RATE_METRIC_NAME, 3.0, 5.0),
+]
+clean_double_data_df = create_pyspark_dataframe(clean_double_data, columns)
 
 @pytest.mark.unit
 class TestEvaluateMetricsThreshold:
@@ -104,7 +150,8 @@ class TestEvaluateMetricsThreshold:
 
     @pytest.mark.parametrize("metrics_df, breached_metrics_df",
                              [(metrics_breached_df, metrics_breached_df),
-                              (metrics_not_breached_df, emptyRDD)])
+                              (metrics_not_breached_df, emptyRDD),
+                              (metrics_str_df, expected_breached_str_df)])
     def test_calculate_metrics_breach(
             self,
             metrics_df,
@@ -112,12 +159,15 @@ class TestEvaluateMetricsThreshold:
     ):
         """Test calculate metrics breach."""
         actual_breached_metrics_df = calculate_metrics_breach(metrics_df)
+        actual_breached_metrics_df.show()
         assert breached_metrics_df.count() == actual_breached_metrics_df.count()
         assert sorted(breached_metrics_df.collect()) == sorted(actual_breached_metrics_df.collect())
 
     @pytest.mark.parametrize("metrics_df, expected_metrics_df",
                              [(test_null_df, emptyRDD),
-                              (test_clean_df, test_clean_df)])
+                              (test_clean_df, test_clean_df),
+                              (clean_str_df, expected_clean_str_df),
+                              (clean_double_data_df, clean_double_data_df)])
     def test_clean_metrics_df(self, metrics_df, expected_metrics_df):
         """Test clean metrics dataframe."""
         actual_cleaned_metrics_df = _clean_metrics_df(metrics_df)
