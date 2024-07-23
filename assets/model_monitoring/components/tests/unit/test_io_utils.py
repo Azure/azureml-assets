17e7af8a96eead2da162ce1f86ba04a6919b2d8b
Alan Poblette <alanpoblette@microsoft.com>
2024-07-23 12:08:20 -0700

    fix test name

--- assets/model_monitoring/components/tests/unit/test_io_utils.py
+++ assets/model_monitoring/components/tests/unit/test_io_utils.py
@@ -12,7 +12,7 @@ from src.shared_utilities.io_utils import (
 
 
 @pytest.mark.unit
-class TestDFUtils:
+class TestIOUtils:
     def test_save_spark_df_as_mltable_duplicate_case_sensitive_columns(self):
         """Test the save dataframe as mltable functionality."""
         spark = init_spark()
