eec007ae26566fc41cde96958a1cf5282826e73c
Ali Soylemezoglu <alisoy@microsoft.com>
2024-08-08 14:54:41 -0400

    addressing comments

--- scripts/azureml-assets/azureml/assets/update_spec.py
+++ scripts/azureml-assets/azureml/assets/update_spec.py
@@ -120,7 +120,7 @@ def update(asset_config: assets.AssetConfig, release_directory_root: Path = None
     if 'tags' in contents_yaml:
         unresolved_tags = contents_yaml['tags']
         contents_yaml['tags'] = {k: (LiteralScalarString(resolve_from_file_for_asset(asset_config, v))
-                                    if os.path.isfile(asset_config._append_to_file_path(v)) else v)
+                                     if os.path.isfile(asset_config._append_to_file_path(v)) else v)
                                     for k, v in unresolved_tags.items()}
 
     # Write spec
