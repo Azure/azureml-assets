bb3d77d0eb0d37c19e6a6aa39fb91e931d4768fd
Yuan Cheng <yuacheng@microsoft.com>
2024-09-09 11:49:55 -0700

    add hidden files

--- assets/model_monitoring/components/tests/e2e/utils/test_publish_components_for_ci.py
+++ assets/model_monitoring/components/tests/e2e/utils/test_publish_components_for_ci.py
@@ -8,11 +8,11 @@ import os
 
 
 @pytest.mark.skipif(
-    condition=os.path.exists('component_version/.version_upload'),
+    condition=os.path.exists('.version_upload'),
     reason="For local e2e pytest runs and in CI if we already uploaded the shared component version")
 class TestPublishComponentsForCI():
     """Class for model-monitoring-ci workflow to upload components and data once before splitting tests to runners."""
 
     def test_publish_components(self, upload_component_version_file, cleanup_previous_e2e_tests):
         """Init calls fixture to publish components."""
-        assert os.path.exists('component_version/.version_upload')
+        assert os.path.exists('.version_upload')
