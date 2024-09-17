532ff5b28bc52abf5bc9b64368a2f4357835be3c
Ayush Mishra <ayushmishra@microsoft.com>
2024-09-18 01:55:29 +0530

    update tests

--- assets/batch_score/components/driver/tests/unit/test_prs_compatability.py
+++ assets/batch_score/components/driver/tests/unit/test_prs_compatability.py
@@ -14,12 +14,12 @@ from tests.fixtures.geneva_event_listener import mock_import
 def test_prs_code_importing():
     """Test PRS code importing."""
     # Should point to the same root as the "code" in the batch-score component yamls
-    component_code_root = "../../../src/batch_score"
+    component_code_root = "../../src/"
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), component_code_root))
     sys.path.append(file_path)
 
     # Should be the exact same as the "entry_script" in the batch-score component yamls
-    entry_script = "root.main"
+    entry_script = "batch_score_oss.main"
 
     with mock.patch('importlib.import_module', side_effect=mock_import):
         module = importlib.import_module(entry_script, file_path)
