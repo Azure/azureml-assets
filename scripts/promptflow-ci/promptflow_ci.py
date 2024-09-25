e15849bdcb0f6458924080f33b6393f37749681b
lykelly19 <kellyly@microsoft.com>
2024-09-25 10:59:54 -0400

    update skipped-flows

--- scripts/promptflow-ci/promptflow_ci.py
+++ scripts/promptflow-ci/promptflow_ci.py
@@ -165,9 +165,14 @@ if __name__ == "__main__":
     parser.add_argument('--flow_submit_mode', type=str, default="sync")
     parser.add_argument('--run_time', type=str, default="default-mir")
     parser.add_argument('--skipped_flows', type=str,
-                        default="bring_your_own_data_qna,template_chat_flow,template_eval_flow,playground-ayod-rag,\
-                        chat-quality-safety-eval,qna-non-rag-metrics-eval,qna-quality-safety-eval,qna-rag-metrics-eval,\
-                        rai-eval-ui-dag-flow,rai-qna-quality-safety-eval")
+                        default="bring_your_own_data_qna,template_chat_flow,template_eval_flow,playground-ayod-rag," \
+                        "chat-quality-safety-eval,qna-non-rag-metrics-eval,qna-quality-safety-eval," \
+                        "qna-rag-metrics-eval,rai-eval-ui-dag-flow,rai-qna-quality-safety-eval," \ 
+                        "qna-ada-similarity-eval,qna-relevance-eval,ask-wikipedia,classification-accuracy-evals," \ 
+                        "qna-f1-score-eval,count-cars,analyze-conversations,rerank-qna,multi-index-rerank-qna," \ 
+                        "detect-defects,analyze-documents,qna-gpt-similarity-eval,use-functions-with-chat-models," \ 
+                        "qna-groundedness-eval,bring-your-own-data-chat-qna,qna-coherence-eval,template-standard-flow," \ 
+                        "qna-with-your-own-data-using-faiss-index,chat-with-wikipedia")
     # Skip bring_your_own_data_qna test, the flow has a bug.
     # Bug 2773738: Add retry when ClientAuthenticationError
     # https://msdata.visualstudio.com/Vienna/_workitems/edit/2773738
