9086563677811224617d85a174f012fc1a67a815
visahan-24 <visahanmohan@microsoft.com>
2024-10-18 21:49:59 +0530

    Doc String for copy file contents function

--- assets/training/distillation/src/dsl_condition_output.py
+++ assets/training/distillation/src/dsl_condition_output.py
@@ -11,6 +11,16 @@ import argparse
 
 
 def copy_file_contents(input_src1, ft_input_train_file_path):
+    """
+    Copy the contents of one file to another.
+
+    Parameters:
+    input_src1 (str): The path to the source file.
+    ft_input_train_file_path (str): The path to the destination file.
+
+    Returns:
+    None
+    """
     # Read the contents of input_src1
     with open(input_src1, "r") as src_file:
         contents = src_file.read()
