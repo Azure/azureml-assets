ca49750aec2ced103abdbb20c891f038e4cf8015
Arun Sureddi <arunsu@microsoft.com>
2024-11-15 09:47:10 -0800

    Pass model folder to test

--- assets/inference/environments/mlflow-ubuntu20.04-py39-cpu-inference/tests/src/main.py
+++ assets/inference/environments/mlflow-ubuntu20.04-py39-cpu-inference/tests/src/main.py
@@ -4,6 +4,7 @@
 """Validate minimal inference cpu environment by running azmlinfsrv."""
 
 # imports
+import json
 import os
 import subprocess
 import requests
@@ -15,24 +16,30 @@ import argparse
 def main(args):
     """Start inference server and post scoring request."""
     # start the server
-    server_process = start_server("/var/tmp", ["--entry_script", args.score, "--port", "8081"])
+    server_process = start_server("/var/tmp", 
+                                  ["--entry_script", args.score, "--port", args.port],
+                                  args.model_dir,
+                                  args.port)
 
     # score a request
-    req = score_with_post()
+    with open(args.score_input) as f:
+        payload_data = json.load(f)
+
+    headers = {"Content-Type": "application/json"}
+    res = score_with_post(headers=headers, data=payload_data)
     server_process.kill()
 
-    print(req)
+    print(res)
 
 
-def start_server(log_directory, args, timeout=timedelta(seconds=15)):
+def start_server(log_directory, args, model_dir, port=8081, timeout=timedelta(seconds=60)):
     """Start inference server with options."""
     stderr_file = open(os.path.join(log_directory, "stderr.txt"), "w")
     stdout_file = open(os.path.join(log_directory, "stdout.txt"), "w")
 
     env = os.environ.copy()
-    env["AZUREML_MODEL_DIR"] = "mlflow_model_folder"
-    env["MLFLOW_MODEL_FOLDER"] = "mlflow_model_folder"
-    env["AZUREML_EXTRA_CONDA_YAML_ABS_PATH"] = "/var/model_dir/mlflow_model_folder/conda.yaml"
+    env["AZUREML_MODEL_DIR"] = model_dir
+    env["MLFLOW_MODEL_FOLDER"] = model_dir
     server_process = subprocess.Popen(["azmlinfsrv"] + args, stdout=stdout_file, stderr=stderr_file, env=env)
 
     max_time = datetime.now() + timeout
@@ -41,7 +48,7 @@ def start_server(log_directory, args, timeout=timedelta(seconds=15)):
         time.sleep(0.25)
         req = None
         try:
-            req = requests.get("http://127.0.0.1:8081", timeout=10)
+            req = requests.get("http://127.0.0.1:{port}", timeout=10)
         except Exception as e:
             print(e)
 
@@ -59,9 +66,9 @@ def start_server(log_directory, args, timeout=timedelta(seconds=15)):
     return server_process
 
 
-def score_with_post(headers=None, data=None):
+def score_with_post(headers=None, data=None, port=8081):
     """Post scoring request to the server."""
-    url = "http://127.0.0.1:8081/score"
+    url = "http://127.0.0.1:{port}/score"
     return requests.post(url=url, headers=headers, data=data)
 
 
