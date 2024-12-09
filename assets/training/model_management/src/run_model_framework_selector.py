1cc8bb541ccf5d09d1601ddf3f5ee4745c5d5749
Abhishek Kumar <abhisheku@microsoft.com>
2024-12-09 11:41:00 +0530

    test conditional block changes

--- assets/training/model_management/src/run_model_framework_selector.py
+++ assets/training/model_management/src/run_model_framework_selector.py
@@ -1,27 +1,28 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Validate Import Pipeline Parameters."""
-# import argparse
+"""Select Model Framework Component."""
 
-from azureml.model.mgmt.utils.exceptions import swallow_all_exceptions
+from azure.ai.ml import Input
+from mldesigner import Output, command_component
 from azureml.model.mgmt.utils.logging_utils import get_logger
-from mldesigner import Input, Output, command_component
+from azureml.model.mgmt.utils.exceptions import swallow_all_exceptions
 
-logger = get_logger(name=__name__)
+logger = get_logger(__name__)
 
 @command_component
 @swallow_all_exceptions(logger)
 def validate(
-        model_framework: Input(type="string", optional=False, default='HuggingFace')
-        ) -> Output(type="boolean", is_control=True): # noqa: F821
-    """Return true if model_framework is MMLab, False otherwise."""
+        model_framework: Input(type="string", optional=False)  # noqa: F821
+) -> Output(type="boolean", is_control=True):  # noqa: F821
+    """Entry function of model validation script."""
 
-    print(f"Model framework: {model_framework}")
-    result = model_framework == "MMLab"
-    
-    print(f"Model framework: {model_framework}, result: {result}")
+    if model_framework == "MMLab":
+        result = True
+    else:
+        result = False
+
     logger.info(f"Model framework: {model_framework}, result: {result}")
-    
-    # Return the result as a boolean control output
+    print(f"Model framework: {model_framework}, result: {result}")
+
     return result
