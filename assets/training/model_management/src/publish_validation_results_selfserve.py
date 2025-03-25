21f4d2bbe9eab11cb1cbb09de34a1a71ab48814c
Abhishek Kumar <abhisheku@microsoft.com>
2025-03-25 11:00:38 +0530

    AML pipeline component for MaaP self serve validation

---
+++ assets/training/model_management/src/publish_validation_results_selfserve.py
@@ -0,0 +1,175 @@
+"""Update model onboarding version with CommonBench results."""
+
+import sys
+import os
+import uuid
+import json
+import logging
+import requests
+import argparse
+from datetime import datetime, timezone
+from azure.identity import ManagedIdentityCredential
+from azure.ai.ml.identity import AzureMLOnBehalfOfCredential
+from datetime import datetime
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def read_results_from_file(file_path):
+    """Read the metrics results from the given file path."""
+    try:
+        with open(file_path, 'r') as f:
+            results_dict = json.load(f)
+        print(f"Results loaded from {file_path}")
+        return results_dict
+    except Exception as e:
+        print(f"Error reading from file: {e}")
+        return None
+
+
+def update_model_onboarding_version(
+    publisher_name,
+    model_name,
+    model_version,
+    selfserve_base_url,
+    sku,
+    metrics_storage_uri
+):
+    """Update model onboarding version with benchmark results."""
+    current_time = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    is_obo = False
+    try:
+        credential = AzureMLOnBehalfOfCredential()
+        token = credential.get_token(
+            "https://management.azure.com/.default").token
+        is_obo = True
+    except Exception as ex:
+        logger.warning(f"Failed to get OBO credentials - {ex}")
+
+    if not is_obo:
+        try:
+            logger.info("Fetching MSI credential")
+            msi_client_id = os.environ.get("DEFAULT_IDENTITY_CLIENT_ID")
+            credential = ManagedIdentityCredential(client_id=msi_client_id)
+            token = credential.get_token(
+                "https://management.azure.com/.default").token
+        except Exception as ex:
+            raise (f"Failed to get MSI credentials : {ex}")
+
+    metrics_path_dict = read_results_from_file(metrics_storage_uri)
+
+    run_id = str(uuid.uuid4())
+
+    validation_result = []
+
+    if metrics_path_dict.get("perf_bench_path") is not None:
+        validation_result.append({
+            "runId": run_id,
+            "type": "PERF_BENCHMARK",
+            "passed": True,
+            "message": "Baseline data is captured successfully",
+            "validationResultUrl": metrics_path_dict.get("perf_bench_path"),
+            "createdTime": current_time,
+            "status": "success",
+            "sku": sku
+        })
+
+    if metrics_path_dict.get("api_validation_path") is not None:
+        validation_result.append({
+            "runId": run_id,
+            "type": "API_VALIDATION",
+            "passed": True,
+            "message": "API validation passed successfully",
+            "validationResultUrl": metrics_path_dict.get("api_validation_path"),
+            "status": "success",
+            "createdTime": current_time,
+            "sku": sku
+        })
+
+    if metrics_path_dict.get("api_inference_path") is not None:
+        validation_result.append({
+            "runId": run_id,
+            "type": "API_VALIDATION",
+            "passed": True,
+            "message": "API inference passed successfully",
+            "validationResultUrl": metrics_path_dict.get("api_inference_path"),
+            "status": "success",
+            "createdTime": current_time,
+            "sku": sku
+        })
+
+    payload = {
+        "suggestedSKU": sku,
+        "status": "Validation",
+        "subStatus": "Validation_Successful",
+        "validationResult": validation_result
+    }
+
+    api_url = f"{selfserve_base_url}/model-publisher-self-serve/publishers/{publisher_name}/models/{model_name}/model-onboarding-version/{model_version}/updateModelOnboardingVersion?api-version=2024-12-31"
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "User-Agent": "AzureML-ModelPublishing/1.0"
+    }
+
+    try:
+        logger.info(f"Sending request to {api_url} \n, headers: {headers} \n, payload: {payload}")
+
+        response = requests.put(api_url, headers=headers, json=payload)
+
+        logger.info(f"Response: {response.json()}")
+
+        if response.ok:
+            logger.info(
+                f"Successfully updated model onboarding version. Response: {response.status_code}")
+            return {"status_code": response.status_code}
+        else:
+            logger.error(
+                f"Failed to update model onboarding version. Status code: {response.status_code}")
+            logger.error(f"Response content: {response.json()}")
+            raise Exception(
+                f"Request failed with status code {response.status_code}: {response.json()}")
+    except requests.RequestException as e:
+        logging.error(f"Request failed: {e}")
+        raise
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Update model onboarding version with CommonBench validation results")
+
+    parser.add_argument("--publisher-name", required=True,
+                        help="Name of the model publisher (e.g., ContosoAI)")
+    parser.add_argument("--model-name", required=True,
+                        help="Name of the model (e.g., VerboGenie)")
+    parser.add_argument("--model-version", required=True,
+                        help="Model onboarding version (e.g., 5)")
+    parser.add_argument("--selfserve-base-url", required=True,
+                        default="https://int.api.azureml-test.ms",
+                        help="Base URL of the model publisher self-serve API")
+    parser.add_argument("--metrics-storage-uri", required=True,
+                        help="URI to the storage where validation metrics are stored")
+    parser.add_argument("--sku", required=False,
+                        default="Standard_NC24ads_A100_v4",
+                        help="Suggested SKU based on benchmark results")
+
+    args = parser.parse_args()
+    logger.info(f"Arguments: {args}")
+
+    try:
+        result = update_model_onboarding_version(
+            args.publisher_name,
+            args.model_name,
+            args.model_version,
+            args.selfserve_base_url,
+            args.sku,
+            args.metrics_storage_uri
+        )
+        logger.info("Model onboarding version update completed successfully")
+    except Exception as e:
+        logger.error(f"Failed to update model onboarding version: {e}")
+        sys.exit(1)
---
+++ assets/training/model_management/src/run_inference_validation.py
@@ -0,0 +1,232 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Validate the structure of expected and actual inference response JSON files."""
+
+import json
+import argparse
+import os
+from azureml.core import Run
+from azureml.model.mgmt.utils.common_utils import get_mlclient
+from azureml.model.mgmt.config import AppName
+from azureml.model.mgmt.utils.logging_utils import custom_dimensions, get_logger
+
+
+logger = get_logger(__name__)
+custom_dimensions.app_name = AppName.RUN_INFERENCE_VALIDATION
+
+
+def load_json(file_path):
+    """Load JSON data from a file. If the loaded data is a string, try to parse it as JSON."""
+    try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        # If data is a string, parse it as JSON.
+        if isinstance(data, str):
+            try:
+                data = json.loads(data)
+            except Exception as e:
+                logger.warning(f"Error parsing JSON from string in {file_path}: {e}")
+        return data
+    except Exception as e:
+        logger.warning(f"Error loading JSON file {file_path}: {e}")
+        return None
+
+
+def get_json_structure(data):
+    """
+    Recursively extract the structure of JSON (keys only).
+
+    For dictionaries, returns a dict of keys mapped to their structure.
+    For lists, returns a list with the structure of the first element.
+    For other types, returns None.
+    """
+    if isinstance(data, dict):
+        return {key: get_json_structure(value) for key, value in data.items()}
+    elif isinstance(data, list) and len(data) > 0:
+        # Assume all elements share the same structure and return the structure of the first element.
+        return [get_json_structure(data[0])]
+    else:
+        return None
+
+
+def compare_structures(inference_payload, expected_response, inference_response):
+    """
+    Compare JSON structures (keys only) of expected and actual.
+
+    Returns a dictionary with validation results.
+    """
+    expected_structure = get_json_structure(expected_response)
+    actual_structure = get_json_structure(inference_response)
+    logger.info(f"expected_structure: {expected_structure} \n actual_structure: {actual_structure}")
+
+    result = {
+        "inference_payload": inference_payload,
+        "inference_output": inference_response, 
+        "structure_match": expected_structure == actual_structure,
+        "expected_structure": expected_structure,
+        "actual_structure": actual_structure,
+        "differences": []
+    }
+
+    if not result["structure_match"]:
+        result["differences"] = [
+            {"expected": expected_structure, "actual": actual_structure}
+        ]
+    logger.info(f"result: {result}")
+    return result
+
+
+def save_validation_result(result, output_path):
+    """Save validation results to a JSON file."""
+    try:
+        with open(output_path, "w") as f:
+            json.dump(result, f, indent=4)
+        logger.info(f"Validation result saved to {output_path}")
+    except Exception as e:
+        logger.error(f"Error saving validation result: {e}")
+
+def replace_name_in_path(path_template, name_value):
+    """Replace the placeholder in the output path with the actual job name."""
+    return path_template.replace('${{name}}', name_value)
+
+def fetch_storage_uri():
+    """Return the storage URI of the output file from the AzureML pipeline run."""
+    try:
+        run = Run.get_context()
+        run_details = run.get_details()
+        output_data_path = run_details['runDefinition']['outputData']['validation_result']['outputLocation']['uri']['path']
+        
+        output_data_uri = replace_name_in_path(output_data_path, run.id)
+
+        logger.info(f"Output data URI: {output_data_uri}, output_data_path: {output_data_path}")
+
+        # Extract datastore name and path from the AzureML URI
+        datastore_name, path = extract_datastore_info(output_data_uri)
+        
+        # Construct the storage URI
+        storage_uri = get_storage_url(datastore_name)
+        full_storage_uri = f"{storage_uri}/{path}"
+        logger.info(f"Full storage URI: {full_storage_uri}")
+        
+        return full_storage_uri
+    except Exception as e:
+        logger.error(f"Error fetching storage URI: {e}")
+        return None
+
+
+def store_metrics_paths(metrics_file_path):
+    """Store the paths of the metrics CSV files in a JSON file."""
+    base_path = fetch_storage_uri()
+
+    logger.info(f"validation_result_path: {base_path}")
+    result_dict = {}
+    result_dict['api_inference_path'] = base_path
+    if result_dict:
+        write_results_to_file(result_dict, metrics_file_path)
+
+
+def fetch_path(output_dir):
+    """Return the relative path of the data from the output directory."""
+    try:
+        # Calculate relative path from the job folder
+        rel_path = os.path.relpath(output_dir, os.getcwd())
+        logger.info(f"rel_path: {rel_path}")
+        result_dict = {
+            'api_inference_path': rel_path
+        }
+        return result_dict
+    except Exception as e:
+        logger.error(f"Error calculating relative path: {e}")
+        return {}
+
+
+def write_results_to_file(results_dict, file_path):
+    """Write the results dictionary to a JSON file."""
+    try:
+        with open(file_path, 'w') as f:
+            json.dump(results_dict, f, indent=4)
+        logger.info(f"Results written to {file_path} in JSON format")
+        return True
+    except Exception as e:
+        logger.error(f"Error writing to file: {e}")
+        return False
+
+
+def get_storage_url(datastore_name):
+    """Retrieve the storage URL for the specified datastore."""
+    # Get MLClient instance
+    ml_client = get_mlclient()
+    logger.info(f"ml_client: {ml_client}")
+    datastore = ml_client.datastores.get(datastore_name)
+    storage_account_name = datastore.account_name
+    container_name = datastore.container_name
+    endpoint = datastore.endpoint
+
+    storage_uri = f"https://{storage_account_name}.blob.{endpoint}/{container_name}"
+    logger.info(f"storage_uri: {storage_uri}")
+
+    return storage_uri
+
+
+def extract_datastore_info(datastore_uri_path):
+    """Extract both datastore name and path from an Azure ML datastore URI path."""
+    # Check if it's a valid datastore URI
+    if not datastore_uri_path.startswith('azureml://datastores/'):
+        return None, None
+
+    parts = datastore_uri_path.split('/')
+
+    # The datastore name should be the part after 'datastores/'
+    if len(parts) >= 5 and parts[0] == 'azureml:' and parts[1] == '' and parts[2] == 'datastores' and 'paths' in parts:
+        datastore_name = parts[3]
+
+        # Find the index of 'paths' in the URI
+        paths_index = parts.index('paths')
+
+        # Join everything after 'paths/' to form the path
+        path = '/'.join(parts[(paths_index + 1):])
+
+        return datastore_name, path
+
+    return None, None
+
+
+def main():
+    """Compare expected and actual inference response structures."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inference_payload", type=str, required=True,
+                        help="Path to the expected inference response JSON file.")
+    parser.add_argument("--expected_response", type=str, required=True,
+                        help="Path to the expected inference response JSON file.")
+    parser.add_argument("--inference_response", type=str, required=True,
+                        help="Path to the actual inference response JSON file.")
+    parser.add_argument("--validation_result", type=str, required=True,
+                        help="Path to save validation results.")
+    parser.add_argument("--metrics_storage_uri", type=str, required=True,
+                        help="Path to store the metrics.")
+
+    args = parser.parse_args()
+
+    # Load expected and actual responses.
+    inference_payload = load_json(args.inference_payload)
+    expected_response = load_json(args.expected_response)
+    inference_response = load_json(args.inference_response)
+    logger.info(f"expected response: {expected_response}, actual response: {inference_response}")
+
+    if expected_response is None or inference_response is None:
+        logger.warning("One or both JSON files could not be loaded.")
+        return
+
+    # Compare the JSON structures.
+    validation_result = compare_structures(inference_payload, expected_response, inference_response)
+
+    # Save the validation result.
+    save_validation_result(validation_result, args.validation_result)
+    logger.info(f"validation_result: {validation_result}, Validation result saved to {args.validation_result}")
+
+    store_metrics_paths(args.metrics_storage_uri)
+
+
+if __name__ == "__main__":
+    main()
