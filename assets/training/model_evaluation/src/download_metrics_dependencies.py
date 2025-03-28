eaa902cbc101675902899fbb044fa6a6aa79c13b
Chandra Sekhar Gupta Aravapalli <caravapalli@microsoft.com>
2025-03-28 14:16:29 +0530

    updating the download dependencies script for compute metrics.

--- assets/training/model_evaluation/src/download_metrics_dependencies.py
+++ assets/training/model_evaluation/src/download_metrics_dependencies.py
@@ -1,63 +1,30 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Script for downloading and installing mlflow model dependencies."""
+"""Script for downloading and installing runtime dependencies for compute metrics."""
 
-from mlflow.pyfunc import _get_model_dependencies
-from utils import ArgumentParser
 from logging_utilities import (
-    custom_dimensions, get_logger, log_traceback, swallow_all_exceptions, get_azureml_exception
+    custom_dimensions, get_logger, swallow_all_exceptions, get_azureml_exception
 )
 from azureml.telemetry.activity import log_activity
-from error_definitions import InvalidModel
-from exceptions import ModelValidationException
-from validation import _validate_model
-from constants import ArgumentLiterals, TelemetryConstants
+from constants import TelemetryConstants
 
 import sys
 import subprocess
 
-custom_dimensions.app_name = TelemetryConstants.DOWNLOAD_MODEL_DEPENDENCIES
+custom_dimensions.app_name = TelemetryConstants.DOWNLOAD_METRICS_DEPENDENCIES
 logger = get_logger(name=__name__)
 custom_dims_dict = vars(custom_dimensions)
 
 
 @swallow_all_exceptions(logger)
 def main():
-    """Download and Install mlflow model dependencies."""
-    parser = ArgumentParser()
-    parser.add_argument("--mlflow-model", type=str, dest=ArgumentLiterals.MLFLOW_MODEL, required=True)
-
-    args, _ = parser.parse_known_args()
-    args = vars(args)
-
-    _validate_model(args)
-
-    with log_activity(logger, TelemetryConstants.DOWNLOAD_MODEL_DEPENDENCIES,
+    """Download and Install compute metrics dependencies."""
+    with log_activity(logger, TelemetryConstants.DOWNLOAD_METRICS_DEPENDENCIES,
                       custom_dimensions=custom_dims_dict):
-        try:
-            reqs_file = _get_model_dependencies(args[ArgumentLiterals.MLFLOW_MODEL], "pip")
-        except Exception as e:
-            exception = get_azureml_exception(ModelValidationException, InvalidModel, e)
-            log_traceback(exception, logger)
-            raise exception
-
-        ignore_deps = [
-            "mlflow",
-            "azureml_evaluate_mlflow",
-            "azureml-evaluate-mlflow",
-            "azureml_metrics",
-            "azureml-metrics",
-            ]
         install_deps = [
             "azure-ai-ml",
         ]
-        logger.info(f"Reading from {reqs_file}")
-        with open(reqs_file, "r") as f:
-            for line in f.readlines():
-                if any(dep in line.strip() for dep in ignore_deps):
-                    continue
-                install_deps += [line.strip()]
 
         no_install = []
         if len(install_deps) > 0:
