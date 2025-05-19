6bafa8740c8623717a5a91d07067f87e7cea770b
UTKARSH SHUKLA <utkshukla@microsoft.com>
2025-05-19 14:46:00 +0530

    4021541

--- assets/training/model_management/environments/mlflow-model-inference/context/mlflow_score_script.py
+++ assets/training/model_management/environments/mlflow-model-inference/context/mlflow_score_script.py
@@ -161,8 +161,9 @@ def create_param_sample(model_signature_params):
                     _logger.info(f"Unhandled param type: {param_type}")
     return sample_params
 
-
-model_path = os.path.normpath(os.path.join(os.getenv("AZUREML_MODEL_DIR"), os.getenv("MLFLOW_MODEL_FOLDER")))
+model_path = os.path.normpath(os.path.join(os.getenv("AZUREML_MODEL_DIR"), "mlflow_model_folder/"))
+if os.getenv("MLFLOW_MODEL_FOLDER"):
+    model_path = os.path.normpath(os.path.join(os.getenv("AZUREML_MODEL_DIR"), os.getenv("MLFLOW_MODEL_FOLDER")))
 
 # model loaded here using mlfow.models import Model so we have access to the model signature
 model = Model.load(model_path)
