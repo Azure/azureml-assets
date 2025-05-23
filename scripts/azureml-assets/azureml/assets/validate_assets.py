d8ffc7a5d7e1744e2d6f80131479ef0dbe9e7b76
Kelly <kellyly@microsoft.com>
2025-05-23 15:55:35 -0400

    Update

--- scripts/azureml-assets/azureml/assets/validate_assets.py
+++ scripts/azureml-assets/azureml/assets/validate_assets.py
@@ -952,20 +952,22 @@ def validate_mlflow_model(asset_config: assets.AssetConfig) -> int:
     Args:
         asset_config (assets.AssetConfig): asset config for model spec
     """
-    model = model_config = None
+    model_config = None
 
     try:
         model_config: assets.ModelConfig = asset_config.extra_config_as_object()
     except Exception:
         logger.log_warning("Could not validate if model is of type MLFlow due to invalid model config")
 
-    if model_config.type == assets.config.ModelType.MLFLOW:
-        logger.log_warning(f"{asset_config.name} is a model of type {model_config.type.value} " 
+    is_mlflow_model = model_config.type == assets.config.ModelType.MLFLOW
+    if is_mlflow_model:
+        logger.log_warning(f"{asset_config.name} is a model of type {model_config.type.value} "
                            f"which is banned from the model catalog")
-        # Update mlflow_model_detected variable for Github Actions only
-        if "GITHUB_OUTPUT" in os.environ:
-            with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-                print(f"mlflow_model_detected=True", file=f)
+
+    # Update mlflow_model_detected variable for Github Actions only
+    if "GITHUB_OUTPUT" in os.environ:
+        with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+            print(f"mlflow_model_detected={is_mlflow_model}", file=f)
 
 
 def get_validated_models_assets_map(model_validation_results_dir: str):
