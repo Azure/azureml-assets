33c80e78431703f7d5d4376a8288e67fccf01106
Elaine Wan <elainewan@microsoft.com>
2025-06-03 12:37:41 -0400

    add docstrings

--- assets/evaluation_on_cloud/environments/evaluations-built-in/tests/test_evaluate_on_data.py
+++ assets/evaluation_on_cloud/environments/evaluations-built-in/tests/test_evaluate_on_data.py
@@ -1,6 +1,10 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+"""
+Tests for evaluate_on_data.py
+"""
+
 import sys
 from pathlib import Path
 import os
@@ -8,6 +12,9 @@ import importlib
 
 
 def test_evaluate_obo_sets_env_var():
+    """
+    Test that importing evaluate_on_data.py sets the AZUREML_OBO_ENABLED environment variable to "True"
+    """
     script_path = Path(__file__).parent.parent / "context" / "evaluate_on_data.py"
     module_name = "evaluate_on_data"
     sys.path.insert(0, str(script_path.parent))
