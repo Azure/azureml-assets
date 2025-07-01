b34abd49fe26cda9254f99432a7be1ef3b06f505
Kelly <40868256+lykelly19@users.noreply.github.com>
2025-07-01 16:22:19 -0400

    Update create_azure_resources_daily.sh with --idle-time-before-scale-down=120

--- scripts/setup/create_azure_resources_daily.sh
+++ scripts/setup/create_azure_resources_daily.sh
@@ -43,21 +43,21 @@ else
     echo "Checking ${resource_name}"
     if ! az ml compute show --name $cpu_cluster --output none >/dev/null 2>&1; then
         echo "Creating ${resource_name}"
-        az ml compute create --name $cpu_cluster --size Standard_DS3_v2 --min-instances 0 --max-instances 10 --type AmlCompute
+        az ml compute create --name $cpu_cluster --size Standard_DS3_v2 --min-instances 0 --max-instances 10 --type AmlCompute --idle-time-before-scale-down 120
     fi
 
     resource_name="compute cluster ${gpu_cluster}"
     echo "Checking ${resource_name}"
     if ! az ml compute show --name $gpu_cluster --output none >/dev/null 2>&1; then
         echo "Creating ${resource_name}"
-        az ml compute create --name $gpu_cluster --size Standard_NC4as_T4_v3 --min-instances 0 --max-instances 10 --type AmlCompute
+        az ml compute create --name $gpu_cluster --size Standard_NC4as_T4_v3 --min-instances 0 --max-instances 10 --type AmlCompute --idle-time-before-scale-down 120
     fi
 
     resource_name="compute cluster ${gpu_v100_cluster}"
     echo "Checking ${resource_name}"
     if ! az ml compute show --name $gpu_v100_cluster --output none >/dev/null 2>&1; then
         echo "Creating ${resource_name}"
-        az ml compute create --name $gpu_v100_cluster --size Standard_NC6s_v3 --min-instances 0 --max-instances 2 --type AmlCompute
+        az ml compute create --name $gpu_v100_cluster --size Standard_NC6s_v3 --min-instances 0 --max-instances 2 --type AmlCompute --idle-time-before-scale-down 120
     fi
 fi
 
