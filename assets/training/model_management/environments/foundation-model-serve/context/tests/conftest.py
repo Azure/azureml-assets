fc02d47745eb00beb8e97521e53ead2e34bc6e50
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2025-11-20 17:51:39 +0530

    Fix source directory path in conftest.py
    Updated the source directory path for test discovery.

--- assets/training/model_management/environments/foundation-model-serve/context/tests/conftest.py
+++ assets/training/model_management/environments/foundation-model-serve/context/tests/conftest.py
@@ -5,7 +5,7 @@ import sys
 def pytest_configure(config):
     current_directory = os.path.dirname(os.path.abspath(__file__))
     parent_directory = os.path.dirname(current_directory)
-    src_directory = os.path.join(parent_directory, "foundation", "model", "serve")
+    src_directory = os.path.join(parent_directory, "context", "foundation", "model", "serve")
 
     # Need to add src directory to the path to enable discovery of src files by the test directory
     sys.path.append(parent_directory)
