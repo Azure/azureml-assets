2549353320031ee3277c6616182a52bf1cf29394
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2025-11-21 11:51:45 +0530

    Update conftest.py

--- assets/training/model_management/environments/foundation-model-serve/context/tests/conftest.py
+++ assets/training/model_management/environments/foundation-model-serve/context/tests/conftest.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-
+"""Model package containing model serving functionality."""
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
 import os
 import sys
 
