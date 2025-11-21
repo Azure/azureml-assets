6a928da22f8c99ad78a35451734e5080ca42b748
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2025-11-21 17:41:05 +0530

    Refactor test_replica_manager.py for clarity

--- assets/training/model_management/environments/foundation-model-serve/context/tests/test_replica_manager.py
+++ assets/training/model_management/environments/foundation-model-serve/context/tests/test_replica_manager.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
@@ -363,4 +362,3 @@ class TestReplicaManager(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
->>>>>>> 4736c86812f5a79482f8001ee49abe9393309f85
