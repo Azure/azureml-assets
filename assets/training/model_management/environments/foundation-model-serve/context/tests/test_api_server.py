d69474b549d9430415989c765a6cbb074e1f5ef3
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2025-11-24 16:37:19 +0530

    Update test_api_server.py

--- assets/training/model_management/environments/foundation-model-serve/context/tests/test_api_server.py
+++ assets/training/model_management/environments/foundation-model-serve/context/tests/test_api_server.py
@@ -297,8 +297,8 @@ class TestAPIServer(unittest.TestCase):
 
     @patch('api_server.g_served_model', new="g_served_model")
     @patch('api_server.task_type', new=SupportedTask.CHAT_COMPLETION)
-    @patch('api_server.g_engine_config', new=asdict(EngineConfig(engine_name="vllm"
-                                                                    , model_id="model_id", tokenizer="tokenizer")))
+    @patch('api_server.g_engine_config', new=asdict(EngineConfig(engine_name="vllm",
+                                                                    model_id="model_id",tokenizer="tokenizer")))
     @patch('api_server.g_model_info', new=mock_model_info())
     @patch('api_server.g_fmscorer')
     def test_chat_completion_valid_request(self, mock_fmscorer):
@@ -306,8 +306,8 @@ class TestAPIServer(unittest.TestCase):
         response.json.return_value = self.vllm_chat_response
         response.status_code = 200
         mock_fmscorer.run_openai_async.return_value = response
-        final_response = self.client.post(get_serving_url(SupportedTask.CHAT_COMPLETION)
-                                                              , json=self.chat_request)
+        final_response = self.client.post(get_serving_url(SupportedTask.CHAT_COMPLETION),
+                                                              json=self.chat_request)
         assert final_response.status_code == 200
         expected_model = "meme"
         final_chat_response = self.vllm_chat_response.copy()
@@ -316,8 +316,8 @@ class TestAPIServer(unittest.TestCase):
 
     @patch('api_server.g_served_model', new="g_served_model")
     @patch('api_server.task_type', new=SupportedTask.CHAT_COMPLETION)
-    @patch('api_server.g_engine_config', new=asdict(EngineConfig(engine_name="vllm"
-                                                                     , model_id="model_id", tokenizer="tokenizer")))
+    @patch('api_server.g_engine_config', new=asdict(EngineConfig(engine_name="vllm",
+                                                                     model_id="model_id", tokenizer="tokenizer")))
     @patch('api_server.g_fmscorer')
     def test_chat_completion_missing_messages(self, mock_fmscorer):
         request_data = {
@@ -495,4 +495,5 @@ class TestAPIServer(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
 
