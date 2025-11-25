8253ce0f621a32490a70fc4305f01f3a6a8b0ee4
SuchethaChintha <141749743+SuchethaChintha@users.noreply.github.com>
2025-11-25 12:15:09 +0530

    Update test_managed_inference.py

--- assets/training/model_management/environments/foundation-model-serve/context/tests/test_managed_inference.py
+++ assets/training/model_management/environments/foundation-model-serve/context/tests/test_managed_inference.py
@@ -84,16 +84,16 @@ class TestMIRPayload(unittest.TestCase):
                                 "role": "user", "content": [
                                     {
                                         "type": "image_url",
-                                                     "image_url": {
+                                        "image_url": {
                                                             "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
                                                                   },
-                                    }, 
+                                    },
                                     {
                                         "type": "image_url", "image_url": {
                                             "url": "https://www.ilankelman.org/stopsigns/australia.jpg"},
-                                    }, 
+                                    },
                                     {
-                                                "type": "text", "text": "What are in these images? What is the difference between two images?", }, ], }], "parameters": {
+                                        "type": "text", "text": "What are in these images? What is the difference between two images?", }, ], }], "parameters": {
                             "temperature": 0.7, "top_p": 0.6, "do_sample": True, "max_new_tokens": 200, }, }}]}
         self.valid_text_gen_queries = [
             ["the meaning of life is"],
@@ -357,6 +357,7 @@ def test_get_request_data_text_to_image_inpainting():
 
 if __name__ == "__main__":
     unittest.main()
+
 
 
 
