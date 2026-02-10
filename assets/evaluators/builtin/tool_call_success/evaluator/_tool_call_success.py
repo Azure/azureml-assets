5dde9f87177bb76c148e230dc70cc3fff193e3df
mohessie <mohessie@microsoft.com>
2026-02-10 23:53:17 +0200

    [Agentic Evaluators]: Accept string as is

--- assets/evaluators/builtin/tool_call_success/evaluator/_tool_call_success.py
+++ assets/evaluators/builtin/tool_call_success/evaluator/_tool_call_success.py
@@ -157,16 +157,27 @@ class ToolCallSuccessEvaluator(PromptyEvaluatorBase[Union[str, float]]):
                 target=ExtendedErrorTarget.TOOL_CALL_SUCCESS_EVALUATOR,
             )
 
-        eval_input["tool_calls"] = _reformat_tool_calls_results(eval_input["response"], logger)
+        # If response is a string, pass directly without reformatting
+        if isinstance(eval_input["response"], str):
+            # Unless tool calls are explicitly provided, then keep it as is
+            if "tool_calls" not in eval_input or not eval_input["tool_calls"]:
+                eval_input["tool_calls"] = eval_input["response"]
+        else:
+            eval_input["tool_calls"] = _reformat_tool_calls_results(eval_input["response"], logger)
 
-        if "tool_definitions" in eval_input:
+        # If tool definitions are string, pass directly without reformatting, else format it.
+        # IF response is not a string, we filter tool definitions to only tools needed.
+        if "tool_definitions" in eval_input and not isinstance(eval_input["tool_definitions"], str):
             tool_definitions = eval_input["tool_definitions"]
-            filtered_tool_definitions = _filter_to_used_tools(
-                tool_definitions=tool_definitions,
-                msgs_list=eval_input["response"],
-                logger=logger,
-            )
-            eval_input["tool_definitions"] = _reformat_tool_definitions(filtered_tool_definitions, logger)
+            if isinstance(eval_input["response"], str):
+                eval_input["tool_definitions"] = _reformat_tool_definitions(tool_definitions, logger)
+            else:
+                filtered_tool_definitions = _filter_to_used_tools(
+                    tool_definitions=tool_definitions,
+                    msgs_list=eval_input["response"],
+                    logger=logger,
+                )
+                eval_input["tool_definitions"] = _reformat_tool_definitions(filtered_tool_definitions, logger)
 
         prompty_output_dict = await self._flow(timeout=self._LLM_CALL_TIMEOUT, **eval_input)
         llm_output = prompty_output_dict.get("llm_output", "")
