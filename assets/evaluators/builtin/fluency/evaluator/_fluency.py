c82bda50c8c7b95d36fe537fb5aa7aac9e440e72
Salma El-Shafey <76866105+salma-elshafey@users.noreply.github.com>
2026-03-31 16:13:47 +0200

    Add response reformatting for Fluency Evaluator (#4904)
    * Add response reformatting for Fluency Evaluator
    
    * code health
    
    * Remove preprocessing of query

--- assets/evaluators/builtin/fluency/evaluator/_fluency.py
+++ assets/evaluators/builtin/fluency/evaluator/_fluency.py
@@ -3,6 +3,7 @@
 
 import math
 import os
+import logging
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Any, Dict, Optional, List, Union
@@ -12,7 +13,7 @@ from typing_extensions import overload, override
 from azure.ai.evaluation._evaluators._common import PromptyEvaluatorBase
 from azure.ai.evaluation._model_configurations import Conversation
 from azure.ai.evaluation._exceptions import EvaluationException, ErrorBlame, ErrorCategory, ErrorTarget
-
+from azure.ai.evaluation._common.utils import reformat_agent_response
 
 # region Validators
 
@@ -546,6 +547,9 @@ def _preprocess_messages(messages):
     return messages
 
 
+logger = logging.getLogger(__name__)
+
+
 class FluencyEvaluator(PromptyEvaluatorBase[Union[str, float]]):
     """
     Evaluates the fluency of a given response or a multi-turn conversation, including reasoning.
@@ -740,8 +744,8 @@ class FluencyEvaluator(PromptyEvaluatorBase[Union[str, float]]):
             )
         if isinstance(eval_input.get("response"), list):
             eval_input["response"] = _preprocess_messages(eval_input["response"])
-        if isinstance(eval_input.get("query"), list):
-            eval_input["query"] = _preprocess_messages(eval_input["query"])
+
+        eval_input["response"] = reformat_agent_response(eval_input.get("response"), logger)
 
         result = await super()._do_eval(eval_input)
 
