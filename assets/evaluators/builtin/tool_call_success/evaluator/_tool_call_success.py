7b80a35e10f538fe0a5b131c019701c2d02af56c
ashaabansoliman <asayedahmed@microsoft.com>
2026-06-04 22:54:22 +0300

    remove f

--- assets/evaluators/builtin/tool_call_success/evaluator/_tool_call_success.py
+++ assets/evaluators/builtin/tool_call_success/evaluator/_tool_call_success.py
@@ -1053,8 +1053,8 @@ def _reformat_tool_calls_results(response, logger=None):
             # fallback to the original response in that case
             if logger:
                 logger.warning(
-                    f"Empty agent response extracted, likely due to input schema change. "
-                    f"Falling back to using the original response "
+                    "Empty agent response extracted, likely due to input schema change. "
+                    "Falling back to using the original response "
                 )
                 logger.debug(f"Original response: {response}")
             return response
